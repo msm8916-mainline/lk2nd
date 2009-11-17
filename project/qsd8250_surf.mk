8adde5a77bbbad79377046d735a5445b8158476f
Chandan Uddaraju <chandanu@quicinc.com>
2009-11-17 11:31:28 -0800

    [project/target]: Add 3 new projects and the corresponding make files.
    Add msm7627_surf, msm7630_surf and qsd8250_surf projects and their corresponding
    target files.
    
    Signed-off-by: Chandan Uddaraju <chandanu@quicinc.com>

---
+++ project/qsd8250_surf.mk
@@ -0,0 +1,11 @@
+# top level project rules for the qsd8250_surf project
+#
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+TARGET := qsd8250_surf
+
+MODULES += app/aboot
+
+#DEFINES += WITH_DEBUG_DCC=1
+#DEFINES += WITH_DEBUG_UART=1
+#DEFINES += WITH_DEBUG_FBCON=1
---
+++ target/msm7630_surf/init.c
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2009, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  * Neither the name of Google, Inc. nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <debug.h>
+#include <dev/keys.h>
+#include <dev/gpio_keypad.h>
+#include <lib/ptable.h>
+#include <dev/flash.h>
+
+static struct ptable flash_ptable;
+
+/* for these partitions, start will be offset by either what we get from
+ * smem, or from the above offset if smem is not useful. Also, we should
+ * probably have smem_ptable code populate our flash_ptable.
+ *
+ * When smem provides us with a full partition table, we can get rid of
+ * this altogether.
+ *
+ */
+static struct ptentry board_part_list[] = {
+	{
+		.start = 0,
+		.length = 40,
+		.name = "boot",
+	},
+	{
+		.start = 56,
+		.length = 608 /* 76MB */,
+		.name = "system",
+	},
+	{
+		.start = 664,
+		.length = 608 /* 76MB */,
+		.name = "cache",
+	},
+	{
+		.start = 1272,
+		.length = 0,
+		.name = "userdata",
+	},
+};
+static int num_parts = sizeof(board_part_list)/sizeof(struct ptentry);
+
+void smem_ptable_init(void);
+unsigned smem_get_apps_flash_start(void);
+
+void keypad_init(void);
+
+void target_init(void)
+{
+	unsigned offset;
+	struct flash_info *flash_info;
+	int i;
+
+	dprintf(INFO, "target_init()\n");
+
+	keys_init();
+	keypad_init();
+
+	ptable_init(&flash_ptable);
+	smem_ptable_init();
+
+	flash_init();
+	flash_info = flash_get_info();
+	ASSERT(flash_info);
+
+	offset = smem_get_apps_flash_start();
+	if (offset == 0xffffffff)
+	        while(1);
+
+	for (i = 0; i < num_parts; i++) {
+		struct ptentry *ptn = &board_part_list[i];
+		unsigned len = ptn->length;
+
+		if ((len == 0) && (i == num_parts - 1))
+			len = flash_info->num_blocks - offset - ptn->start;
+		ptable_add(&flash_ptable, ptn->name, offset + ptn->start,
+			   len, ptn->flags);
+	}
+
+	ptable_dump(&flash_ptable);
+	flash_set_ptable(&flash_ptable);
+}
