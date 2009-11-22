a66de4f52c1def2fa9ee9a5ff22a493f3baca9b2
Ajay Dudani <adudani@quicinc.com>
2009-11-22 08:57:39 -0800

    bootloader: Clone msm7627_ffa and qsd8250_ffa target from surf

--- AndroidBoot.mk
+++ AndroidBoot.mk
@@ -1,11 +1,11 @@
 #Android makefile to build lk bootloader as a part of Android Build
 
 BOOTLOADER_OUT := $(TOP)/$(TARGET_OUT_INTERMEDIATES)/BOOTLOADER_OBJ
-TARGET_BOOTLOADER := $(BOOTLOADER_OUT)/lk.elf
+TARGET_BOOTLOADER := out/target/product/$(TARGET_PRODUCT)/appsboot.mbn
 
 $(BOOTLOADER_OUT):
 	mkdir -p $(BOOTLOADER_OUT)
 
 $(TARGET_BOOTLOADER): $(BOOTLOADER_OUT)
-	$(MAKE) -C bootable/bootloader/lk TARGET_OUT=../../../$(TARGET_OUT) BOOTLOADER_OUT=../../../$(BOOTLOADER_OUT) $(TARGET_PRODUCT)
+	$(MAKE) -C bootable/bootloader/lk BOOTLOADER_OUT=../../../$(BOOTLOADER_OUT) $(TARGET_PRODUCT)
 
---
+++ project/qsd8250_ffa.mk
@@ -0,0 +1,11 @@
+# top level project rules for the qsd8250_ffa project
+#
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+TARGET := qsd8250_ffa
+
+MODULES += app/aboot
+
+#DEFINES += WITH_DEBUG_DCC=1
+#DEFINES += WITH_DEBUG_UART=1
+#DEFINES += WITH_DEBUG_FBCON=1
