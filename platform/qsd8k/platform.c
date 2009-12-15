aeb979e0595bd622f33ba18441091589ad35f0b2
Chandan Uddaraju <chandanu@quicinc.com>
2009-12-14 18:04:26 -0800

    [qsd8250]: Enable daisy chain flashing and fix nandwrite compiling issue.
    Update makefile to fix nandwrite compiling issues.
    Add ENABLE_NANDWRITE in list of defines for nandwrite makefiles.
    Reduce the size of cache partition to 5MB from 76MB.
    Disable keypad initialization for nandwrite.
    Add recovery partition for surf and ffa.

--- platform/qsd8k/platform.c
+++ platform/qsd8k/platform.c
@@ -55,7 +55,9 @@ void platform_early_init(void)
 void platform_init(void)
 {
         dprintf(INFO, "platform_init()\n");
+#if (!ENABLE_NANDWRITE)
         acpu_clock_init();
+#endif
 }
 
 void display_init(void)
