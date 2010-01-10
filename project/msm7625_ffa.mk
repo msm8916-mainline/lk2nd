e0adbdb222d31398213378cb153a48d30f6d86cb
Chandan Uddaraju <chandanu@quicinc.com>
2010-01-09 23:12:46 -0800

    [msm7625]: Add LK bootloader support for msm7625_surf and msm7625_ffa.

---
+++ project/msm7625_ffa.mk
@@ -0,0 +1,11 @@
+# top level project rules for the msm7625_ffa project
+#
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+TARGET := msm7625_ffa
+
+MODULES += app/aboot
+
+#DEFINES += WITH_DEBUG_DCC=1
+#DEFINES += WITH_DEBUG_UART=1
+#DEFINES += WITH_DEBUG_FBCON=1
