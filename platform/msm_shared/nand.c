9e5ab966faae3bd01baaa4fc1356605a241000c7
Chandan Uddaraju <chandanu@quicinc.com>
2010-01-21 15:36:37 -0800

    [target/[msm7627,msm7630]: Add persist partition and re-arange partition table.
    Move recovery partition to the end.
    Add persist partition to msm7627_surf, msm7627_ffa and msm7630_surf targets.
    Make userdata partition to have variable length and add code to
    allocate the remaining/leftover space on flash to userdata partition.
    Correct the density value for 4k page nand device in the supported nand
    devices array.
    Increase the system partition for msm7630_surf target by 5MB.

--- platform/msm_shared/nand.c
+++ platform/msm_shared/nand.c
@@ -132,7 +132,7 @@ static struct flash_identification supported_flash[] =
 	{0x004000ec, 0xFFFFFFFF, (256<<20), 0, 2048, (2048<<6), 64, 1}, /*Sams*/
 	{0x005c00ec, 0xFFFFFFFF, (256<<20), 0, 2048, (2048<<6), 64, 1}, /*Sams*/
 	{0x005800ec, 0xFFFFFFFF, (256<<20), 0, 2048, (2048<<6), 64, 1}, /*Sams*/
-	{0x6600bcec, 0xFF00FFFF, (1024<<20), 1, 4096, (4096<<6), 128, 0}, /*Sams*/
+	{0x6600bcec, 0xFF00FFFF, (512<<20), 1, 4096, (2048<<6), 128, 0}, /*Sams*/
 	/* Note: Width flag is 0 for 8 bit Flash and 1 for 16 bit flash	  */
 	/* Note: Onenand flag is 0 for NAND Flash and 1 for OneNAND flash	*/
 	/* Note: The First row will be filled at runtime during ONFI probe	*/
