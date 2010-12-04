ab958e5012e48920fdba6270a28f760291837cdd
Shashank Mittal <mittals@codeaurora.org>
2010-12-03 17:57:22 -0800

    [platform/msm_shared]: Add support for new Micron NAND flash memory.
    CR-Fixed: 267016
    
    Change-Id: Ie4892fc42d57e83c4d2b52f5d70c0d4d0f07759c

--- platform/msm_shared/nand.c
+++ platform/msm_shared/nand.c
@@ -131,6 +131,7 @@ static struct flash_identification supported_flash[] =
 	{0xd580b12c, 0xFFFFFFFF, (256<<20), 1, 2048, (2048<<6), 64, 0}, /*Micr*/
 	{0x5590bc2c, 0xFFFFFFFF, (512<<20), 1, 2048, (2048<<6), 64, 0}, /*Micr*/
 	{0x1580aa2c, 0xFFFFFFFF, (256<<20), 0, 2048, (2048<<6), 64, 0}, /*Micr*/
+	{0x1590aa2c, 0xFFFFFFFF, (256<<20), 0, 2048, (2048<<6), 64, 0}, /*Micr*/
 	{0x1590ac2c, 0xFFFFFFFF, (512<<20), 0, 2048, (2048<<6), 64, 0}, /*Micr*/
 	{0x5580baad, 0xFFFFFFFF, (256<<20), 1, 2048, (2048<<6), 64, 0}, /*Hynx*/
 	{0x5510baad, 0xFFFFFFFF, (256<<20), 1, 2048, (2048<<6), 64, 0}, /*Hynx*/
