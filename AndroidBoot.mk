9055606cb014a1dbeb1e3b9a2de8debd369b9e91
David Ng <dave@codeaurora.org>
2010-12-20 18:36:33 -0800

    lk: Force GCC 4.4.0 for Android LK build and update eMMC check flags
    Change-Id: I82fedb4c4ff4e7471af2c78bf0b63fc069d5b438

--- AndroidBoot.mk
+++ AndroidBoot.mk
@@ -3,6 +3,9 @@
 TARGET_BOOTLOADER := $(PRODUCT_OUT)/appsboot.mbn
 BOOTLOADER_OUT := $(TOP)/$(TARGET_OUT_INTERMEDIATES)/BOOTLOADER_OBJ
 
+# Force GCC 4.4.0 crosstool chain for Android builds
+CROSS_TOOL := ../../../prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-
+
 # Remove bootloader binary to trigger recompile when source changes
 appsbootldr_clean:
 	$(hide) rm -f $(TARGET_BOOTLOADER)
@@ -12,7 +15,19 @@ $(BOOTLOADER_OUT):
 	mkdir -p $(BOOTLOADER_OUT)
 
 
+TARGET_EMMC := 0
 ifeq ($(TARGET_USERIMAGES_USE_EXT2),true)
+  TARGET_EMMC := 1
+endif
+ifeq ($(TARGET_USERIMAGES_USE_EXT3),true)
+  TARGET_EMMC := 1
+endif
+ifeq ($(TARGET_USERIMAGES_USE_EXT4),true)
+  TARGET_EMMC := 1
+endif
+
+
+ifeq ($(TARGET_EMMC),1)
 TARGET_BOOTLOADER_EMMC := $(PRODUCT_OUT)/EMMCBOOT.MBN
 BOOTLOADER_EMMC_OUT := $(TOP)/$(TARGET_OUT_INTERMEDIATES)/BOOTLOADER_EMMC_OBJ
 
@@ -24,13 +39,13 @@ $(BOOTLOADER_EMMC_OUT):
 	mkdir -p $(BOOTLOADER_EMMC_OUT)
 
 $(TARGET_BOOTLOADER): appsbootldr_clean emmc_appsbootldr_clean $(BOOTLOADER_OUT) $(BOOTLOADER_EMMC_OUT)
-	$(MAKE) -C bootable/bootloader/lk BOOTLOADER_OUT=../../../$(BOOTLOADER_OUT) $(TARGET_PRODUCT)
-	$(MAKE) -C bootable/bootloader/lk BOOTLOADER_OUT=../../../$(BOOTLOADER_EMMC_OUT) $(TARGET_PRODUCT) EMMC_BOOT=1
+	$(MAKE) -C bootable/bootloader/lk TOOLCHAIN_PREFIX=$(CROSS_TOOL) BOOTLOADER_OUT=../../../$(BOOTLOADER_OUT) $(TARGET_PRODUCT)
+	$(MAKE) -C bootable/bootloader/lk TOOLCHAIN_PREFIX=$(CROSS_TOOL) BOOTLOADER_OUT=../../../$(BOOTLOADER_EMMC_OUT) $(TARGET_PRODUCT) EMMC_BOOT=1
 
 else
 
 $(TARGET_BOOTLOADER): appsbootldr_clean $(BOOTLOADER_OUT)
-	$(MAKE) -C bootable/bootloader/lk BOOTLOADER_OUT=../../../$(BOOTLOADER_OUT) $(TARGET_PRODUCT)
+	$(MAKE) -C bootable/bootloader/lk TOOLCHAIN_PREFIX=$(CROSS_TOOL) BOOTLOADER_OUT=../../../$(BOOTLOADER_OUT) $(TARGET_PRODUCT)
 
 endif
 
@@ -47,5 +62,5 @@ $(NANDWRITE_OUT):
 
 $(TARGET_NANDWRITE): nandwrite_clean $(NANDWRITE_OUT)
 	@echo $(TARGET_PRODUCT)_nandwrite
-	$(MAKE) -C bootable/bootloader/lk BOOTLOADER_OUT=../../../$(NANDWRITE_OUT) $(TARGET_PRODUCT)_nandwrite BUILD_NANDWRITE=1
+	$(MAKE) -C bootable/bootloader/lk TOOLCHAIN_PREFIX=$(CROSS_TOOL) BOOTLOADER_OUT=../../../$(NANDWRITE_OUT) $(TARGET_PRODUCT)_nandwrite BUILD_NANDWRITE=1
 
