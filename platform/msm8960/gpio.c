f34df2f9c321340e7b3abe59b53782ab0a2bfb01
Amol Jadi <amoljadi@codeaurora.org>
2012-10-19 13:38:16 -0700

    msm: fix gpio config for mpq8064 uart
    TLMM was not configured correctly for uart.
    
    Change-Id: I3ac0a65bd7a72f90e562907714cf793ad9a3624c

--- platform/msm8960/gpio.c
+++ platform/msm8960/gpio.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2012, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -9,7 +9,7 @@
  *    copyright notice, this list of conditions and the following
  *    disclaimer in the documentation and/or other materials provided
  *    with the distribution.
- *  * Neither the name of Code Aurora Forum, Inc. nor the names of its
+ *  * Neither the name of The Linux Foundation. nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
@@ -69,10 +69,10 @@ void gpio_config_uart_dm(uint8_t id)
 
 		case GSBI_ID_5:
 			/* configure rx gpio */
-			gpio_tlmm_config(52, 1, GPIO_INPUT, GPIO_NO_PULL,
+			gpio_tlmm_config(52, 2, GPIO_INPUT, GPIO_NO_PULL,
 							 GPIO_8MA, GPIO_DISABLE);
 			/* configure tx gpio */
-			gpio_tlmm_config(51, 1, GPIO_OUTPUT, GPIO_NO_PULL,
+			gpio_tlmm_config(51, 2, GPIO_OUTPUT, GPIO_NO_PULL,
 							 GPIO_8MA, GPIO_DISABLE);
 			break;
 
