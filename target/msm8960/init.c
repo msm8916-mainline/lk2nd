141f298f0417969ae9d990cd679fc9917cee8280
Channagoud Kadabi <ckadabi@codeaurora.org>
2012-10-31 11:23:02 +0530

    msm8960: Add support for dsda platform
    Add support for DSDA platform subtype & pass the command line
    argument based on the platform subtype.
    
    Change-Id: Id921f780184d731c937f9202cf994ce6ee73a55f

--- target/msm8960/init.c
+++ target/msm8960/init.c
@@ -431,6 +431,8 @@ void target_baseband_detect(struct board_data *board)
 		baseband = BASEBAND_MDM;
 	else if (platform_subtype == HW_PLATFORM_SUBTYPE_SGLTE)
 		baseband = BASEBAND_SGLTE;
+	else if (platform_subtype == HW_PLATFORM_SUBTYPE_DSDA)
+		baseband = BASEBAND_DSDA;
 	else {
 		switch(platform) {
 		case APQ8060:
