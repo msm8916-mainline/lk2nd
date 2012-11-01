d99e2cc75713865ccb154082f8b00d81e275bca8
Travis Geiselbrecht <geist@foobox.com>
2012-10-31 21:47:17 -0700

    [lib] run (not) all of lib and include through astyle
    (cherry picked from commit 9b961b4a342bf236a0b3d21d0cabb766574a4794)

--- include/lib/fs/ext2.h
+++ include/lib/fs/ext2.h
@@ -8,10 +8,10 @@
  * publish, distribute, sublicense, and/or sell copies of the Software,
  * and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -39,4 +39,3 @@ int ext2_close_file(fsfilecookie fcookie);
 int ext2_stat_file(fsfilecookie fcookie, struct file_stat *);
 
 #endif
-
