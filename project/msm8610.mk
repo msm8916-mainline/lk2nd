e92cdb0ec48804c88be660486280d9dbf1763642
Channagoud Kadabi <ckadabi@codeaurora.org>
2013-05-09 16:06:30 -0700

    project: msm8610: Compile time config for sdhci
    Enable sdhci by default for 8610 target
    
    Change-Id: Ibcc3a4f6d97bf569fe41c766532d00884c2b4b05

--- project/msm8610.mk
+++ project/msm8610.mk
@@ -7,6 +7,7 @@ TARGET := msm8610
 MODULES += app/aboot
 
 DEBUG := 1
+ENABLE_SDHCI_SUPPORT := 1
 
 #DEFINES += WITH_DEBUG_DCC=1
 DEFINES += WITH_DEBUG_UART=1
@@ -20,3 +21,7 @@ DEFINES += DEVICE_TREE=1
 #thumb interworking code for LK. Confirm that the issue
 #is with the linker and file a bug report.
 ENABLE_THUMB := false
+
+ifeq ($(ENABLE_SDHCI_SUPPORT),1)
+DEFINES += MMC_SDHCI_SUPPORT=1
+endif
