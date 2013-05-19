710e2ec00c45abc59a731d828a089d7072be30cb
Asaf Penso <apenso@codeaurora.org>
2013-05-19 18:29:01 +0300

    msm8974: display: Add support for eDP panel in LiQUID
    Add support for AUO 1920x1080 RGB666 eDP panel for LiQUID8974.
    
    Change-Id: Ie2b7c87b7751f86ef3167bfa1ab5ff4db437bc9f

--- dev/panel/msm/rules.mk
+++ dev/panel/msm/rules.mk
@@ -24,7 +24,8 @@ endif
 ifeq ($(PLATFORM),msm8974)
 OBJS += \
 	$(LOCAL_DIR)/mipi_toshiba_video_720p.o \
-	$(LOCAL_DIR)/mipi_sharp_video_qhd.o
+	$(LOCAL_DIR)/mipi_sharp_video_qhd.o \
+	$(LOCAL_DIR)/edp_auo_1080p.o
 endif
 
 ifeq ($(PLATFORM),msm8226)
