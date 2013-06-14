caf9e773696d96e8de672f61755438bdf87bc23a
Deepa Dinamani <deepad@codeaurora.org>
2013-06-14 12:39:41 -0700

    target: msm8974: Add baseband detect for MSM8974 variants.
    Change-Id: I7278ae708c301bd42d6eae8b364e9c49da0153a6

--- target/msm8974/init.c
+++ target/msm8974/init.c
@@ -406,9 +406,21 @@ void target_baseband_detect(struct board_data *board)
 	case MSM8974:
 	case MSM8274:
 	case MSM8674:
+	case MSM8274AA:
+	case MSM8274AB:
+	case MSM8274AC:
+	case MSM8674AA:
+	case MSM8674AB:
+	case MSM8674AC:
+	case MSM8974AA:
+	case MSM8974AB:
+	case MSM8974AC:
 		board->baseband = BASEBAND_MSM;
 		break;
 	case APQ8074:
+	case APQ8074AA:
+	case APQ8074AB:
+	case APQ8074AC:
 		board->baseband = BASEBAND_APQ;
 		break;
 	default:
