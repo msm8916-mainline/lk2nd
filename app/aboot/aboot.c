23b60d464d7ac4d2a740a0fdf957959cd72187d4
Deepa Dinamani <deepad@codeaurora.org>
2013-06-24 18:10:52 -0700

    app: aboot: Do not rely on the tampered flag to authenticate kernel.
    Modify the boot flow to not rely on tampered flag when authenticating
    kernel.
    
    CRs-Fixed: 501721
    
    Change-Id: Iac9c94606172e8855292e713a53bf2fa0f14e79e

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -696,7 +696,7 @@ int boot_linux_from_mmc(void)
 		device.is_unlocked,
 		device.is_tampered);
 
-	if(target_use_signed_kernel() && (!device.is_unlocked) && (!device.is_tampered))
+	if(target_use_signed_kernel() && (!device.is_unlocked))
 	{
 		offset = 0;
 
@@ -984,7 +984,7 @@ int boot_linux_from_flash(void)
 #endif
 
 	/* Authenticate Kernel */
-	if(target_use_signed_kernel() && (!device.is_unlocked) && (!device.is_tampered))
+	if(target_use_signed_kernel() && (!device.is_unlocked))
 	{
 		image_addr = (unsigned char *)target_get_scratch_address();
 		offset = 0;
