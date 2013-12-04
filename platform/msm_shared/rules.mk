d128325065461794f946a5c71174059923e760cb
Channagoud Kadabi <ckadabi@codeaurora.org>
2013-12-04 13:43:50 -0800

    platform: msm_shared: Enable crypto driver compilation
    Enable crypto driver for 8610 target
    
    CRs-Fixed: 580159
    Change-Id: I91c99f0535bf96157f5a4515b1d9e81c9471ed1c

--- platform/msm_shared/rules.mk
+++ platform/msm_shared/rules.mk
@@ -172,7 +172,12 @@ DEFINES += DISPLAY_TYPE_MDSS=1
             $(LOCAL_DIR)/qpic_nand.o \
             $(LOCAL_DIR)/dev_tree.o \
             $(LOCAL_DIR)/scm.o \
-            $(LOCAL_DIR)/gpio.o
+            $(LOCAL_DIR)/gpio.o \
+            $(LOCAL_DIR)/certificate.o \
+            $(LOCAL_DIR)/image_verify.o \
+            $(LOCAL_DIR)/crypto_hash.o \
+            $(LOCAL_DIR)/crypto5_eng.o \
+            $(LOCAL_DIR)/crypto5_wrapper.o
 endif
 
 ifeq ($(PLATFORM),apq8084)
