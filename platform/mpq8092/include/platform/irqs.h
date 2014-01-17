f76d252b7e74a78a2a622b028d18c043cfc3898b
Unnati Gandhi <ugandhi@codeaurora.org>
2014-01-17 14:08:34 +0530

    MPQ8092: SDHCI: Adding support for SDHCI
    Adding SDHCI support for MPQ8092 in lk.
    
    Change-Id: I363b4cab3cf13ff76e5691ca368c55c64481f1d8

--- platform/mpq8092/include/platform/irqs.h
+++ platform/mpq8092/include/platform/irqs.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -47,6 +47,8 @@
 #define USB1_HS_BAM_IRQ                        (GIC_SPI_START + 135)
 #define USB1_HS_IRQ                            (GIC_SPI_START + 134)
 
+#define SDCC1_PWRCTL_IRQ                       (GIC_SPI_START + 138)
+#define SDCC2_PWRCTL_IRQ                       (GIC_SPI_START + 221)
 /* Retrofit universal macro names */
 #define INT_USB_HS                             USB1_HS_IRQ
 
