5ab26ccc77deb9121157e51db4d18e2e1b7c7c42
Aparna Mallavarapu <aparnam@codeaurora.org>
2014-02-16 02:21:09 +0530

    platform: msm_shared: Enable Dload mode support for 8610 target.
    Change-Id: I67070b3872e42731967844c51ea2955bee5a0962

--- platform/msm_shared/rules.mk
+++ platform/msm_shared/rules.mk
@@ -195,7 +195,8 @@ DEFINES += DISPLAY_TYPE_MDSS=1
             $(LOCAL_DIR)/image_verify.o \
             $(LOCAL_DIR)/crypto_hash.o \
             $(LOCAL_DIR)/crypto5_eng.o \
-            $(LOCAL_DIR)/crypto5_wrapper.o
+            $(LOCAL_DIR)/crypto5_wrapper.o \
+            $(LOCAL_DIR)/dload_util.o
 endif
 
 ifeq ($(PLATFORM),apq8084)
