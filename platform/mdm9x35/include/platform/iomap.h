75fe3ea00fe3a581471d73bad522d1650243d6cd
Channagoud Kadabi <ckadabi@codeaurora.org>
2014-04-22 15:03:17 -0700

    platform: msm9x35: Move QMP registers to common code
    Move QMP registers to qmp header file in the common
    code layer and add platform specific qmp offset.
    
    CRs-Fixed: 636135
    Change-Id: I9c83de85b483a039fea2b5ee6f18eafc73309514

--- platform/mdm9x35/include/platform/iomap.h
+++ platform/mdm9x35/include/platform/iomap.h
@@ -165,55 +165,9 @@
 #define USB3_AUX_N                           (CLK_CTL_BASE + 0x1BCC)
 #define USB3_AUX_D                           (CLK_CTL_BASE + 0x1BD0)
 
-/* QMP register offsets */
-#define PCIE_USB3_PHY_POWER_DOWN_CONTROL     0x604
-
-#define QSERDES_COM_SYSCLK_EN_SEL_TXBAND     0x48
-#define QSERDES_COM_DEC_START1               0xA4
-#define QSERDES_COM_DEC_START2               0x104
-#define QSERDES_COM_DIV_FRAC_START1          0xF8
-#define QSERDES_COM_DIV_FRAC_START2          0xFC
-#define QSERDES_COM_DIV_FRAC_START3          0x100
-#define QSERDES_COM_PLLLOCK_CMP_EN           0x94
-#define QSERDES_COM_PLLLOCK_CMP1             0x88
-#define QSERDES_COM_PLLLOCK_CMP2             0x8C
-#define QSERDES_COM_PLL_CRCTRL               0x10C
-#define QSERDES_COM_PLL_CP_SETI              0x34
-#define QSERDES_COM_PLL_IP_SETP              0x38
-#define QSERDES_COM_PLL_CP_SETP              0x3C
-#define QSERDES_COM_PLL_IP_SETI              0x24
-#define QSERDES_COM_IE_TRIM                  0xC
-#define QSERDES_COM_IP_TRIM                  0x10
-#define QSERDES_COM_PLL_CNTRL                0x14
-#define QSERDES_RX_CDR_CONTROL1              0x400
-#define QSERDES_RX_CDR_CONTROL2              0x404
-#define QSERDES_COM_RESETSM_CNTRL            0x4C
-#define QSERDES_COM_RESETSM_CNTRL2           0x50
-#define QSERDES_COM_RES_CODE_START_SEG1      0xD8
-#define QSERDES_COM_RES_CODE_CAL_CSR         0xE0
-#define QSERDES_COM_RES_TRIM_CONTROL         0xE8
-#define QSERDES_TX_RCV_DETECT_LVL            0x268
-#define QSERDES_RX_RX_EQU_ADAPTOR_CNTRL2     0x4BC
-#define QSERDES_RX_RX_EQU_ADAPTOR_CNTRL3     0x4C0
-#define QSERDES_RX_RX_EQU_ADAPTOR_CNTRL4     0x4C4
-#define QSERDES_RX_SIGDET_ENABLES            0x4F8
-#define QSERDES_RX_SIGDET_CNTRL              0x500
-#define QSERDES_RX_SIGDET_DEGLITCH_CNTRL     0x504
-#define PCIE_USB3_PHY_RX_IDLE_DTCT_CNTRL     0x64C
-#define QSERDES_COM_SSC_EN_CENTER            0xAC
-#define QSERDES_COM_SSC_ADJ_PER1             0xB0
-#define QSERDES_COM_SSC_PER1                 0xB8
-#define QSERDES_COM_SSC_PER2                 0xBC
-#define QSERDES_COM_SSC_STEP_SIZE1           0xC0
-#define QSERDES_COM_SSC_STEP_SIZE2           0xC4
-#define PCIE_USB3_PHY_POWER_STATE_CONFIG2    0x654
-#define PCIE_USB3_PHY_RCVR_DTCT_DLY_P1U2_L   0x5C
-#define PCIE_USB3_PHY_RCVR_DTCT_DLY_P1U2_H   0x60
-#define PCIE_USB3_PHY_SW_RESET               0x600
-#define PCIE_USB3_PHY_START                  0x608
-
 /* USB3.0 Mux selector */
 #define TCSR_PHSS_USB2_PHY_SEL               0xFD4AB000
 #define PERIPH_SS_AHB2PHY_TOP_CFG            0xF9B3E010
 
+#define PLATFORM_QMP_OFFSET                  0x0
 #endif
