27ff9349c28e87d9adc87352d03eba9bfea63296
Channagoud Kadabi <ckadabi@codeaurora.org>
2014-06-16 11:19:29 -0700

    platform/target: msm8994: Use CE2 for kernel authentication
    CE2 is assigned for appsbl use, use CE2 instead of CE3
    for kernel authentication
    
    Change-Id: I3e62cefca82dfa1836b5d955c7ec7bc84618ab36

--- target/msm8994/init.c
+++ target/msm8994/init.c
@@ -55,7 +55,7 @@
 #include <boot_device.h>
 #include <qmp_phy.h>
 
-#define CE_INSTANCE             3
+#define CE_INSTANCE             2
 #define CE_EE                   1
 #define CE_FIFO_SIZE            64
 #define CE_READ_PIPE            3
@@ -424,8 +424,8 @@ void target_crypto_init_params()
 
 	/* Set up base addresses and instance. */
 	ce_params.crypto_instance  = CE_INSTANCE;
-	ce_params.crypto_base      = MSM_CE3_BASE;
-	ce_params.bam_base         = MSM_CE3_BAM_BASE;
+	ce_params.crypto_base      = MSM_CE2_BASE;
+	ce_params.bam_base         = MSM_CE2_BAM_BASE;
 
 	/* Set up BAM config. */
 	ce_params.bam_ee               = CE_EE;
