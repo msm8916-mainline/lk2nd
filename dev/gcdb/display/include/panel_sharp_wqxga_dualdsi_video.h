c0ce7ddbcb40755d21011de997f0727933deeaa7
Dhaval Patel <pdhaval@codeaurora.org>
2014-08-12 11:48:54 -0700

    dev: gcdb: fix sharp dual DSI panel configuration
    Fix panel on/off commands, DTS node information,
    back light settings and reset sequence timeout for
    sharp dual DSI panel.
    
    Change-Id: Ifcf3a3300ab1d01e66de3e294f87151c6938e3d6

--- dev/gcdb/display/include/panel_sharp_wqxga_dualdsi_video.h
+++ dev/gcdb/display/include/panel_sharp_wqxga_dualdsi_video.h
@@ -44,7 +44,7 @@
 /* Panel configuration                                                       */
 /*---------------------------------------------------------------------------*/
 static struct panel_config sharp_wqxga_dualdsi_video_panel_data = {
-	"qcom,mdss_dsi_sharp_wqxga_dualdsi_video", "dsi:0:", "qcom,mdss-dsi-panel",
+	"qcom,mdss_dsi_sharp_wqxga_video_0", "dsi:0:", "qcom,mdss-dsi-panel",
 	10, 0, "DISPLAY_1", 0, 0, 60, 0, 0, 1, 0, 0, 0, 0, 0, 25, 1, 0,
 	"qcom,mdss_dsi_sharp_wqxga_video_1"
 };
@@ -67,16 +67,16 @@ static struct color_info sharp_wqxga_dualdsi_video_color = {
 /* Panel on/off command information                                          */
 /*---------------------------------------------------------------------------*/
 static char sharp_wqxga_dualdsi_video_on_cmd0[] = {
-	0x11, 0X00, 0x05, 0x80
+	0x11, 0x00, 0x05, 0x80
 };
 
 static char sharp_wqxga_dualdsi_video_on_cmd1[] = {
-	0x29, 0x00, 0x5, 0x80
+	0x29, 0x00, 0x05, 0x80
 };
 
 static struct mipi_dsi_cmd sharp_wqxga_dualdsi_video_on_command[] = {
-	{0x4, sharp_wqxga_dualdsi_video_on_cmd0, 0xa0},
-	{0x4, sharp_wqxga_dualdsi_video_on_cmd1, 0x02}
+	{0x4, sharp_wqxga_dualdsi_video_on_cmd0, 0x0a},
+	{0x4, sharp_wqxga_dualdsi_video_on_cmd1, 0x0a}
 };
 
 #define SHARP_WQXGA_DUALDSI_VIDEO_ON_COMMAND 2
@@ -90,6 +90,12 @@ static char sharp_wqxga_dualdsi_videooff_cmd1[] = {
 	0x10, 0x00, 0x05, 0x80
 };
 
+static struct mipi_dsi_cmd sharp_wqxga_dualdsi_video_off_command[] = {
+	{0x4, sharp_wqxga_dualdsi_videooff_cmd0, 0x32},
+	{0x4, sharp_wqxga_dualdsi_videooff_cmd1, 0x78}
+};
+
+#define SHARP_WQXGA_DUALDSI_VIDEO_OFF_COMMAND 2
 
 static struct command_state sharp_wqxga_dualdsi_video_state = {
 	0, 1
@@ -131,17 +137,16 @@ static struct panel_timing sharp_wqxga_dualdsi_video_timing_info = {
 /* Panel reset sequence                                                      */
 /*---------------------------------------------------------------------------*/
 static struct panel_reset_sequence sharp_wqxga_dualdsi_video_reset_seq = {
-	{1, 0, 1, }, {2, 5, 120, }, 2
+	{1, 0, 1, }, {10, 10, 120, }, 2
 };
 
 /*---------------------------------------------------------------------------*/
 /* Backlight setting                                                         */
 /*---------------------------------------------------------------------------*/
 static struct backlight sharp_wqxga_dualdsi_video_backlight = {
-	1, 1, 4095, 100, 1, "PMIC_8941"
+	1, 1, 4095, 100, 1, "PMIC_8941"		/* BL_WLED */
 };
 
 #define SHARP_WQXGA_DUALDSI_VIDEO_SIGNATURE 0x210000
-
 
 #endif /*_PANEL_SHARP_WQXGA_DUALDSI_VIDEO_H_*/
