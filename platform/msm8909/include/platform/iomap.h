babab6d39b760e918f1cca662f7e662e2433f088
Aparna Mallavarapu <aparnam@codeaurora.org>
2014-10-16 14:32:40 -0700

    platform: msm8909: Poll PLL_MODE register for PLL_ACTIVE_FLAG for stromer PLLs.
    GPLL0 is a stromer PLL in MSM8909. Do not rely on the status register to check
    if the PLL is active, rather use PLL_MODE register to check for PLL_ACTIVE_FLAG.
    
    Change-Id: I5f1b57ee91d082b18e816eb8b8de87056b5bd095

--- platform/msm8909/include/platform/iomap.h
+++ platform/msm8909/include/platform/iomap.h
@@ -110,6 +110,7 @@
 
 /* GPLL */
 #define GPLL0_STATUS                (CLK_CTL_BASE + 0x21024)
+#define GPLL0_MODE                  (CLK_CTL_BASE + 0x21000)
 #define GPLL1_STATUS                (CLK_CTL_BASE + 0x2001C)
 #define APCS_GPLL_ENA_VOTE          (CLK_CTL_BASE + 0x45000)
 #define APCS_CLOCK_BRANCH_ENA_VOTE  (CLK_CTL_BASE + 0x45004)
