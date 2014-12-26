c32d830d3ed0ff50c87a261be3cbe986ef2cc8ab
Sandeep Panda <spanda@codeaurora.org>
2014-12-26 10:32:01 +0530

    platform: msm_shared: update MDP QOS remapper settings
    Update MDP3 QOS remapper settings based on panel
    width.
    
    Change-Id: I265e25fcfc8ae44e07dd1b1851b9589b4dea69fe

--- platform/msm_shared/mdp3.c
+++ platform/msm_shared/mdp3.c
@@ -71,7 +71,10 @@ int mdp_dsi_video_config(struct msm_panel_info *pinfo,
 	writel(0x0, MDP_DMA_P_WATERMARK_0);
 	writel(0x0, MDP_DMA_P_WATERMARK_1);
 	writel(0x0, MDP_DMA_P_WATERMARK_2);
-	writel(0xFFFF, MDP_PANIC_LUT0);
+	if (pinfo->xres >= 720)
+		writel(0xFFFF, MDP_PANIC_LUT0);
+	else
+		writel(0x00FF, MDP_PANIC_LUT0);
 	writel(0x1, MDP_PANIC_ROBUST_CTRL);
 	writel(0xFF00, MDP_ROBUST_LUT);
 
@@ -124,7 +127,10 @@ int mdp_dsi_cmd_config(struct msm_panel_info *pinfo,
 	writel(0x0, MDP_DMA_P_WATERMARK_0);
 	writel(0x0, MDP_DMA_P_WATERMARK_1);
 	writel(0x0, MDP_DMA_P_WATERMARK_2);
-	writel(0xFFFF, MDP_PANIC_LUT0);
+	if (pinfo->xres >= 720)
+		writel(0xFFFF, MDP_PANIC_LUT0);
+	else
+		writel(0x00FF, MDP_PANIC_LUT0);
 	writel(0x1, MDP_PANIC_ROBUST_CTRL);
 	writel(0xFF00, MDP_ROBUST_LUT);
 
