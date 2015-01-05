b9e73b596a20170db18944c3d3d9cf250e196f38
vijay kumar <vpendo@codeaurora.org>
2015-01-05 12:45:06 +0530

    platform: msm_shared: Add new chip ID for identification.
    Add new chip ID for MSM8x29 targets.
    
    Change-Id: I1a9818099f0c9ce82d3fd89192bb5d674891317d

--- platform/msm_shared/smem.h
+++ platform/msm_shared/smem.h
@@ -2,7 +2,7 @@
  * Copyright (c) 2009, Google Inc.
  * All rights reserved.
  *
- * Copyright (c) 2009-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2009-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -384,6 +384,9 @@ enum {
 	MDM9609  = 262,
 	MSM8239  = 263,
 	APQ8009  = 265,
+	MSM8929  = 268,
+	MSM8629  = 269,
+	MSM8229  = 270,
 };
 
 enum platform {
