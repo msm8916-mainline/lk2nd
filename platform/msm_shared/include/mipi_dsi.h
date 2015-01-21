e0a5b3d0b278608b93548001af12b7363848ad0b
Ray Zhang <rayz@codeaurora.org>
2015-01-21 10:14:09 +0800

    platform: msm_shared: add support to keep clock lane in HS
    Some targets require to force DSI clock lanes in HS mode,
    because in some corner cases clock lanes might be stuck due
    to frequent transition between HS and LP modes.
    
    Change-Id: I7001faaa041f5e67bf2c3405082e6d721c72014e
    CRs-fixed: 783723

--- platform/msm_shared/include/mipi_dsi.h
+++ platform/msm_shared/include/mipi_dsi.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -211,6 +211,7 @@ struct mipi_dsi_panel_config {
 	char mode;
 	char num_of_lanes;
 	char lane_swap;
+	char force_clk_lane_hs;
 	char pack;
 	uint8_t t_clk_pre;
 	uint8_t t_clk_post;
