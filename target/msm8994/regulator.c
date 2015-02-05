71ed5c7377a30d5134f13f7789eacb2d30b2c6d7
Sridhar Parasuram <sridhar@codeaurora.org>
2015-02-05 10:00:12 -0800

    target: msm8994: Use IPC Wrapper instead of SMD header
    With GLink and SMD support, we now have the common functions
    to talk to RPM in rpm-ipc.h
    
    Change-Id: Ia60af8cdf28371e46c9447c048190b16a2759be6

--- target/msm8994/regulator.c
+++ target/msm8994/regulator.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -28,7 +28,7 @@
  */
 
 #include <regulator.h>
-#include <rpm-smd.h>
+#include <rpm-ipc.h>
 
 static uint32_t ldo2[][11]=
 {
