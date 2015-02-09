5cd5b1154db0297249fc7569988b7027701a57e1
Vineet Bajaj <vbajaj@codeaurora.org>
2015-02-09 16:13:30 +0530

    target: msm8929: enable auto detection of panel on 8929 MTP
    This change enables auto detection of panels for 8929 MTP's.
    
    Change-Id: I9d2439cd35718d52bc6846790161446c6bfa7b92

--- target/msm8916/init.c
+++ target/msm8916/init.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -411,6 +411,7 @@ uint8_t target_panel_auto_detect_enabled()
 
 	switch(board_hardware_id()) {
 	case HW_PLATFORM_SURF:
+	case HW_PLATFORM_MTP:
 		ret = 1;
 		break;
 	default:
