1c4d7a2ca3b8bd01adf44335686bf26e374ca1df
Channagoud Kadabi <ckadabi@codeaurora.org>
2015-02-11 11:21:36 -0500

    target: fsm9900: Add FSM9916 support
    FSM9916 has chip ID 276.
    
    Change-Id: I9d3662a03851bb38c81409580b40c9bfc36ffe23

--- target/fsm9900/init.c
+++ target/fsm9900/init.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -342,6 +342,7 @@ void target_baseband_detect(struct board_data *board)
 	case FSM9915:
 	case FSM9950:
 	case FSM9955:
+	case FSM9916:
 		board->baseband = BASEBAND_MSM;
 		break;
 	default:
