ba025ece4d9fc2d0843b267dc79ea7b79ddba2c8
Channagoud Kadabi <ckadabi@codeaurora.org>
2015-02-19 15:06:33 -0800

    target: msm8994: Use pon reset for pm8994
    Use reset api added for 8994 , which resets all the pmic's present
    on the device.
    
    Change-Id: I8907ea9740b2d213e3d878960bce825e8bba71d7

--- target/msm8994/init.c
+++ target/msm8994/init.c
@@ -455,7 +455,7 @@ void reboot_device(unsigned reboot_reason)
 	else
 		reset_type = PON_PSHOLD_HARD_RESET;
 
-	pm8x41_reset_configure(reset_type);
+	pm8994_reset_configure(reset_type);
 
 	/* Drop PS_HOLD for MSM */
 	writel(0x00, MPM2_MPM_PS_HOLD);
@@ -556,7 +556,7 @@ void shutdown_device()
 	dprintf(CRITICAL, "Going down for shutdown.\n");
 
 	/* Configure PMIC for shutdown. */
-	pm8x41_reset_configure(PON_PSHOLD_SHUTDOWN);
+	pm8994_reset_configure(PON_PSHOLD_SHUTDOWN);
 
 	/* Drop PS_HOLD for MSM */
 	writel(0x00, MPM2_MPM_PS_HOLD);
