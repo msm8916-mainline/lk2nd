cb9fe03bf924e8dc22f17110e13f8e7182bc5c06
Unnati Gandhi <ugandhi@codeaurora.org>
2015-02-25 19:38:38 +0530

    platform: msm_shared: Add Changes to support verified boot on L-MR1
    Add changes to support new method of validating signed images for
    authentication, and updating the keystore, on L-MR1.
    
    Change-Id: I1329bbbb2a779f69a10159c3b63fd0b5026f074a

--- platform/msm_shared/include/boot_verifier.h
+++ platform/msm_shared/include/boot_verifier.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2015 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -59,6 +59,7 @@ DECLARE_ASN1_FUNCTIONS(AUTH_ATTR)
 typedef struct verif_boot_sig_st
 {
 	ASN1_INTEGER *version;
+	X509 *certificate;
 	X509_ALGOR *algor;
 	AUTH_ATTR *auth_attr;
 	ASN1_OCTET_STRING *sig;
