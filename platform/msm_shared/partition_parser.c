ce42d96ec7106e02d3f45247a8e91e21e9ed2763
Channagoud Kadabi <ckadabi@codeaurora.org>
2015-02-26 11:33:52 -0800

    platform: msm_shared: Avoid buffer over read
    While finding the partition entry from look up table, make sure
    that the length of the input partition matches before doing a memcmp,
    to avoid buffer overreads.
    
    Change-Id: I24b318deeb7fad9aa1ce11fd2bf3fdf6302cc892

--- platform/msm_shared/partition_parser.c
+++ platform/msm_shared/partition_parser.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2015, The Linux Foundation. All rights reserved.
 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -872,10 +872,8 @@ int partition_get_index(const char *name)
 		return INVALID_PTN;
 	}
 	for (n = 0; n < partition_count; n++) {
-		if (!memcmp
-		    (name, &partition_entries[n].name, input_string_length)
-		    && input_string_length ==
-		    strlen((const char *)&partition_entries[n].name)) {
+		if ((input_string_length == strlen((const char *)&partition_entries[n].name))
+			&& !memcmp(name, &partition_entries[n].name, input_string_length)) {
 			return n;
 		}
 	}
