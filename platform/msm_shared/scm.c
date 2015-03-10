f9248cbb41494fb84283417240452dcab4fcfe13
Aparna Mallavarapu <aparnam@codeaurora.org>
2015-03-10 23:44:36 +0530

    platform: msm_shared: Correct the SCM API response interpretation.
    Correct the SCM API Response interpretation to determine if the
    device is secure boot enabled / debug disabled or not.
    
    Change-Id: I95329833cdee9ea19506def9f782f302d2ca37bd

--- platform/msm_shared/scm.c
+++ platform/msm_shared/scm.c
@@ -1215,7 +1215,7 @@ uint32_t is_secure_boot_enable()
 	* Bit 2 - DEBUG_DISABLE_CHECK
 	*/
 	if(!ret) {
-		if((resp[0] & 0x1) || (resp[0] & 0x4))
+		if(!((resp[0] & 0x1) && (resp[0] & 0x4)))
 			ret = 1;
 	} else
 		dprintf(CRITICAL, "scm call is_secure_boot_enable failed\n");
