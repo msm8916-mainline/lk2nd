e9bdacdc224296b2cd79396a354b81f60356d2a0
Aparna Mallavarapu <aparnam@codeaurora.org>
2015-03-15 14:24:21 +0530

    platform: msm8952: Enable MMU Mapping and reading board info for 8952.
    Enable MMU mapping for the memory used in LK. This patch also
    adds support to read board info from SMEM.
    
    Change-Id: I38513d24cf88f2dcfa539188f1e4eb74ec4365a7

--- platform/msm8952/include/platform/iomap.h
+++ platform/msm8952/include/platform/iomap.h
@@ -34,6 +34,12 @@
 
 #define SDRAM_START_ADDR                   0x80000000
 
+#define DDR_START                          get_ddr_start()
+#define ABOOT_FORCE_KERNEL_ADDR            DDR_START + 0x8000
+#define ABOOT_FORCE_KERNEL64_ADDR          DDR_START + 0x80000
+#define ABOOT_FORCE_RAMDISK_ADDR           DDR_START + 0x2000000
+#define ABOOT_FORCE_TAGS_ADDR              DDR_START + 0x1E00000
+
 #define MSM_SHARED_BASE                    0x86300000
 #define MSM_SHARED_IMEM_BASE               0x08600000
 
@@ -43,6 +49,7 @@
 #define RESTART_REASON_ADDR                (MSM_SHARED_IMEM_BASE + 0x65C)
 
 #define APPS_SS_BASE                       0x0B000000
+#define APPS_SS_END                        0x0B200000
 
 #define MSM_GIC_DIST_BASE                  APPS_SS_BASE
 #define MSM_GIC_CPU_BASE                   (APPS_SS_BASE + 0x2000)
