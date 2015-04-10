bc6315e8d164369d05f6a3cc05cbf2053850792e
Aparna Mallavarapu <aparnam@codeaurora.org>
2015-04-11 04:00:43 +0530

    target/platform: Fix compilation warnings
    Fix compilation warnings to turn on Werror for MSM8952.
    
    Change-Id: I9782b9acf74aafa02f4c0e8dbd3b6e72be92a016

--- platform/msm8952/include/platform/clock.h
+++ platform/msm8952/include/platform/clock.h
@@ -41,4 +41,6 @@ void clock_config_mmc(uint32_t interface, uint32_t freq);
 void clock_config_uart_dm(uint8_t id);
 void hsusb_clock_init(void);
 void clock_config_ce(uint8_t instance);
+void clock_ce_enable(uint8_t instance);
+void clock_ce_disable(uint8_t instance);
 #endif
