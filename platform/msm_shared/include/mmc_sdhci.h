9161eb1a93392d5065a7b936856d98f81a96b242
Channagoud Kadabi <ckadabi@codeaurora.org>
2015-04-20 11:33:09 -0700

    platform: msm_shared: Add 171Mhz frequency
    Add 171Mhz frequency for sdcc1 clock.
    
    Change-Id: I1abc30d9fafadbcff27285976d99592916696356

--- platform/msm_shared/include/mmc_sdhci.h
+++ platform/msm_shared/include/mmc_sdhci.h
@@ -160,6 +160,7 @@
 #define MMC_CLK_48MHZ                             48000000
 #define MMC_CLK_50MHZ                             49152000
 #define MMC_CLK_96MHZ                             96000000
+#define MMC_CLK_171MHZ                            171430000
 #define MMC_CLK_177MHZ                            177770000
 #define MMC_CLK_200MHZ                            200000000
 #define MMC_CLK_192MHZ                            192000000
