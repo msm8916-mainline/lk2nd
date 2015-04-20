560e77c675b99719430a6898e55a4c0e1866f7cb
vijay kumar <vpendo@codeaurora.org>
2015-04-20 15:26:29 +0530

    fermium: add initial project support for fermium
    Add compilation flags support for fermium target.
    
    Change-Id: I2c7de99020cf85b95a7b452216f7df3e59ea2855

---
+++ project/mdmfermium.mk
@@ -0,0 +1,28 @@
+# top level project rules for the fermium project
+#
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+TARGET := mdmfermium
+
+MODULES += app/aboot
+
+ifeq ($(TARGET_BUILD_VARIANT),user)
+DEBUG := 0
+else
+DEBUG := 1
+endif
+
+DEFINES += WITH_DEBUG_UART=1
+DEFINES += DEVICE_TREE=1
+
+DEFINES += BAM_V170=1
+#Disable thumb mode
+ENABLE_THUMB := false
+
+#Override linker for mdm targets
+LD := $(TOOLCHAIN_PREFIX)ld.bfd
+
+ENABLE_SMD_SUPPORT := 1
+ifeq ($(ENABLE_SMD_SUPPORT),1)
+DEFINES += SMD_SUPPORT=1
+endif
