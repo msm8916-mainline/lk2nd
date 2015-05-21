7ad71165a8ccc945140bdcadb4685f28aa05f3f3
vijay kumar <vpendo@codeaurora.org>
2015-05-21 11:38:20 +0530

    include: add display fbcon function prototype
    Add the prototype for display fbcon function.
    
    Change-Id: Ic7f85034ea803d71a124548b6aee20194c8b7923

--- include/platform.h
+++ include/platform.h
@@ -49,6 +49,7 @@ addr_t get_bs_info_addr(void);
 void display_init(void);
 void display_shutdown(void);
 void display_image_on_screen(void);
+void display_fbcon_message(char *str);
 
 unsigned board_machtype(void);
 unsigned board_platform_id(void);
