cd60680782c214acfee25115d477a5210bc42cfb
Unnati Gandhi <ugandhi@codeaurora.org>
2015-06-03 12:03:24 +0530

    platform: msm_shared: Add API to unlock BAM pipes
    Add API to unlock BAM pipes used for crypto engine
    and unlock the pipes once data transfer is over.
    
    Change-Id: Ie51817ca85dc90966a312adabedccf18d8fa2edb

--- platform/msm_shared/include/crypto5_eng.h
+++ platform/msm_shared/include/crypto5_eng.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012,2015, The Linux Foundation. All rights reserved.
 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -177,5 +177,6 @@ uint32_t crypto5_get_digest(struct crypto_dev *dev,
 							crypto_auth_alg_type auth_alg);
 void crypto5_get_ctx(struct crypto_dev *dev, void *ctx_ptr);
 uint32_t crypto5_get_max_auth_blk_size(struct crypto_dev *dev);
+void crypto5_unlock_pipes(struct crypto_dev *dev);
 
 #endif
