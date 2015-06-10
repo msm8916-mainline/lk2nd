4fe906de2494eb855d6f9729e8f1c70778cf2c72
Matthew Qin <yqin@codeaurora.org>
2015-06-10 21:24:28 +0800

    dev: pmic: pm8x41: Fix compile warning
    Fix compile warning of pm8x41_vib.c
    
    Change-Id: I27fd81f7c3a015ff654623844ef3688c917802d9

--- dev/pmic/pm8x41/pm8x41_vib.c
+++ dev/pmic/pm8x41/pm8x41_vib.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -27,9 +27,7 @@
  */
 
 #include <bits.h>
-#include <debug.h>
-#include <reg.h>
-#include <pm8x41.h>
+#include <pm8x41_hw.h>
 #include <pm_vib.h>
 
 #define QPNP_VIB_EN    BIT(7)
