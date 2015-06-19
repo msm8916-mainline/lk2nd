42dc264bd992afc34f02be693ed0350e7b84e528
Parth Dixit <parthd@codeaurora.org>
2015-06-19 16:48:22 +0530

    platform: msm_shared: read power on bit for long press
    For long press feature power on reason is checked with key press only.
    In case of pmic8950 pon bit is also set along with key press bit. Add the check
    to accomodate this condition.
    
    Change-Id: Ia63fb1a254e8c5a99d3b8b413db5fb4b5f4735c4

--- platform/msm_shared/shutdown_detect.c
+++ platform/msm_shared/shutdown_detect.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -9,7 +9,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of The Linux Foundation, Inc. nor the names of its
+ *   * Neither the name of The Linux Foundation, nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -70,12 +70,16 @@ static uint32_t is_pwrkey_time_expired()
  */
 static uint32_t is_pwrkey_pon_reason()
 {
+#if PMI_CONFIGURED
+	return target_is_pwrkey_pon_reason();
+#else
 	uint8_t pon_reason = pm8x41_get_pon_reason();
 
 	if (pm8x41_get_is_cold_boot() && (pon_reason == KPDPWR_N))
 		return 1;
 	else
 		return 0;
+#endif
 }
 
 /*
