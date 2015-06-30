431dafad45ef816198b1c476dcd865e80644b3b1
Zhenhua Huang <zhenhuah@codeaurora.org>
2015-06-30 16:13:37 +0800

    app: aboot: Always enter into power off charging mode in USER build
    Skip the judgement of flag enable_charger_screen in USER build.
    
    CRs-Fixed: 855012
    Change-Id: I72a1edf82ba7b9f934833c6005f004bd51dafc3d

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -289,8 +289,8 @@ unsigned char *update_cmdline(const char * cmdline)
 		cmdline_len += strlen(loglevel);
 	} else if (boot_reason_alarm) {
 		cmdline_len += strlen(alarmboot_cmdline);
-	} else if (device.charger_screen_enabled &&
-			target_pause_for_battery_charge()) {
+	} else if ((target_build_variant_user() || device.charger_screen_enabled)
+			&& target_pause_for_battery_charge()) {
 		pause_at_bootup = 1;
 		cmdline_len += strlen(battchg_pause);
 	}
@@ -1726,11 +1726,7 @@ void read_device_info(device_info *dev)
 			info->is_unlocked = 0;
 			info->is_verified = 0;
 			info->is_tampered = 0;
-#if USER_BUILD_VARIANT
-			info->charger_screen_enabled = 1;
-#else
 			info->charger_screen_enabled = 0;
-#endif
 			write_device_info(info);
 		}
 		memcpy(dev, info, sizeof(device_info));
