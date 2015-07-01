f2fc323f7927a4f01bb0528f48b494ea4c8ca070
Ajay Singh Parmar <aparmar@codeaurora.org>
2015-07-01 12:07:59 -0700

    dev: gcdb: update compatible names for adv bridge panels
    Update compatible names of the resolution being used to common
    ones, which are being used by other modules as well. So that all
    modules can work with same compatible strings.
    
    Change-Id: I65ddab293bdb9b8a2ed71761797fb195a649558d

--- dev/gcdb/display/include/panel_adv7533_720p60.h
+++ dev/gcdb/display/include/panel_adv7533_720p60.h
@@ -38,7 +38,7 @@
 /* Panel configuration                                                       */
 /*---------------------------------------------------------------------------*/
 static struct panel_config adv7533_720p_video_panel_data = {
-	"qcom,mdss_dsi_adv7533_720p60_video", "dsi:0:", "qcom,mdss-dsi-panel",
+	"qcom,mdss_dsi_adv7533_720p", "dsi:0:", "qcom,mdss-dsi-panel",
 	10, 0, "DISPLAY_1", 0, 0, 60, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
 
