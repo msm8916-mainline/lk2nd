6143b275bcfc458377696e239c87351397703977
Tatenda Chipeperekwa <tatendac@codeaurora.org>
2015-10-28 18:21:28 -0700

    target: msm8996: provide hdmi vic in cmdline
    Add HDMI VIC (Video Identification Code) to
    cmdline so that kernel can know HDMI resolution
    configured in the bootloader, and can configure
    MDP accordingly.
    The mdss cmdline is of below format:
    mdss_mdp.panel=<lk_cfg>:<panel name>:<vic code>
    e.g.
    mdss_mdp.panel=1:hdmi:16
    where 16 represents 1080p resolution.
    
    Change-Id: I29b35a1700c417fff46c47c485e4c38347d1560c

--- target/msm8996/target_display.c
+++ target/msm8996/target_display.c
@@ -579,6 +579,7 @@ bool target_display_panel_node(char *pbuf, uint16_t buf_size)
 	int prefix_string_len = strlen(DISPLAY_CMDLINE_PREFIX);
 	bool ret = true;
 	struct oem_panel_data oem = mdss_dsi_get_oem_data();
+	char vic_buf[HDMI_VIC_LEN] = "0";
 
 	if (!strcmp(oem.panel, HDMI_PANEL_NAME)) {
 		if (buf_size < (prefix_string_len + LK_OVERRIDE_PANEL_LEN +
@@ -592,6 +593,9 @@ bool target_display_panel_node(char *pbuf, uint16_t buf_size)
 		strlcat(pbuf, LK_OVERRIDE_PANEL, buf_size);
 		buf_size -= LK_OVERRIDE_PANEL_LEN;
 		strlcat(pbuf, HDMI_CONTROLLER_STRING, buf_size);
+		buf_size -= strlen(HDMI_CONTROLLER_STRING);
+		mdss_hdmi_get_vic(vic_buf);
+		strlcat(pbuf, vic_buf, buf_size);
 	} else {
 		ret = gcdb_display_cmdline_arg(pbuf, buf_size);
 	}
