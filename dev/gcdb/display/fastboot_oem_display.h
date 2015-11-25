41431551b806b8de9d1fe39a4ca0a9c230cc31f4
Siddhartha Agrawal <agrawals@codeaurora.org>
2015-11-25 14:37:15 -0800

    dev: gcdb: display: add support for Sharp 1080p cmd selection
    Add support to select sharp 1080p command mode panel
    via oem display override command. The following
    command can be used
    
    - fastboot oem select-display-panel
    	sharp_1080p_cmd:skip
    
    Change-Id: I9a3e83fe958d3f05fea2a33f971583af13e4de99

--- dev/gcdb/display/fastboot_oem_display.h
+++ dev/gcdb/display/fastboot_oem_display.h
@@ -105,6 +105,7 @@ struct panel_lookup_list lookup_skip_panels[] = {
 	{"adv16", "qcom,mdss_dsi_adv7533_1080p", false},
 	{"adv4", "qcom,mdss_dsi_adv7533_720p", false},
 	{"nt35950_4k_dsc_cmd", "qcom,mdss_dsi_nt35950_4k_dsc_cmd", true},
+	{"sharp_1080p_cmd", "qcom,mdss_dsi_sharp_1080p_cmd", false},
 };
 
 struct sim_lookup_list lookup_sim[] = {
