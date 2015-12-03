130899bd3debd8d06a4d37e01e3d936f13c30163
Channagoud Kadabi <ckadabi@codeaurora.org>
2015-12-03 14:28:07 -0800

    platform: msm_shared: Remove writes to gic waker register
    Do not write to gic waker register from ns world, the register is
    configured from secure world.
    
    Change-Id: I02f62c5408908d0c0627a6909f8293b227add951

--- platform/msm_shared/qgic_v3.c
+++ platform/msm_shared/qgic_v3.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -76,9 +76,6 @@ void qgic_cpu_init()
 	uint32_t pmr = 0xff;
 	uint32_t eoimode = 0;
 	uint32_t grpen1 = 0x1;
-
-	/* For cpu init need to wake up the redistributor */
-	writel((readl(GICR_WAKER_CPU0) & ~GIC_WAKER_PROCESSORSLEEP), GICR_WAKER_CPU0);
 
 	/* Wait until redistributor is up */
 	while(readl(GICR_WAKER_CPU0) & GIC_WAKER_CHILDRENASLEEP)
