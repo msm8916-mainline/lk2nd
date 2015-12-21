68dc44b75b9e8ee5aca6762673cd883f1db57cdd
vijay kumar <vpendo@codeaurora.org>
2015-12-21 20:12:12 +0530

    platform: msm_shared: add support for booting from SD card
    BOOT CONFIG register value configured to 1 for booting from
    SD card. Added support for the same.
    
    Change-Id: I684b77eea08f784301665fb40cf119a8b97f7a04

--- platform/msm_shared/include/boot_device.h
+++ platform/msm_shared/include/boot_device.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014,2015 The Linux Foundation. All rights reserved.
 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -49,6 +49,7 @@ enum boot_device
 enum boot_device
 {
 	BOOT_DEFAULT=0, /* EMMC */
+	BOOT_SD=1,     /* SD card*/
 	BOOT_EMMC=2,
 	BOOT_UFS=4,
 };
