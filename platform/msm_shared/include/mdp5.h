9919248b39b97778baa6210d6dac5073c332870f
Jayant Shekhar <jshekhar@codeaurora.org>
2016-01-14 11:24:41 +0530

    platform: msm_shared: Update MDP5 for msmtitanium support
    This change updates MDP5 with the new MDSS_MDP version checks
    and adds display files for compilation.
    
    Change-Id: I368ccaf6ff6485a25e284b6478c6f0b998b56608

--- platform/msm_shared/include/mdp5.h
+++ platform/msm_shared/include/mdp5.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -94,6 +94,7 @@
 #define MDSS_MDP_HW_REV_111    MDSS_MDP_REV(1, 11, 0) /* 8956 v1.0 */
 #define MDSS_MDP_HW_REV_112    MDSS_MDP_REV(1, 12, 0) /* 8952 v1.0 */
 #define MDSS_MDP_HW_REV_114    MDSS_MDP_REV(1, 14, 0) /* 8937 v1.0 */
+#define MDSS_MDP_HW_REV_116    MDSS_MDP_REV(1, 16, 0) /* msmtitanium */
 #define MDSS_MDP_HW_REV_200    MDSS_MDP_REV(2, 0, 0) /* 8092 v1.0 */
 
 #define MDSS_MAX_LINE_BUF_WIDTH 2048
