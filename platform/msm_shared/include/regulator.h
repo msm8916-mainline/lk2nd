81d3b65ac1e7af77c4c8f0537417d17564d936a0
Jayant Shekhar <jshekhar@codeaurora.org>
2016-01-19 12:49:30 +0530

    platform: msm_shared: Add regulator settings for msmtitanium
    msmtitanium requires LDO0 and SMPS3 as part of regulator settings.
    Add these in regulator include file. Add new file for enabling
    all regulators.
    
    Change-Id: Ifc4c2794bf40aa336b5aa9671a6c522cb1c0ec76

--- platform/msm_shared/include/regulator.h
+++ platform/msm_shared/include/regulator.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -72,6 +72,8 @@
 #define REG_LDO14	BIT(13)
 #define REG_LDO17	BIT(16)
 #define REG_LDO28	BIT(27)
+#define REG_SMPS3	BIT(7)
+#define REG_LDO3	BIT(2)
 
 void regulator_enable(uint32_t enable);
 void regulator_disable(uint32_t enable);
