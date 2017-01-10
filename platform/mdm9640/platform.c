e51e137eecd40140e501538efc0010b86b27b144
Jeevan Shriram <jshriram@codeaurora.org>
2017-01-10 15:51:31 -0800

    platform: mdm9640: update platform id for sdxhedgehog
    Add new platform id for sdxhedgehog.
    
    Change-Id: Ia162d225ad0741dea06e5014679601d4481cc10c

--- platform/mdm9640/platform.c
+++ platform/mdm9640/platform.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2017, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -184,7 +184,8 @@ bool platform_is_sdxhedgehog()
 
 	switch(platform_id)
 	{
-		case SDXHEDGEHOG:
+		case SDXHEDGEHOG1:
+		case SDXHEDGEHOG2:
 				ret = true;
 				break;
 		default:
