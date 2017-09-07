79b0a5eca702539594eeff60113061ff4fffbe43
Loic Poulain <loic.poulain@linaro.org>
2017-09-07 14:56:08 +0200

    dev-tree: Support non-skales DTB if only one appended
    If people append a unique DTB which has not been generated
    with skales/dtbTool, we consider they expect to use it
    without any compatibility check/filtering.
    
    You can generate such boot.img with generic android mkbootimg tool:
    $ gzip -c Image > Image.gz
    $ cat Image.gz Image.dtb > Image.gz+dtb
    $ mkbootimg --kernel ./Image.gz+dtb --ramdisk ./initrd.img \
      --output boot.img --pagesize 4096 --base 80000000
      --cmdline "root=/dev/disk/by-partlabel/system rw rootwait console=ttyMSM0,115200n8"
    
    Change-Id: I2334b50136dcc6094fd1e58563a38f03542eadaa
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>
    (cherry picked from commit 3be1d459a546a24f2bf10b9551663a3e69a8214e)
    (cherry picked from commit 5e9adabfcfc229372b7186b3a7cc87352e69a155)

--- platform/msm_shared/dev_tree.c
+++ platform/msm_shared/dev_tree.c
@@ -1182,6 +1182,7 @@ void *dev_tree_appended(void *kernel, uint32_t kernel_size, uint32_t dtb_offset,
 	struct dt_entry_node *dt_node_tmp1 = NULL;
 	struct dt_entry_node *dt_node_tmp2 = NULL;
 	dtbo_error ret = DTBO_NOT_SUPPORTED;
+	unsigned dtb_count = 0;
 
 	if (dtb_offset)
 		app_dtb_offset = dtb_offset;
@@ -1228,6 +1229,19 @@ void *dev_tree_appended(void *kernel, uint32_t kernel_size, uint32_t dtb_offset,
 
 		/* goto the next device tree if any */
 		dtb += dtb_size;
+		dtb_count++;
+	}
+
+	if (list_is_empty(&dt_entry_queue->node) && (dtb_count == 1)) {
+		/* Special case: If we only have one DTB appended which has not
+		 * been skales-generated, consider this DTB as the one which is
+		 * expected to be used regardless ok LK compatibility check.
+		 * Force it as the bestmatch.
+		 */
+		dprintf(INFO, "Only one appended non-skales DTB, select it.\n");
+
+		bestmatch_tag = kernel + app_dtb_offset;
+		bestmatch_tag_size = dtb - (kernel + app_dtb_offset);
 	}
 
 	best_match_dt_entry = platform_dt_match_best(dt_entry_queue);
