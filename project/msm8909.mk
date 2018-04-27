29bc54ce2f3644c543be2a5ca269571b43cdcc66
Mayank Grover <groverm@codeaurora.org>
2018-04-27 11:38:39 +0530

    project: msm8909: Remove flag NO_SCM_V8_SUPPORT from rules.
    Change to remove NO_SCM_V8_SUPPORT flag from project rules,
    as to make scm version detection runtime.
    
    Change-Id: I6e45a6afcd423b3c01487bf573c802bf9a11d95e

--- project/msm8909.mk
+++ project/msm8909.mk
@@ -97,7 +97,6 @@ endif
 
 #SCM call before entering DLOAD mode
 DEFINES += PLATFORM_USE_SCM_DLOAD=1
-DEFINES += NO_SCM_V8_SUPPORT=1
 
 #Enable the external reboot functions
 ENABLE_REBOOT_MODULE := 1
