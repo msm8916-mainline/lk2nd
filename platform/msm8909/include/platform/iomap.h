497ee44ea3171333cf2a9570cd34c29e1acaccf5
Mayank Grover <groverm@codeaurora.org>
2018-04-27 11:40:52 +0530

    platform: msm8909: Support TZ app region address, based on SCM version.
    Change to dyanamically query tz app region address, based on scm
    version detected. This is required as with scmv8 tz 4.0, app region
    address and size is changed.
    
    Change-Id: I72fd0e105c9e168d4279fa7424942edb438b47b9

--- platform/msm8909/include/platform/iomap.h
+++ platform/msm8909/include/platform/iomap.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2015, 2017-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2015, 2017-2018 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
