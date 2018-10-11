88168a399d9bb1209af3c6127a90079a8d595c7b
Saranya Chidura <schidura@codeaurora.org>
2018-10-11 17:21:11 +0530

    platform: msm_shared: Remove the CACHE_LINE alignment of RPMB response buffer
    RPMB response buffer is not needed to be cache aligned as the RPMB request packet
    which is having RPMB frame at req_buff_offset, is also not cache aligned. This
    change is added unnecessarily in the commit 4c12e92a93396f79bb8cebf15e551017ec611ce9.
    
    Change-Id: I3f6b9952c6b76e7783648fe50db6cf289086fbbc
    Signed-off-by: Saranya Chidura <schidura@codeaurora.org>

--- platform/msm_shared/rpmb/rpmb_listener.c
+++ platform/msm_shared/rpmb/rpmb_listener.c
@@ -108,7 +108,7 @@ static void handle_rw_request(void *buf, uint32_t sz)
 	struct tz_rpmb_rw_req *req_p = (struct tz_rpmb_rw_req *)buf;
 	struct tz_rpmb_rw_resp *resp_p = NULL;
 	uint32_t *req_buf = buf + req_p->req_buff_offset;
-	uint32_t *resp_buf = buf + ROUNDUP(sizeof(struct tz_rpmb_rw_resp), CACHE_LINE);
+	uint32_t *resp_buf = buf + sizeof(struct tz_rpmb_rw_resp);
 
 	resp_p = (struct tz_rpmb_rw_resp *) buf;
 
@@ -133,7 +133,7 @@ static void handle_rw_request(void *buf, uint32_t sz)
 			ASSERT(0);
 	};
 
-	resp_p->res_buff_offset = ROUNDUP(sizeof(struct tz_rpmb_rw_resp), CACHE_LINE);
+	resp_p->res_buff_offset = sizeof(struct tz_rpmb_rw_resp);
 	resp_p->cmd_id = req_p->cmd_id;
 }
 
