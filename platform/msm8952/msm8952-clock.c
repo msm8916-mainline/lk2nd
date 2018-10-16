78c59638d01187e7c37b954c05870b8ed817b75e
lijuang <lijuang@codeaurora.org>
2018-10-16 18:27:48 +0800

    platform: msm8952: create a new api to get platform info for qm215
    The old api is used across the lk by multiple clients, so create a
    new api for easily handled by other drivers.
    
    Change-Id: I21f5ea3b9e75a726b44ad25b80f7ced0a4806228

--- platform/msm8952/msm8952-clock.c
+++ platform/msm8952/msm8952-clock.c
@@ -651,7 +651,8 @@ void platform_clock_init(void)
 	}
 
 	if (platform_is_msm8937() || platform_is_msm8917() ||
-		platform_is_sdm429() || platform_is_sdm439())
+		platform_is_sdm429() || platform_is_sdm439() ||
+		platform_is_qm215())
 		msm8937_clock_override();
 
 	clk_init(msm_clocks_8952, ARRAY_SIZE(msm_clocks_8952));
--- platform/msm8952/platform.c
+++ platform/msm8952/platform.c
@@ -214,6 +214,22 @@ int platform_is_msm8917()
 		case MSM8217:
 		case MSM8617:
 		case APQ8017:
+			ret = 1;
+			break;
+		default:
+			ret = 0;
+	};
+
+	return ret;
+}
+
+int platform_is_qm215()
+{
+	uint32_t platform = board_platform_id();
+	uint32_t ret = 0;
+
+	switch(platform)
+	{
 		case QM215:
 			ret = 1;
 			break;
@@ -321,7 +337,8 @@ int platform_is_msm8956()
 uint32_t platform_get_tz_app_add()
 {
 	if(platform_is_msm8937() || platform_is_msm8917() ||
-		platform_is_sdm429() || platform_is_sdm439())
+		platform_is_sdm429() || platform_is_sdm439() ||
+		platform_is_qm215())
 		return APP_REGION_ADDR_8937;
 	else
 		return APP_REGION_ADDR_8952;
@@ -330,7 +347,8 @@ uint32_t platform_get_tz_app_add()
 uint32_t platform_get_tz_app_size()
 {
 	if(platform_is_msm8937() || platform_is_msm8917() ||
-		platform_is_sdm429() || platform_is_sdm439())
+		platform_is_sdm429() || platform_is_sdm439() ||
+		platform_is_qm215())
 		return APP_REGION_SIZE_8937;
 	else
 		return APP_REGION_SIZE_8952;
@@ -338,7 +356,7 @@ uint32_t platform_get_tz_app_size()
 
 uint32_t platform_get_apcs_ipc_base()
 {
-	if(platform_is_msm8917() || platform_is_sdm429())
+	if(platform_is_msm8917() || platform_is_sdm429() || platform_is_qm215())
 		return APCS_ALIAS1_IPC_INTERRUPT_1;
 	else
 		return APCS_ALIAS0_IPC_INTERRUPT_2;
