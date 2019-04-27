ab56d41faa68b11602a9346256f8ad4aae1ec250
lambdadroid <lambdadroid@gmail.com>
2019-04-27 21:22:10 +0200

    Add build scripts

---
+++ make.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -eu
+TARGET="msm8916-secondary"
+
+rm -rf "build-$TARGET"
+
+make -j6 TOOLCHAIN_PREFIX=arm-none-eabi- "$TARGET"
+
+cd "build-$TARGET"
+../../skales/mkbootimg \
+    --kernel=lk.bin \
+    --ramdisk=/dev/null \
+    --dt=../../dt.img \
+    --base=0x80000000 \
+    --output=boot.img \
+    --cmdline="lk2"
---
+++ project/msm8916-secondary.mk
@@ -0,0 +1,20 @@
+# top level project rules for the msm8916 project
+#
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+# Enable fastboot display menu
+ENABLE_FBCON_DISPLAY_MSG := 1
+
+include $(LOCAL_DIR)/msm8916.mk
+
+# Use maximum verbosity
+DEBUG := 2
+DEFINES += LK_LOG_BUF_SIZE=16384
+
+# Avoid writing device info
+DEFINES += SAFE_MODE=1
+# Display as unlocked by default
+DEFINES += DEFAULT_UNLOCK=1
+
+# Use continuous splash from primary bootloader for display
+DISPLAY_USE_CONTINUOUS_SPLASH := 1
