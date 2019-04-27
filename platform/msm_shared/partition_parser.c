f106c86bbbd17bb989e84c47c5a8ba1e885d0a06
lambdadroid <lambdadroid@gmail.com>
2019-04-27 21:21:29 +0200

    platform: partition_parser: Offset boot partition by 1 MB
    TODO: Make this conditional

--- platform/msm_shared/partition_parser.c
+++ platform/msm_shared/partition_parser.c
@@ -87,6 +87,15 @@ static unsigned partition_count;
 /* this is a pointer to ptn_entries_buffer */
 static unsigned char *new_buffer = NULL;
 
+void partition_offset_boot(uint32_t block_size)
+{
+	int index = partition_get_index("boot");
+	int offset = (1 * 1024 * 1024) / block_size;
+
+	partition_entries[index].first_lba += offset;
+	partition_entries[index].size -= offset;
+}
+
 unsigned int partition_read_table()
 {
 	unsigned int ret;
@@ -116,6 +125,8 @@ unsigned int partition_read_table()
 			return 1;
 		}
 	}
+
+	partition_offset_boot(block_size);
 	return 0;
 }
 
