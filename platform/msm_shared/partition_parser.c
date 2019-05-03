03c8ac2d0f014efa1013a9dd576d0545f1f94e33
lambdadroid <lambdadroid@gmail.com>
2019-05-03 15:25:13 +0200

    platform: partition_parser: Add emulated "lk" partition
    This allows flashing the secondary lk directly from the lk
    fastboot screen instead of going through the primary flashing
    process.

--- platform/msm_shared/partition_parser.c
+++ platform/msm_shared/partition_parser.c
@@ -96,13 +96,35 @@ struct partition_entry* partition_get_partition_entries()
 	return partition_entries;
 }
 
-void partition_offset_boot(uint32_t block_size)
+void partition_split_boot(uint32_t block_size)
 {
+	struct partition_entry *boot;
 	int index = partition_get_index("boot");
-	int offset = (1 * 1024 * 1024) / block_size;
+	unsigned long long lk_size = (1 * 1024 * 1024) / block_size;
 
-	partition_entries[index].first_lba += offset;
-	partition_entries[index].size -= offset;
+	if (index == INVALID_PTN) {
+		dprintf(CRITICAL, "Boot partition not found\n");
+		return;
+	}
+	boot = &partition_entries[index];
+
+	if (boot->size < lk_size) {
+		dprintf(CRITICAL, "Boot partition has not enough space for secondary lk\n");
+		return;
+	}
+
+	if (partition_count < NUM_PARTITIONS) {
+		struct partition_entry *lk = &partition_entries[partition_count++];
+		memcpy(lk, boot, sizeof(*lk));
+		strcpy(lk->name, "lk");
+		lk->last_lba = lk->first_lba + lk_size - 1;
+		lk->size = lk_size;
+	} else {
+		dprintf(INFO, "Too many partitions to add virtual 'lk' partition\n");
+	}
+
+	boot->first_lba += lk_size;
+	boot->size -= lk_size;
 }
 
 unsigned int partition_read_table()
@@ -138,7 +160,7 @@ unsigned int partition_read_table()
 	/* TODO: Move this to mmc_boot_read_gpt() */
 	partition_scan_for_multislot();
 
-	partition_offset_boot(block_size);
+	partition_split_boot(block_size);
 	return 0;
 }
 
