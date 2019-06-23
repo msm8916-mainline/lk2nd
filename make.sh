d48c3e32c4d6d55536fce3ff7df0d14497c3dadf
Mis012 <michael.srba@seznam.cz>
2019-06-23 14:49:02 +0200

    edit make.sh

--- make.sh
+++ make.sh
@@ -2,15 +2,22 @@
 set -eu
 TARGET="msm8916-secondary"
 
-rm -rf "build-$TARGET"
+#rm -rf "build-$TARGET"
 
-make -j6 TOOLCHAIN_PREFIX=arm-none-eabi- "$TARGET"
+make -j8 TOOLCHAIN_PREFIX=arm-none-eabi- "$TARGET"
 
 cd "build-$TARGET"
-../scripts/mkbootimg \
-    --kernel=lk.bin \
-    --ramdisk=/dev/null \
-    --dt=../../dt.img \
-    --base=0x80000000 \
-    --output=boot.img \
-    --cmdline="lk2nd"
+
+sudo cp lk.bin ~/.local/var/pmbootstrap/chroot_native/tmp/mainline/lk.bin
+
+pmbootstrap chroot -- mkbootimg-osm0sis \
+--kernel "/tmp/mainline/lk.bin" \
+--dt "/tmp/mainline/dt.img" \
+--base "0x80000000" \
+--second_offset "0x00f00000" \
+--kernel_offset "0x00008000" \
+--tags_offset "0x01e00000" \
+--pagesize "2048" \
+--cmdline "lk2nd" \
+-o "/tmp/mainline/aboot.img"
+
