f1e9469df87f986d0eba792c56c87c47cf22fbf8
lambdadroid <lambdadroid@gmail.com>
2019-07-11 18:59:43 +0200

    fastboot-lk2nd: Add "fastboot oem lk_log"
    Usage: fastboot oem lk_log && fastboot get_staged /dev/stdout
    (cherry picked from commit 5a57c14fd71565c8f646da9d13974fcdbb27520e)

--- app/aboot/fastboot-lk2nd.c
+++ app/aboot/fastboot-lk2nd.c
@@ -1,4 +1,15 @@
+#include <debug.h>
 #include "fastboot.h"
 
+#if WITH_DEBUG_LOG_BUF
+static void cmd_oem_lk_log(const char *arg, void *data, unsigned sz)
+{
+	fastboot_stage(lk_log_getbuf(), lk_log_getsize());
+}
+#endif
+
 void fastboot_lk2nd_register_commands(void) {
+#if WITH_DEBUG_LOG_BUF
+	fastboot_register("oem lk_log", cmd_oem_lk_log);
+#endif
 }
