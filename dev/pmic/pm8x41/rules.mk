5a8b7cd3fbb33a49d54d8dcdaf9767808678d371
lambdadroid <lambdadroid@gmail.com>
2019-07-15 19:32:34 +0200

    Add "fastboot oem dump-regulators" to dump current regulator state

--- dev/pmic/pm8x41/rules.mk
+++ dev/pmic/pm8x41/rules.mk
@@ -5,7 +5,8 @@ INCLUDES += -I$(LOCAL_DIR)/include
 OBJS += \
 	$(LOCAL_DIR)/pm8x41.o \
 	$(LOCAL_DIR)/pm8x41_adc.o \
-	$(LOCAL_DIR)/pm8x41_wled.o
+	$(LOCAL_DIR)/pm8x41_wled.o \
+	$(LOCAL_DIR)/pm8x41_regulator.o
 
 ifeq ($(ENABLE_PON_VIB_SUPPORT),true)
 OBJS += \
