5d09efff95cd78c70aa3f4cac529e2925629bb85
Sorixelle <srxl@protonmail.com>
2019-10-11 22:45:01 +1100

    smem: Default to unknown platform if hardware id is out of range

--- platform/msm_shared/smem.c
+++ platform/msm_shared/smem.c
@@ -200,6 +200,9 @@ size_t smem_get_hw_platform_name(void *buf, uint32_t buf_size)
 	}
 
 	hw_id = board_hardware_id();
+	if (hw_id >= ARRAY_SIZE(hw_platform))
+		hw_id = HW_PLATFORM_UNKNOWN;
+
 	if (buf_size < strlen(hw_platform[hw_id]) + 1)
 		return 1;
 
