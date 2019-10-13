57d798c39fcbd4f2823b6cc5c6a045cfd667fd0a
lambdadroid <lambdadroid@gmail.com>
2019-10-13 21:10:07 +0200

    app: aboot: fastboot-lk2nd: Add "fastboot oem cmdline"
    Usage: fastboot oem cmdline && fastboot get_staged /dev/stdout
    
    since cmdline tends to be longer than the 64 character limit
    imposed by fastboot.

--- app/aboot/fastboot-lk2nd.c
+++ app/aboot/fastboot-lk2nd.c
@@ -1,5 +1,7 @@
+#include <string.h>
 #include <debug.h>
 #include <pm8x41_regulator.h>
+#include <lk2nd-device.h>
 #include "fastboot.h"
 
 static void cmd_oem_dump_regulators(const char *arg, void *data, unsigned sz)
@@ -22,9 +24,17 @@ static void cmd_oem_lk_log(const char *arg, void *data, unsigned sz)
 }
 #endif
 
+static void cmd_oem_cmdline(const char *arg, void *data, unsigned sz)
+{
+	fastboot_stage(lk2nd_dev.cmdline, strlen(lk2nd_dev.cmdline));
+}
+
 void fastboot_lk2nd_register_commands(void) {
 	fastboot_register("oem dump-regulators", cmd_oem_dump_regulators);
 #if WITH_DEBUG_LOG_BUF
 	fastboot_register("oem lk_log", cmd_oem_lk_log);
 #endif
+
+	if (lk2nd_dev.cmdline)
+		fastboot_register("oem cmdline", cmd_oem_cmdline);
 }
