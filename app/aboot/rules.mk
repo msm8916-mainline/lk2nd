5863586699905ac039f2e9a3812e364fafa5bd8e
lambdadroid <lambdadroid@gmail.com>
2019-11-22 21:11:05 +0100

    lk2nd: Add Samsung MUIC reset workaround

--- app/aboot/rules.mk
+++ app/aboot/rules.mk
@@ -11,6 +11,7 @@ OBJS += \
 	$(LOCAL_DIR)/fastboot.o \
 	$(LOCAL_DIR)/fastboot-lk2nd.o \
 	$(LOCAL_DIR)/lk2nd-device.o \
+	$(LOCAL_DIR)/lk2nd-samsung.o \
 	$(LOCAL_DIR)/recovery.o
 
 ifeq ($(ENABLE_UNITTEST_FW), 1)
@@ -30,3 +31,7 @@ OBJS += \
 	$(LOCAL_DIR)/mdtp_defs.o \
 	$(LOCAL_DIR)/mdtp_fs.o
 endif
+
+# lk2nd-samsung.c
+GPIO_I2C_BUS_COUNT := 1
+MODULES += dev/gpio_i2c
