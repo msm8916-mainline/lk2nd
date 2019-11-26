b2697abba794ecbcfd540b7ebabdd7b869fea783
lambdadroid <lambdadroid@gmail.com>
2019-11-26 22:35:34 +0100

    dts: Add separate msm8916-samsung.dts for each device revision
    Some Samsung devices exist in multiple revisions: Those may have
    hardware differences, although they are usually only used during
    early development at Samsung. Therefore it is important that the
    device tree for the correct device revision is used.
    
    When lk2nd attempts to boot downstream kernels it emulates the
    behavior of the original Samsung bootloader by patching the real
    qcom,board-id of the device to the one the original bootloader has
    matched.
    
    However, it detects this by taking the first qcom,board-id in the
    device tree that it is given - therefore this does not work when
    multiple board IDs are given like currently in msm8916-samsung.dts.
    To make this work properly we have to split it up per device revision.
    
    An advantage of this is that we only pretend to support the device
    revision that was really tested (i.e. usually the latest). Other
    device revisions should usually not be picked up by the bootloader
    anymore.

--- dts/rules.mk
+++ dts/rules.mk
@@ -2,8 +2,10 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 ifeq ($(PROJECT), msm8916-secondary)
 DTBS += \
-	$(LOCAL_DIR)/apq8016-samsung.dtb \
+	$(LOCAL_DIR)/apq8016-samsung-r01.dtb \
 	$(LOCAL_DIR)/msm8916-longcheer-l8150.dtb \
 	$(LOCAL_DIR)/msm8916-motorola-harpia.dtb \
-	$(LOCAL_DIR)/msm8916-samsung.dtb
+	$(LOCAL_DIR)/msm8916-samsung-r01.dtb \
+	$(LOCAL_DIR)/msm8916-samsung-r03.dtb \
+	$(LOCAL_DIR)/msm8916-samsung-r04.dtb
 endif
