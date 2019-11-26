ff8ce7cc73488eb4e159c85acec5bf2fd4d45bb4
lambdadroid <lambdadroid@gmail.com>
2019-11-26 22:45:18 +0100

    dts: apq8016-samsung: Move gt510wifi to rev07
    The gt510wifi we support really has device revision 7,
    it just also accepted device trees for older revisions.
    Let's not pretend to support the older versions.

--- dts/rules.mk
+++ dts/rules.mk
@@ -2,7 +2,7 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 ifeq ($(PROJECT), msm8916-secondary)
 DTBS += \
-	$(LOCAL_DIR)/apq8016-samsung-r01.dtb \
+	$(LOCAL_DIR)/apq8016-samsung-r07.dtb \
 	$(LOCAL_DIR)/msm8916-longcheer-l8150.dtb \
 	$(LOCAL_DIR)/msm8916-motorola-harpia.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r01.dtb \
