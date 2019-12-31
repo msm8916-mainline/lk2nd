946a6c8dd20728a761bfebac9c0402eb29240aaf
Armen <armen138@gmail.com>
2019-12-31 09:07:46 -0500

    dts: Add Galaxy Grand Prime (SM-G530W)

--- dts/rules.mk
+++ dts/rules.mk
@@ -6,6 +6,7 @@ DTBS += \
 	$(LOCAL_DIR)/msm8916-longcheer-l8150.dtb \
 	$(LOCAL_DIR)/msm8916-motorola-harpia.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r01.dtb \
+	$(LOCAL_DIR)/msm8916-samsung-r02.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r03.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r04.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r06.dtb
