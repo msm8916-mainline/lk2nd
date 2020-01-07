b678fb917ea42f32cb932bf2942bc09fd1004253
lijuang <lijuang@codeaurora.org>
2020-01-07 10:45:51 +0800

    target: msm8996: Fix the compilation error
    Fix the compilation error - "target/msm8996/init.c:457:2: error:
    implicit declaration of function 'pm_appsbl_charging_in_progress'".
    
    Change-Id: I628cd657b61772bc79cd03caab1d040e145c8f2a

--- target/msm8996/init.c
+++ target/msm8996/init.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2018 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2018, 2020 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -63,6 +63,7 @@
 #include <rpm-glink.h>
 #if ENABLE_WBC
 #include <pm_app_smbchg.h>
+#include <pm_smbchg_common.h>
 #endif
 
 #if LONG_PRESS_POWER_ON
