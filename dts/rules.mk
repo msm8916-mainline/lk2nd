23ee1064c782228f3042424d74c91ad33bcba735
Minecrell <minecrell@minecrell.net>
2020-02-10 18:49:37 +0100

    dts: Add device tree for Marshall London / msm8916-mtp
    Use hw.id=500 at end of cmdline to detect if the msm8916-mtp
    is really a Marshall London. Not sure if this is really unique,
    but we will see I guess...

--- dts/rules.mk
+++ dts/rules.mk
@@ -5,6 +5,7 @@ DTBS += \
 	$(LOCAL_DIR)/apq8016-samsung-r07.dtb \
 	$(LOCAL_DIR)/msm8916-longcheer-l8150.dtb \
 	$(LOCAL_DIR)/msm8916-motorola-harpia.dtb \
+	$(LOCAL_DIR)/msm8916-mtp.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r01.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r03.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r04.dtb \
