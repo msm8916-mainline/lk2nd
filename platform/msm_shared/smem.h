b19b242857becfd327e0c48217007ce821b3c802
Minecrell <minecrell@minecrell.net>
2020-02-12 10:43:07 +0100

    smem: Add function to allocate and write to SMEM

--- platform/msm_shared/smem.h
+++ platform/msm_shared/smem.h
@@ -558,6 +558,7 @@ typedef enum {
 
 /* Note: buf MUST be 4byte aligned, and max_len MUST be a multiple of 4. */
 unsigned smem_read_alloc_entry(smem_mem_type_t type, void *buf, int max_len);
+unsigned smem_alloc_write_entry(smem_mem_type_t type, void *buf, unsigned size);
 
 /* SMEM RAM Partition */
 enum {
