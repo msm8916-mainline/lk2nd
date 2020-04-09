27e3b85bdda9426141eebacb6699a9938ee124e4
Minecrell <minecrell@minecrell.net>
2020-04-09 10:38:23 +0200

    target: display_cont_splash_mdp5: detect cmd mode and flush
    Like the original code, the cmd mode flush code is borrowed from efidroid:
    https://github.com/efidroid/bootloader_lk/tree/common/shared/lib/2ndstage_display
    
    I added dynamic detection of command mode additionally.

--- target/target_display_cont_splash_mdp5.c
+++ target/target_display_cont_splash_mdp5.c
@@ -1,46 +1,73 @@
+#include <arch/defines.h>
+#include <bits.h>
 #include <debug.h>
 #include <reg.h>
 #include <string.h>
+#include <target.h>
+
+#include <dev/fbcon.h>
 #include <mdp5.h>
 #include <platform.h>
-#include <dev/fbcon.h>
-#include <target/display.h>
+#include <platform/iomap.h>
+#include <platform/timer.h>
 
-#define PIPE_BASE  MDP_VP_0_RGB_0_BASE
+/* Assume ctl/pipe 0 is used */
+#define MDP_CTL_BASE			MDP_CTL_0_BASE
+#define MDP_PIPE_BASE			MDP_VP_0_RGB_0_BASE
+
+#define MDP_CTL_TOP_MODE_SEL_CMD	BIT(17)
 
 static struct fbcon_config fb = {
 	.bpp    = 24,
 	.format = FB_FORMAT_RGB888,
 };
 
+static struct msm_panel_info dummy_panel = {
+	.pipe_type = MDSS_MDP_PIPE_TYPE_RGB,
+};
+
 extern int check_aboot_addr_range_overlap(uintptr_t start, uint32_t size);
 extern int check_ddr_addr_range_bound(uintptr_t start, uint32_t size);
 
-static int mdss_read_config(struct fbcon_config *fb)
+static void mdp5_cmd_mode_flush(void)
+{
+	mdp_dma_on(&dummy_panel);
+	dsb();
+	mdelay(20);
+}
+
+static int mdp5_read_config(struct fbcon_config *fb)
 {
 	uint32_t stride, src_size, out_size, src_xy, out_xy, size;
+	bool cmd_mode;
 
-	fb->base = (void*) readl(PIPE_BASE + PIPE_SSPP_SRC0_ADDR);
+	fb->base = (void*) readl(MDP_PIPE_BASE + PIPE_SSPP_SRC0_ADDR);
 	if (!fb->base) {
 		dprintf(CRITICAL, "Continuous splash does not appear to be enabled\n");
 		return -1;
 	}
 
-	stride = readl(PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE);
-	src_size = readl(PIPE_BASE + PIPE_SSPP_SRC_IMG_SIZE);
-	out_size = readl(PIPE_BASE + PIPE_SSPP_SRC_OUT_SIZE);
-	src_xy = readl(PIPE_BASE + PIPE_SSPP_SRC_XY);
-	out_xy = readl(PIPE_BASE + PIPE_SSPP_OUT_XY);
+	stride = readl(MDP_PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE);
+	src_size = readl(MDP_PIPE_BASE + PIPE_SSPP_SRC_IMG_SIZE);
+	out_size = readl(MDP_PIPE_BASE + PIPE_SSPP_SRC_OUT_SIZE);
+	src_xy = readl(MDP_PIPE_BASE + PIPE_SSPP_SRC_XY);
+	out_xy = readl(MDP_PIPE_BASE + PIPE_SSPP_OUT_XY);
+	cmd_mode = !!(readl(MDP_CTL_BASE + CTL_TOP) & MDP_CTL_TOP_MODE_SEL_CMD);
 
 	dprintf(SPEW, "Continuous splash detected: base: %p, stride: %d, "
-		"source: %dx%d (%d,%d), output: %dx%d (%d,%d)\n", fb->base, stride,
+		"source: %dx%d (%d,%d), output: %dx%d (%d,%d) (cmd mode: %d)\n",
+		fb->base, stride,
 		src_size & 0xffff, src_size >> 16, src_xy & 0xffff, src_xy >> 16,
-		out_size & 0xffff, out_size >> 16, out_xy & 0xffff, out_xy >> 16
+		out_size & 0xffff, out_size >> 16, out_xy & 0xffff, out_xy >> 16,
+		cmd_mode
 	);
 
 	fb->stride = stride / (fb->bpp/8);
 	fb->width = fb->stride;
 	fb->height = out_size >> 16;
+
+	if (cmd_mode)
+		fb->update_start = mdp5_cmd_mode_flush;
 
 	// Validate parameters
 	if (fb->stride == 0 || fb->width == 0 || fb->height == 0) {
@@ -71,7 +98,7 @@ void target_display_init(const char *panel_name)
 {
 	dprintf(SPEW, "Panel: %s\n", panel_name);
 
-	if (mdss_read_config(&fb))
+	if (mdp5_read_config(&fb))
 		return;
 
 	// Setup framebuffer
