45b04b071b5adc19dac7a7d95a0b6aeda57674eb
Minecrell <minecrell@minecrell.net>
2020-04-09 11:48:21 +0200

    dts: Make msm8916-longcheer-l8150.dts generic, add bq-paella
    Every Longcheer L8150 needs to be explicitly added as extra device
    node now (e.g. seed would have previously matched as Longcheer L8150,
    now it needs its own device node).

--- dts/rules.mk
+++ dts/rules.mk
@@ -4,9 +4,9 @@ ifeq ($(PROJECT), msm8916-secondary)
 DTBS += \
 	$(LOCAL_DIR)/apq8016-samsung-r02.dtb \
 	$(LOCAL_DIR)/apq8016-samsung-r07.dtb \
-	$(LOCAL_DIR)/msm8916-longcheer-l8150.dtb \
 	$(LOCAL_DIR)/msm8916-motorola-harpia.dtb \
 	$(LOCAL_DIR)/msm8916-mtp.dtb \
+	$(LOCAL_DIR)/msm8916-qrd9-v1.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r01.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r03.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r04.dtb \
