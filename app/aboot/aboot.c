0bbd2ebebe1f31ffe0a60cc8334498efdcc6dde4
Nikita Travkin <nikitos.tr@gmail.com>
2020-07-17 17:26:07 +0500

    app: aboot: Use HOME button to enter fastboot mode

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -3960,10 +3960,10 @@ void aboot_init(const struct app_descriptor *app)
 	}
 	if (!boot_into_fastboot)
 	{
-		if (keys_get_state(KEY_HOME) || keys_get_state(KEY_VOLUMEUP))
+		if (keys_get_state(KEY_VOLUMEUP))
 			boot_into_recovery = 1;
 		if (!boot_into_recovery &&
-			(keys_get_state(KEY_BACK) || keys_get_state(KEY_VOLUMEDOWN)))
+			(keys_get_state(KEY_HOME) || keys_get_state(KEY_BACK) || keys_get_state(KEY_VOLUMEDOWN)))
 			boot_into_fastboot = true;
 	}
 	#if NO_KEYPAD_DRIVER
