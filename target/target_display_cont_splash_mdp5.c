866b87283f608a1b8720a4b5d94709e94ea0a45d
Minecrell <minecrell@minecrell.net>
2020-10-11 17:58:52 +0200

    target: target_display_cont_splash_mdp5: Add support for VIG/DMA pipes
    Those are really weird but some devices seemingly use that.
    As long as it works...

--- target/target_display_cont_splash_mdp5.c
+++ target/target_display_cont_splash_mdp5.c
@@ -11,9 +11,8 @@
 #include <platform/iomap.h>
 #include <platform/timer.h>
 
-/* Assume ctl/pipe 0 is used */
+/* Assume ctl 0 is used */
 #define MDP_CTL_BASE			MDP_CTL_0_BASE
-#define MDP_PIPE_BASE			MDP_VP_0_RGB_0_BASE
 
 #define MDP_CTL_TOP_MODE_SEL_CMD	BIT(17)
 
@@ -22,9 +21,30 @@ static struct fbcon_config fb = {
 	.format = FB_FORMAT_RGB888,
 };
 
-static struct msm_panel_info dummy_panel = {
-	.pipe_type = MDSS_MDP_PIPE_TYPE_RGB,
+struct pipe {
+	unsigned int base;
+	uint32_t type;
 };
+
+/*
+ * VIG/DMA pipes are weird, but some devices use that...
+ * Might need more changes to make them work properly.
+ */
+static const struct pipe pipes[] = {
+	{
+		.base = MDP_VP_0_RGB_0_BASE,
+		.type = MDSS_MDP_PIPE_TYPE_RGB,
+	},
+	{
+		.base = MDP_VP_0_VIG_0_BASE,
+		.type = MDSS_MDP_PIPE_TYPE_VIG,
+	},
+	{
+		.base = MDP_VP_0_DMA_0_BASE,
+		.type = MDSS_MDP_PIPE_TYPE_DMA,
+	},
+};
+static struct msm_panel_info dummy_panel = {};
 
 extern int check_aboot_addr_range_overlap(uintptr_t start, uint32_t size);
 extern int check_ddr_addr_range_bound(uintptr_t start, uint32_t size);
@@ -38,25 +58,30 @@ static void mdp5_cmd_mode_flush(void)
 
 static int mdp5_read_config(struct fbcon_config *fb)
 {
+	const struct pipe *pipe = pipes, *pipe_end = pipe + ARRAY_SIZE(pipes);
 	uint32_t stride, src_size, out_size, src_xy, out_xy, size;
 	bool cmd_mode;
 
-	fb->base = (void*) readl(MDP_PIPE_BASE + PIPE_SSPP_SRC0_ADDR);
-	if (!fb->base) {
+	for (; pipe < pipe_end; pipe++) {
+		fb->base = (void*) readl(pipe->base + PIPE_SSPP_SRC0_ADDR);
+		if (fb->base)
+			break;
+	}
+	if (pipe == pipe_end) {
 		dprintf(CRITICAL, "Continuous splash does not appear to be enabled\n");
 		return -1;
 	}
 
-	stride = readl(MDP_PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE);
-	src_size = readl(MDP_PIPE_BASE + PIPE_SSPP_SRC_IMG_SIZE);
-	out_size = readl(MDP_PIPE_BASE + PIPE_SSPP_SRC_OUT_SIZE);
-	src_xy = readl(MDP_PIPE_BASE + PIPE_SSPP_SRC_XY);
-	out_xy = readl(MDP_PIPE_BASE + PIPE_SSPP_OUT_XY);
+	stride = readl(pipe->base + PIPE_SSPP_SRC_YSTRIDE);
+	src_size = readl(pipe->base + PIPE_SSPP_SRC_IMG_SIZE);
+	out_size = readl(pipe->base + PIPE_SSPP_SRC_OUT_SIZE);
+	src_xy = readl(pipe->base + PIPE_SSPP_SRC_XY);
+	out_xy = readl(pipe->base + PIPE_SSPP_OUT_XY);
 	cmd_mode = !!(readl(MDP_CTL_BASE + CTL_TOP) & MDP_CTL_TOP_MODE_SEL_CMD);
 
-	dprintf(SPEW, "Continuous splash detected: base: %p, stride: %d, "
+	dprintf(SPEW, "Continuous splash detected: pipe: %d, base: %p, stride: %d, "
 		"source: %dx%d (%d,%d), output: %dx%d (%d,%d) (cmd mode: %d)\n",
-		fb->base, stride,
+		pipe->type, fb->base, stride,
 		src_size & 0xffff, src_size >> 16, src_xy & 0xffff, src_xy >> 16,
 		out_size & 0xffff, out_size >> 16, out_xy & 0xffff, out_xy >> 16,
 		cmd_mode
@@ -66,8 +91,10 @@ static int mdp5_read_config(struct fbcon_config *fb)
 	fb->width = fb->stride;
 	fb->height = out_size >> 16;
 
-	if (cmd_mode)
+	if (cmd_mode) {
+		dummy_panel.pipe_type = pipe->type;
 		fb->update_start = mdp5_cmd_mode_flush;
+	}
 
 	// Validate parameters
 	if (fb->stride == 0 || fb->width == 0 || fb->height == 0) {
