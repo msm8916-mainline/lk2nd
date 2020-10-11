6309c37559fdc32e89285c1bb74b824e0b4f49c8
Minecrell <minecrell@minecrell.net>
2020-10-11 17:54:20 +0200

    project: Split out lk2nd configuration into lk2nd.mk
    Prepare for adding MSM8226 and MSM8974 setup by sharing some common
    options. Yay!

---
+++ project/lk2nd.mk
@@ -0,0 +1,31 @@
+# Use maximum verbosity
+DEBUG := 2
+DEFINES += LK_LOG_BUF_SIZE=16384
+
+# Avoid writing device info
+DEFINES += SAFE_MODE=1
+# Display as unlocked by default
+DEFINES += DEFAULT_UNLOCK=1
+
+# Use continuous splash from primary bootloader for display
+DISPLAY_USE_CONTINUOUS_SPLASH := 1
+
+# Enable fastboot display menu
+ENABLE_FBCON_DISPLAY_MSG := 1
+
+# Android boot image
+OUTBOOTIMG := $(BUILDDIR)/lk2nd.img
+OUTODINTAR := $(BUILDDIR)/lk2nd.tar
+ANDROID_BOOT_CMDLINE := lk2nd
+
+# Set in platform specific makefile (might need to change ANDROID_BOOT_BASE):
+#APPSBOOTHEADER: $(OUTBOOTIMG) $(OUTODINTAR)
+#ANDROID_BOOT_BASE := 0x80000000
+
+# Some old platforms are missing the proper defines
+ifeq ($(ENABLE_FBCON_DISPLAY_MSG),1)
+DEFINES += FBCON_DISPLAY_MSG=1
+endif
+ifeq ($(EMMC_BOOT),1)
+DEFINES += _EMMC_BOOT=1
+endif
--- project/msm8916-secondary.mk
+++ project/msm8916-secondary.mk
@@ -1,26 +1,7 @@
-# top level project rules for the msm8916 project
-#
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
-OUTBOOTIMG := $(BUILDDIR)/lk2nd.img
-OUTODINTAR := $(BUILDDIR)/lk2nd.tar
-
-# Enable fastboot display menu
-ENABLE_FBCON_DISPLAY_MSG := 1
-
 include $(LOCAL_DIR)/msm8916.mk
-
-# Use maximum verbosity
-DEBUG := 2
-DEFINES += LK_LOG_BUF_SIZE=16384
-
-# Avoid writing device info
-DEFINES += SAFE_MODE=1
-# Display as unlocked by default
-DEFINES += DEFAULT_UNLOCK=1
-
-# Use continuous splash from primary bootloader for display
-DISPLAY_USE_CONTINUOUS_SPLASH := 1
+include $(LOCAL_DIR)/lk2nd.mk
 
 # NOTE! Typically you don't need this when porting lk2nd to other platforms.
 # Right now this is only used in workarounds for some MSM8916 Samsung devices.
@@ -29,4 +10,3 @@ GPIO_I2C_BUS_COUNT := 1
 
 APPSBOOTHEADER: $(OUTBOOTIMG) $(OUTODINTAR)
 ANDROID_BOOT_BASE := 0x80000000
-ANDROID_BOOT_CMDLINE := lk2nd
