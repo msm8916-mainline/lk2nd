abaa7ab47b810fb5b91166c63a620c54c632d372
Minecrell <minecrell@minecrell.net>
2020-10-11 17:58:19 +0200

    project: Add makefile for MSM8226 and MSM8974
    Still need to add some device trees later for a proper Android boot
    image.

---
+++ project/msm8974-secondary.mk
@@ -0,0 +1,8 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+include $(LOCAL_DIR)/msm8974.mk
+include $(LOCAL_DIR)/lk2nd.mk
+
+# TODO: Configure Android boot image after adding some device trees
+#APPSBOOTHEADER: $(OUTBOOTIMG) $(OUTODINTAR)
+#ANDROID_BOOT_BASE := 0x80000000
