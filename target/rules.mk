9763281b94bdf28cdd17bb99f91fd14a905b42d4
Minecrell <minecrell@minecrell.net>
2020-10-11 16:14:02 +0200

    target: Automatically filter out original display when using splash
    This avoids having to edit target/$(TARGET)/rules.mk for every SoC.

--- target/rules.mk
+++ target/rules.mk
@@ -5,6 +5,8 @@ OBJS += \
 	$(LOCAL_DIR)/target_display.o
 
 ifeq ($(DISPLAY_USE_CONTINUOUS_SPLASH),1)
+# Filter out original display implementation
+OBJS := $(filter-out target/$(TARGET)/target_display.o target/$(TARGET)/oem_panel.o, $(OBJS))
 ifneq ($(filter $(DEFINES),DISPLAY_TYPE_MDSS=1),)
     OBJS += $(LOCAL_DIR)/target_display_cont_splash_mdp5.o
 else
