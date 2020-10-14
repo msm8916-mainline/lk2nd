f706cef6549c96983813b73ebaac102f13cbe77c
Martijn Braam <martijn@brixit.nl>
2020-10-14 12:56:28 +0200

    dts: motorola-osprey: Add support for the Motorola Moto G 2015

--- dts/rules.mk
+++ dts/rules.mk
@@ -9,6 +9,7 @@ DTBS += \
 	$(LOCAL_DIR)/msm8916-lg.dtb \
 	$(LOCAL_DIR)/msm8916-motorola-harpia-p1b-4d.dtb \
 	$(LOCAL_DIR)/msm8916-motorola-harpia-p1b-4e.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-osprey.dtb \
 	$(LOCAL_DIR)/msm8916-motorola-surnia.dtb \
 	$(LOCAL_DIR)/msm8916-mtp.dtb \
 	$(LOCAL_DIR)/msm8916-qrd9-v1.dtb \
