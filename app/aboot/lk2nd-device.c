904bfbf6e278ab36b271e275870aa921264340bb
Minecrell <minecrell@minecrell.net>
2021-01-29 19:07:36 +0100

    lk2nd-device: Also fixup board hlos subtype if necessary

--- app/aboot/lk2nd-device.c
+++ app/aboot/lk2nd-device.c
@@ -37,6 +37,7 @@ static void update_board_id(struct board_id *board_id)
 {
 	uint32_t hw_id = board_id->variant_id & 0xff;
 	uint32_t hw_subtype = board_id->platform_subtype & 0xff;
+	uint32_t hlos_subtype = board_id->platform_subtype & 0x700;
 	uint32_t target_id = board_id->variant_id & 0xffff00;
 
 	/* Some Huawei devices seem to use hw_id > 0xff, match entire variant */
@@ -54,6 +55,13 @@ static void update_board_id(struct board_id *board_id)
 		dprintf(INFO, "Updating board hardware subtype: 0x%x -> 0x%x\n",
 			board_hardware_subtype(), hw_subtype);
 		board.platform_subtype = hw_subtype;
+	}
+
+	if ((board_hlos_subtype() & 0x700) != hlos_subtype) {
+		hlos_subtype |= board_hlos_subtype() & ~0x700;
+		dprintf(INFO, "Updating board hlos subtype: 0x%x -> 0x%x\n",
+			board_hlos_subtype(), hlos_subtype);
+		board.platform_hlos_subtype = hlos_subtype;
 	}
 
 	if (!(target_id <= (board_target_id() & 0xffff00))) {
