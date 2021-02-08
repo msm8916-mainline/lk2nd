d60bdbc824a86bce740805a5004ff6ba371e54b0
Minecrell <minecrell@minecrell.net>
2021-02-08 13:16:15 +0100

    lk2nd: Add initial stub for qcom battery detection
    This is incomplete, just to show the idea how to integrate it and
    how to read the BATT_ID channel from lk2nd.

--- lk2nd/smb1360/smb1360.h
+++ lk2nd/smb1360/smb1360.h
@@ -3,8 +3,15 @@
 #ifndef __LK2ND_SMB1360_H
 #define __LK2ND_SMB1360_H
 
+enum smb1360_battery_profile {
+	SMB1360_BATTERY_PROFILE_KEEP,
+	SMB1360_BATTERY_PROFILE_A,
+	SMB1360_BATTERY_PROFILE_B,
+};
+
 struct smb1360_battery {
 	const char *name;
+	enum smb1360_battery_profile profile;
 	uint16_t capacity_mah;
 	uint16_t cc_soc_coeff;
 	uint16_t therm_coeff;
@@ -14,5 +21,6 @@ void smb1360_detect_battery(const void *fdt, int offset);
 void smb1360_update_device_tree(void *fdt);
 
 const struct smb1360_battery *smb1360_wt88047_detect_battery(const void *fdt, int offset);
+const struct smb1360_battery *smb1360_qcom_detect_battery(const void *fdt, int offset);
 
 #endif
