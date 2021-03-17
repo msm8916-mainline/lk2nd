9776e654dc402fcfdab2f5ff24bcaf9840607a6d
Minecrell <minecrell@minecrell.net>
2021-03-17 15:24:08 +0100

    app: aboot: fastboot: Simplify fastboot_stage() a bit
    I don't think the cache invalidate is really necessary.
    Also, avoid copying if giving in the download_base as address
    (in that case we assume it was already copied there).

--- app/aboot/fastboot.c
+++ app/aboot/fastboot.c
@@ -406,15 +406,14 @@ void fastboot_okay(const char *info)
 
 void fastboot_stage(const void *data, unsigned sz)
 {
-	arch_invalidate_cache_range((addr_t) download_base, download_size);
 	download_size = 0;
-
 	if (sz > download_max) {
 		fastboot_fail("data too large");
 		return;
 	}
 
-	memcpy(download_base, data, sz);
+	if (data != download_base)
+		memcpy(download_base, data, sz);
 	download_size = sz;
 
 	fastboot_okay("");
