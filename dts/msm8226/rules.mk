038c1216c2e5d102c4a2246f9e241c9b93a738a9
Luca Weiss <luca@z3ntu.xyz>
2021-04-27 20:13:26 +0200

    msm8226: Add support for LG G Watch R

---
+++ dts/msm8226/rules.mk
@@ -0,0 +1,4 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+DTBS += \
+	$(LOCAL_DIR)/apq8026-lge-lenok.dtb
--- project/msm8226-secondary.mk
+++ project/msm8226-secondary.mk
@@ -3,6 +3,5 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 include $(LOCAL_DIR)/msm8226.mk
 include $(LOCAL_DIR)/lk2nd.mk
 
-# TODO: Configure Android boot image after adding some device trees
-#APPSBOOTHEADER: $(OUTBOOTIMG) $(OUTODINTAR)
-#ANDROID_BOOT_BASE := 0x80000000
+APPSBOOTHEADER: $(OUTBOOTIMGADTB)
+ANDROID_BOOT_BASE := 0x00000000
