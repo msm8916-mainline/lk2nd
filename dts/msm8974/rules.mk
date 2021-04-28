27ca6e4098a04f2ad48dbc2dc539a5db5dcd9799
Luca Weiss <luca@z3ntu.xyz>
2021-04-28 21:59:28 +0200

    dts: msm8974: Add support for Fairphone 2

--- dts/msm8974/rules.mk
+++ dts/msm8974/rules.mk
@@ -3,4 +3,5 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 DTBS += \
 	$(LOCAL_DIR)/msm8974-lge-d855.dtb \
 	$(LOCAL_DIR)/msm8974-lge-hammerhead.dtb \
-	$(LOCAL_DIR)/msm8974-samsung-klte.dtb
+	$(LOCAL_DIR)/msm8974-samsung-klte.dtb \
+	$(LOCAL_DIR)/msm8974pro-ab-pm8941-mtp.dtb
