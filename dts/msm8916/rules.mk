1f51756b27ff3d160b8c4313d8a3e915782d053b
SUNGOLDSV <suraajvashisht@gmail.com>
2021-06-03 15:01:29 +0530

    dts: Add support for LYF Wind 1 (lyf-ls5010)

--- dts/msm8916/rules.mk
+++ dts/msm8916/rules.mk
@@ -15,6 +15,7 @@ DTBS += \
 	$(LOCAL_DIR)/msm8916-motorola-surnia.dtb \
 	$(LOCAL_DIR)/msm8916-mtp.dtb \
 	$(LOCAL_DIR)/msm8916-mtp-smb1360.dtb \
+	$(LOCAL_DIR)/msm8916-qrd5-v1.dtb \
 	$(LOCAL_DIR)/msm8916-qrd7+12-v1.dtb \
 	$(LOCAL_DIR)/msm8916-qrd8-v1.dtb \
 	$(LOCAL_DIR)/msm8916-qrd9-v1.dtb \
