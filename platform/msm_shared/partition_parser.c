712d90ad7602f9b80c1c4e5d75a22fda0304d220
Minecrell <minecrell@minecrell.net>
2021-06-09 22:40:19 +0200

    partition_parser: Only split qhypstub partition on MSM8916

--- platform/msm_shared/partition_parser.c
+++ platform/msm_shared/partition_parser.c
@@ -140,7 +140,9 @@ static void partition_split(uint32_t block_size, const char *base_name,
 static void partition_split_boot(uint32_t block_size)
 {
 	partition_split(block_size, "boot", "lk2nd", LK2ND_SIZE / block_size, false);
+#if TARGET_MSM8916
 	partition_split(block_size, "lk2nd", "qhypstub", 4096 / block_size, true);
+#endif
 }
 #endif
 
