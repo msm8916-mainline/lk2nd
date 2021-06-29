8bb410f4b9afa894263b0db496427b7e1e7b0dab
Minecrell <minecrell@minecrell.net>
2021-06-29 18:17:02 +0200

    app: aboot: fastboot-extra: Add "fastboot oem dump <partition>"
    This can be used to dump a partition from lk2nd. Usage:
    "fastboot oem dump <partition> && fastboot get_staged <part>.img"

--- app/aboot/fastboot-extra.c
+++ app/aboot/fastboot-extra.c
@@ -1,11 +1,37 @@
 #include <arch/ops.h>
 #include <debug.h>
 #include <dev/fbcon.h>
+#include <mmc.h>
+#include <partition_parser.h>
 #include <platform.h>
 #include <platform/iomap.h>
 #include <pm8x41_regulator.h>
 #include <reg.h>
+#include <target.h>
 #include "fastboot.h"
+
+static void cmd_oem_dump_partition(const char *arg, void *data, unsigned sz)
+{
+	struct partition_info part = partition_get_info(arg);
+	int ret;
+
+	if (!part.offset) {
+		fastboot_fail("partition not found");
+		return;
+	}
+	if (part.size > target_get_max_flash_size()) {
+		fastboot_fail("partition too large");
+		return;
+	}
+
+	if (mmc_read(part.offset, data, part.size)) {
+		fastboot_fail("failed to read partition");
+		return;
+	}
+
+	fastboot_stage(data, part.size);
+	fastboot_okay("");
+}
 
 #if WITH_DEBUG_LOG_BUF
 static void cmd_oem_lk_log(const char *arg, void *data, unsigned sz)
@@ -107,6 +133,8 @@ static void cmd_oem_dump_saw2(const char *arg, void *data, unsigned sz)
 #endif
 
 void fastboot_extra_register_commands(void) {
+	fastboot_register("oem dump", cmd_oem_dump_partition);
+
 #if WITH_DEBUG_LOG_BUF
 	fastboot_register("oem lk_log", cmd_oem_lk_log);
 #endif
