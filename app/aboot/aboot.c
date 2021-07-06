faa7cc41d093cfbd3db9dd61a370ab932fc5c4a3
Nikita Travkin <nikita@trvn.ru>
2021-07-06 12:12:42 +0500

    aboot: Add a compile time option to force fastboot mode

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -4006,6 +4006,11 @@ void aboot_init(const struct app_descriptor *app)
 #endif
 #endif
 
+#if LK2ND_FORCE_FASTBOOT
+	boot_into_fastboot = true;
+	dprintf(INFO, "Fastboot mode was forced with compile-time flag.\n");
+#endif
+
 normal_boot:
 	if (!boot_into_fastboot)
 	{
