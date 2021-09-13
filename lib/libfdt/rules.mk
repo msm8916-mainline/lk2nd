df2c529c0b3c490c9d6819e13c17a80c16e307e1
Minecrell <minecrell@minecrell.net>
2021-09-13 21:39:22 +0200

    lib: libfdt: Import libfdt v1.6.1
    From: https://git.kernel.org/pub/scm/utils/dtc/dtc.git tag v1.6.1
    subdirectory libfdt

--- lib/libfdt/rules.mk
+++ lib/libfdt/rules.mk
@@ -1,9 +1,5 @@
 LOCAL_PATH := $(GET_LOCAL_DIR)
 
-LIBFDT_INCLUDES = fdt.h libfdt.h
-LIBFDT_SRCS = fdt.c fdt_ro.c fdt_wip.c fdt_sw.c fdt_rw.c fdt_strerror.c
-LIBFDT_OBJS = $(LIBFDT_SRCS:%.c=%.o)
-
-INCLUDES += -I$(LOCAL_PATH)
-
+include $(LOCAL_PATH)/Makefile.libfdt
 OBJS += $(addprefix $(LOCAL_PATH)/, $(LIBFDT_OBJS))
+INCLUDES += -I$(LOCAL_PATH)
