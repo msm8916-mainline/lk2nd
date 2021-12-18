fa9f37645c29fbdb4f29583c964990e67f263670
Lin, Meng-Bo <linmengbo0689@protonmail.com>
2021-12-18 18:38:10 +0900

    dts: Add Samsung Galaxy A7 2015 (SM-A700YD)
    lk2nd,smd-rpm-hack-opening; is required to boot mainline.
    Otherwise the screen goes pitch black and leaves no sign of further booting.

--- dts/msm8916/rules.mk
+++ dts/msm8916/rules.mk
@@ -37,4 +37,5 @@ DTBS += \
 	$(LOCAL_DIR)/msm8939-asus-z00t.dtb \
 	$(LOCAL_DIR)/msm8939-huawei-kiwi.dtb \
 	$(LOCAL_DIR)/msm8939-mtp.dtb \
-	$(LOCAL_DIR)/msm8939-qrd-skuk.dtb
+	$(LOCAL_DIR)/msm8939-qrd-skuk.dtb \
+	$(LOCAL_DIR)/msm8939-samsung-r01.dtb
