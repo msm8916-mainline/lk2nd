e79f7baeb330de1941145fc86f83fc823dbdd5a4
Nikita Travkin <nikita@trvn.ru>
2021-12-20 21:36:46 +0500

    lib/partition: allow rootfs partition to be scanned for subpartitions

--- lib/partition/partition.c
+++ lib/partition/partition.c
@@ -251,7 +251,9 @@ int partition_publish(const char *device, off_t offset)
 
 				/* Some linux distros make use of subpartitions.
 				 * Scan some devices recursively to publish them. */
-				if (strcmp(name, "system") == 0 || strcmp(name, "userdata") == 0)
+				if (strcmp(name, "system") == 0
+				    || strcmp(name, "userdata") == 0
+				    || strcmp(name, "rootfs") == 0)
 					partition_publish(subdevice, 0);
 
 				count++;
