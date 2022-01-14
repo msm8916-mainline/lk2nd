c5b3e905d80837b9557a85ce01ba76abf8757b2d
Minecrell <minecrell@minecrell.net>
2022-01-14 22:41:21 +0100

    lk2nd: panel: generated: Add boe_otm8019a_5p0_fwvga_video for huawei-y635

--- lk2nd/panel/generated/panels.h
+++ lk2nd/panel/generated/panels.h
@@ -3,6 +3,7 @@
 #ifndef _LK2ND_GENERATED_PANELS_H_
 #define _LK2ND_GENERATED_PANELS_H_
 
+#include "lk_panel_boe_otm8019a_5p0_fwvga_video.h"
 #include "lk_panel_r61308_720p_video.h"
 #include "lk_panel_yushun_nt35520_720p_cmd.h"
 
