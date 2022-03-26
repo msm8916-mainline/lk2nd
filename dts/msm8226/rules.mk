8e944e06c4e40db72f3afc1a0f7d882dc86603d5
anShigenta <60007738+anShigenta@users.noreply.github.com>
2022-03-26 18:49:29 +0300

    Add support for Samsung Galaxy Grand 2

--- dts/msm8226/rules.mk
+++ dts/msm8226/rules.mk
@@ -3,5 +3,5 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 DTBS += \
 	$(LOCAL_DIR)/apq8026-asus-sparrow.dtb \
 	$(LOCAL_DIR)/apq8026-huawei-sturgeon.dtb \
-	$(LOCAL_DIR)/msm8226-samsung-ms013g.dtb \
-	$(LOCAL_DIR)/apq8026-lg-lenok.dtb
+	$(LOCAL_DIR)/apq8026-lg-lenok.dtb \
+	$(LOCAL_DIR)/msm8226-samsung-ms013g.dtb
