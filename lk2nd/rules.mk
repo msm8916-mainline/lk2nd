7f273211ff0caf7439933c922a1e0e5dbd55a901
Minecrell <minecrell@minecrell.net>
2022-04-10 17:11:49 +0200

    lk2nd: Add new universal continuous splash implementation
    Has support for mdp3/mdp4/mdp5, can automatically detect bpp/format
    and also autorefresh now. Various cleanup.

--- lk2nd/rules.mk
+++ lk2nd/rules.mk
@@ -5,12 +5,14 @@ ifneq ($(LK1ST_PANEL),)
 OBJS := $(filter-out target/$(TARGET)/oem_panel.o, $(OBJS))
 MODULES += lk2nd/panel
 CFLAGS += -DLK1ST_PANEL=$(LK1ST_PANEL)
-else ifeq ($(DISPLAY_USE_CONTINUOUS_SPLASH),1)
+else
 # Filter out original display implementation
 OBJS := $(filter-out target/$(TARGET)/target_display.o target/$(TARGET)/oem_panel.o, $(OBJS))
-ifneq ($(filter $(DEFINES),DISPLAY_TYPE_MDSS=1),)
-    OBJS += $(LOCAL_DIR)/target_display_cont_splash_mdp5.o
-else
-    $(error Continuous splash display is not supported for the current target)
+ifeq ($(DISPLAY_USE_CONTINUOUS_SPLASH),1)
+DEFINES += \
+	$(if $(filter %/mdp3.o,$(OBJS)), MDP3=1) \
+	$(if $(filter %/mdp4.o,$(OBJS)), MDP4=1) \
+	$(if $(filter %/mdp5.o,$(OBJS)), MDP5=1)
+OBJS += $(LOCAL_DIR)/target_display_cont_splash.o
 endif
 endif
