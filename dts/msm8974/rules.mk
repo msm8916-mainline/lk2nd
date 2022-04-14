e0fd8784899160913768191c943ffd8da533be3d
Rongrong <15956627+Rongronggg9@users.noreply.github.com>
2022-04-14 23:39:13 +0800

    dts: msm8974: new device kltechn (SM-G9006V/W)

--- dts/msm8974/rules.mk
+++ dts/msm8974/rules.mk
@@ -3,5 +3,6 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 DTBS += \
 	$(LOCAL_DIR)/msm8974-lge-d855.dtb \
 	$(LOCAL_DIR)/msm8974-lge-hammerhead.dtb \
+	$(LOCAL_DIR)/msm8974-samsung-r10.dtb \
 	$(LOCAL_DIR)/msm8974-samsung-r14.dtb \
 	$(LOCAL_DIR)/msm8974pro-ab-pm8941-mtp.dtb
