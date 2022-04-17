c115fdc53d62a55591417fdc73aac3b93b36dc6c
Minecrell <minecrell@minecrell.net>
2022-04-17 19:20:40 +0200

    drone: Add gcc10 runs

--- .drone.sh
+++ .drone.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-2.0-only
-DIR=$(dirname "$0")
 set -eux
 
 nproc && grep Mem /proc/meminfo && df -hT .
-apk add build-base dtc dtc-dev gcc-arm-none-eabi git python3 tar
+apk add build-base dtc dtc-dev git python3 tar
 git clean -dxf
 
 # Workaround problem with faccessat2() on Drone CI
@@ -12,8 +11,16 @@ wget https://gist.githubusercontent.com/TravMurav/36c83efbc188115aa9b0fc7f4afba6
 gcc -O2 -shared -o /opt/faccessat.so /opt/faccessat.c
 export LD_PRELOAD=/opt/faccessat.so
 
-wget https://gitlab.com/postmarketOS/pmbootstrap/raw/master/pmb/data/keys/build.postmarketos.org.rsa.pub -P /etc/apk/keys
-echo https://mirror.postmarketos.org/postmarketos/v21.12 >> /etc/apk/repositories
-apk add gcc4-armv7
+case "$DRONE_STEP_NAME" in
+*gcc4*)
+	wget https://gitlab.com/postmarketOS/pmbootstrap/raw/master/pmb/data/keys/build.postmarketos.org.rsa.pub -P /etc/apk/keys
+	echo https://mirror.postmarketos.org/postmarketos/v21.12 >> /etc/apk/repositories
+	apk add gcc4-armv7
+	TOOLCHAIN_PREFIX=gcc4-armv7-alpine-linux-musleabihf-
+	;;
+*)
+	apk add gcc-arm-none-eabi
+	TOOLCHAIN_PREFIX=arm-none-eabi-
+esac
 
-make -j$(nproc) TOOLCHAIN_PREFIX=gcc4-armv7-alpine-linux-musleabihf- "$@"
+exec make -j$(nproc) TOOLCHAIN_PREFIX=$TOOLCHAIN_PREFIX "$@"
