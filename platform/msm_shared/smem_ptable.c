b3d283d7448cb2a5bb23e2d98d6b6471db4aa0fe
Stephan Gerhold <stephan@gerhold.net>
2022-04-17 12:12:31 +0200

    platform: msm_shared: smem_ptable: Ensure lowest SDRAM comes first
    Looks like on some older devices the SMEM RAM partition table is not
    ordered properly, the higher SDRAM part comes before the lower one.
    This confuses at least get_ddr_start(), so swap those two if needed.

--- platform/msm_shared/smem_ptable.c
+++ platform/msm_shared/smem_ptable.c
@@ -217,6 +217,14 @@ static void smem_copy_ram_ptable(void *buf)
 			ptable.parts[pentry].num_partitions = table_v0->parts[pentry].num_partitions;
 		}
 
+		/* Make sure lowest SDRAM address comes first, for get_ddr_start() */
+		if (smem_ram_ptn_is_ddr(&ptable.parts[0]) &&
+		    smem_ram_ptn_is_ddr(&ptable.parts[1]) &&
+		    ptable.parts[0].start > ptable.parts[1].start) {
+			ram_partition tmp = ptable.parts[0];
+			ptable.parts[0] = ptable.parts[1];
+			ptable.parts[1] = tmp;
+		}
 	}
 	else
 	{
