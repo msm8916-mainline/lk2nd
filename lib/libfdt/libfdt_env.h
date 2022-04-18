5acc065b04ab27ced9b1facaef75718fcbbfbfad
Minecrell <minecrell@minecrell.net>
2022-04-18 11:28:53 +0200

    lib: libfdt: Import libfdt v1.6.1
    From https://git.kernel.org/pub/scm/utils/dtc/dtc.git tag v1.6.1
    subdirectory libfdt

--- lib/libfdt/libfdt_env.h
+++ lib/libfdt/libfdt_env.h
@@ -1,134 +1,96 @@
-#ifndef _LIBFDT_ENV_H
-#define _LIBFDT_ENV_H
+/* SPDX-License-Identifier: (GPL-2.0-or-later OR BSD-2-Clause) */
+#ifndef LIBFDT_ENV_H
+#define LIBFDT_ENV_H
+/*
+ * libfdt - Flat Device Tree manipulation
+ * Copyright (C) 2006 David Gibson, IBM Corporation.
+ * Copyright 2012 Kim Phillips, Freescale Semiconductor.
+ */
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
-#include <km_main.h>
+#include <limits.h>
 
-typedef uint32_t fdt32_t;
+#ifdef __CHECKER__
+#define FDT_FORCE __attribute__((force))
+#define FDT_BITWISE __attribute__((bitwise))
+#else
+#define FDT_FORCE
+#define FDT_BITWISE
+#endif
 
-#define EXTRACT_BYTE(n)	((unsigned long long)((uint8_t *)&x)[n])
-static inline uint16_t fdt16_to_cpu(uint16_t x)
+typedef uint16_t FDT_BITWISE fdt16_t;
+typedef uint32_t FDT_BITWISE fdt32_t;
+typedef uint64_t FDT_BITWISE fdt64_t;
+
+#define EXTRACT_BYTE(x, n)	((unsigned long long)((uint8_t *)&x)[n])
+#define CPU_TO_FDT16(x) ((EXTRACT_BYTE(x, 0) << 8) | EXTRACT_BYTE(x, 1))
+#define CPU_TO_FDT32(x) ((EXTRACT_BYTE(x, 0) << 24) | (EXTRACT_BYTE(x, 1) << 16) | \
+			 (EXTRACT_BYTE(x, 2) << 8) | EXTRACT_BYTE(x, 3))
+#define CPU_TO_FDT64(x) ((EXTRACT_BYTE(x, 0) << 56) | (EXTRACT_BYTE(x, 1) << 48) | \
+			 (EXTRACT_BYTE(x, 2) << 40) | (EXTRACT_BYTE(x, 3) << 32) | \
+			 (EXTRACT_BYTE(x, 4) << 24) | (EXTRACT_BYTE(x, 5) << 16) | \
+			 (EXTRACT_BYTE(x, 6) << 8) | EXTRACT_BYTE(x, 7))
+
+static inline uint16_t fdt16_to_cpu(fdt16_t x)
 {
-	return (EXTRACT_BYTE(0) << 8) | EXTRACT_BYTE(1);
+	return (FDT_FORCE uint16_t)CPU_TO_FDT16(x);
 }
-#define cpu_to_fdt16(x) fdt16_to_cpu(x)
-
-static inline uint32_t fdt32_to_cpu(uint32_t x)
+static inline fdt16_t cpu_to_fdt16(uint16_t x)
 {
-	return (EXTRACT_BYTE(0) << 24) | (EXTRACT_BYTE(1) << 16) | (EXTRACT_BYTE(2) << 8) | EXTRACT_BYTE(3);
+	return (FDT_FORCE fdt16_t)CPU_TO_FDT16(x);
 }
-#define cpu_to_fdt32(x) fdt32_to_cpu(x)
 
-static inline uint64_t fdt64_to_cpu(uint64_t x)
+static inline uint32_t fdt32_to_cpu(fdt32_t x)
 {
-	return (EXTRACT_BYTE(0) << 56) | (EXTRACT_BYTE(1) << 48) | (EXTRACT_BYTE(2) << 40) | (EXTRACT_BYTE(3) << 32)
-		| (EXTRACT_BYTE(4) << 24) | (EXTRACT_BYTE(5) << 16) | (EXTRACT_BYTE(6) << 8) | EXTRACT_BYTE(7);
+	return (FDT_FORCE uint32_t)CPU_TO_FDT32(x);
 }
-#define cpu_to_fdt64(x) fdt64_to_cpu(x)
+static inline fdt32_t cpu_to_fdt32(uint32_t x)
+{
+	return (FDT_FORCE fdt32_t)CPU_TO_FDT32(x);
+}
+
+static inline uint64_t fdt64_to_cpu(fdt64_t x)
+{
+	return (FDT_FORCE uint64_t)CPU_TO_FDT64(x);
+}
+static inline fdt64_t cpu_to_fdt64(uint64_t x)
+{
+	return (FDT_FORCE fdt64_t)CPU_TO_FDT64(x);
+}
+#undef CPU_TO_FDT64
+#undef CPU_TO_FDT32
+#undef CPU_TO_FDT16
 #undef EXTRACT_BYTE
 
-#define toupper(a)  ((((a) >= 'a') && ((a) <= 'z')) ? ((a) - 'a' + 'A') : (a))
-#define isalpha(chr) (('a' <= chr && chr <= 'z') || ('A' <= chr && chr <= 'Z'))
+#ifdef __APPLE__
+#include <AvailabilityMacros.h>
 
-static bool isalnum(unsigned char Chr)
+/* strnlen() is not available on Mac OS < 10.7 */
+# if !defined(MAC_OS_X_VERSION_10_7) || (MAC_OS_X_VERSION_MAX_ALLOWED < \
+                                         MAC_OS_X_VERSION_10_7)
+
+#define strnlen fdt_strnlen
+
+/*
+ * fdt_strnlen: returns the length of a string or max_count - which ever is
+ * smallest.
+ * Input 1 string: the string whose size is to be determined
+ * Input 2 max_count: the maximum value returned by this function
+ * Output: length of the string or max_count (the smallest of the two)
+ */
+static inline size_t fdt_strnlen(const char *string, size_t max_count)
 {
-        return (('0' <= Chr && Chr <= '9') ||
-                ('A' <= Chr && Chr <= 'Z') ||
-                ('a' <= Chr && Chr <= 'z')
-                );
+    const char *p = memchr(string, 0, max_count);
+    return p ? p - string : max_count;
 }
 
-static int Digit2Val( int c)
-{
-        if(isalpha(c)) {  /* If c is one of [A-Za-z]... */
-                c = toupper(c) - 7;   // Adjust so 'A' is ('9' + 1)
-        }
-        return c - '0';   // Value returned is between 0 and 35, inclusive.
-}
+#endif /* !defined(MAC_OS_X_VERSION_10_7) || (MAC_OS_X_VERSION_MAX_ALLOWED <
+          MAC_OS_X_VERSION_10_7) */
 
+#endif /* __APPLE__ */
 
-static inline int isspace (int c)
-{
-  //
-  // <space> ::= [ ]
-  //
-        return ((c) == ' ');
-}
-
-/** The strtoul function converts the initial portion of the string pointed to
-    by nptr to unsigned long int representation.
-
-    See the description for strtol for more information.
-
-  @return   The strtoul function returns the converted value, if any. If no
-            conversion could be performed, zero is returned. If the correct
-            value is outside the range of representable values, ULONG_MAX is
-            returned and the value of the macro ERANGE is stored in errno.
-**/
-static inline unsigned long
-strtoul(const char * __restrict nptr, char ** __restrict endptr, int base)
-{
-        const char     *pEnd;
-        unsigned long   Result = 0;
-        unsigned long   Previous;
-        int             temp;
-
-        pEnd = nptr;
-
-        if((base < 0) || (base == 1) || (base > 36)) {
-                if(endptr != NULL) {
-                        *endptr = NULL;
-                }
-                return 0;
-        }
-// Skip leading spaces.
-        while(isspace(*nptr))   ++nptr;
-
-// Process Subject sequence: optional + sign followed by digits.
-        if(*nptr == '+') {
-                ++nptr;
-        }
-
-        if(*nptr == '0') {  /* Might be Octal or Hex */
-                if(toupper(nptr[1]) == 'X') {   /* Looks like Hex */
-                        if((base == 0) || (base == 16)) {
-                                nptr += 2;  /* Skip the "0X"      */
-                                base = 16;  /* In case base was 0 */
-                        }
-                }
-                else {    /* Looks like Octal */
-                        if((base == 0) || (base == 8)) {
-                                ++nptr;     /* Skip the leading "0" */
-                                base = 8;   /* In case base was 0   */
-                        }
-                }
-        }
-        if(base == 0) {   /* If still zero then must be decimal */
-                base = 10;
-
-	}
-        if(*nptr  == '0') {
-                for( ; *nptr == '0'; ++nptr);  /* Skip any remaining leading zeros */
-                pEnd = nptr;
-	}
-
-        while( isalnum(*nptr) && ((temp = Digit2Val(*nptr)) < base)) {
-                Previous = Result;
-                Result = (Result * base) + (unsigned long)temp;
-                if( Result < Previous)  {   // If we overflowed
-                        Result = UINT32_MAX;
-                        //errno = -1;
-                        break;
-                }
-                pEnd = ++nptr;
-        }
-// Save pointer to final sequence
-        if(endptr != NULL) {
-                *endptr = (char *)pEnd;
-        }
-        return Result;
-}
-
-#endif /* _LIBFDT_ENV_H */
+#endif /* LIBFDT_ENV_H */
