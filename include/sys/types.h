9e0058301f06b38ab200fc6f52e694198796c6ca
Minecrell <minecrell@minecrell.net>
2022-04-18 11:35:25 +0200

    Drop duplicate boolean typedef

--- include/sys/types.h
+++ include/sys/types.h
@@ -62,7 +62,6 @@ typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
-typedef int     boolean;
 typedef int8_t int8;
 typedef int16_t int16;
 typedef int32_t int32;
