4143c9aa63ad8d99d4c63abe7d150d7abc2df20b
Minecrell <minecrell@minecrell.net>
2022-04-18 13:45:07 +0200

    project: lk2nd-msm8953: Disable SECURE_CODE_MEM=1

--- project/lk2nd-msm8953.mk
+++ project/lk2nd-msm8953.mk
@@ -3,3 +3,8 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 TARGET := msm8953
 DISPLAY_USE_CONTINUOUS_SPLASH := 1
 include $(LOCAL_DIR)/lk2nd-base.mk
+
+# Since lk2nd is typically used through lk.bin, having separate code/data
+# segments with a fixed 1 MiB offset increases the binary size significantly,
+# since a lot of padding has to be added inbetween. Let's just disable this for now.
+DEFINES := $(filter-out SECURE_CODE_MEM=1, $(DEFINES))
