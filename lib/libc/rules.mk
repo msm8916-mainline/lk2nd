littlekernel/lk

Add a handful of libc functions for BoringSSL.
This change adds:
	declarations:
		abort
		strcasecmp
		getenv
	definitions:
		bsearch
		strtoul
	macros:
		PRIu32
		PRIx32

Change-Id: I2968da411d6810520905bcd3b8a370bf83bee804

lib/libc/rules.mk
@@ -4,6 +4,7 @@ MODULE := $(LOCAL_DIR)
 
 MODULE_SRCS += \
 	$(LOCAL_DIR)/atoi.c \
+	$(LOCAL_DIR)/bsearch.c \
 	$(LOCAL_DIR)/ctype.c \
 	$(LOCAL_DIR)/printf.c \
 	$(LOCAL_DIR)/rand.c \

msm8916-mainline/lk2nd

(cherry picked from commit 99c543c6d8c6fa9fc90c22f299522d1c029a5827)

lib/libc/rules.mk
@@ -2,6 +2,7 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 OBJS += \
 	$(LOCAL_DIR)/atoi.o \
+	$(LOCAL_DIR)/bsearch.o \
 	$(LOCAL_DIR)/ctype.o \
 	$(LOCAL_DIR)/gcd_lcm.o \
 	$(LOCAL_DIR)/itoa.o \