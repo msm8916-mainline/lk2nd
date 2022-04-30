littlekernel/lk

[lib][libc] add simple errno implementation
Completely un-threadsafe, but enough to satisfy the __geterrno extern.

Change-Id: I95b23f381767a3d55f43d77c188cb0cd611536ee

lib/libc/rules.mk
@@ -6,6 +6,7 @@ MODULE_SRCS += \
 	$(LOCAL_DIR)/atoi.c \
 	$(LOCAL_DIR)/bsearch.c \
 	$(LOCAL_DIR)/ctype.c \
+	$(LOCAL_DIR)/errno.c \
 	$(LOCAL_DIR)/printf.c \
 	$(LOCAL_DIR)/rand.c \
 	$(LOCAL_DIR)/stdio.c \

msm8916-mainline/lk2nd

lib/libc/rules.mk
@@ -4,6 +4,7 @@ OBJS += \
 	$(LOCAL_DIR)/atoi.o \
 	$(LOCAL_DIR)/bsearch.o \
 	$(LOCAL_DIR)/ctype.o \
+	$(LOCAL_DIR)/errno.o \
 	$(LOCAL_DIR)/gcd_lcm.o \
 	$(LOCAL_DIR)/itoa.o \
 	$(LOCAL_DIR)/printf.o \