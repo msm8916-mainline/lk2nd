f137dbae55cdb7c0ee9f0141c3aee248f91a99a1
Running Child <46817089+AzeAstro@users.noreply.github.com>
2022-05-04 13:19:23 +0400

    We added SM-A700H support. SM-A700YD's img doesn't work with SM-A700H. Special thanks to "huh?" in msm8939-mainline in matrix

--- dts/msm8916/rules.mk
+++ dts/msm8916/rules.mk
@@ -35,6 +35,8 @@ DTBS += \
 	$(LOCAL_DIR)/msm8916-samsung-r11.dtb \
 	$(LOCAL_DIR)/msm8929-samsung-r00.dtb \
 	$(LOCAL_DIR)/msm8929-samsung-r04.dtb \
+	$(LOCAL_DIR)/msm8239-samsung-r09.dtb \
+	$(LOCAL_DIR)/msm8239-samsung-r08.dtb \
 	$(LOCAL_DIR)/msm8939-asus-z00t.dtb \
 	$(LOCAL_DIR)/msm8939-huawei-kiwi.dtb \
 	$(LOCAL_DIR)/msm8939-mtp.dtb \
