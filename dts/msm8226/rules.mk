850f24ffde5cc548bf07ccd2f77be147b6a7564d
Matti Lehtimäki <matti.lehtimaki@gmail.com>
2022-05-15 03:10:00 +0300

    dts: msm8226: Add Samsung Galaxy Tab 4 10.1 (2014) (SM-T530)

--- dts/msm8226/rules.mk
+++ dts/msm8226/rules.mk
@@ -4,5 +4,6 @@ DTBS += \
 	$(LOCAL_DIR)/apq8026-asus-sparrow.dtb \
 	$(LOCAL_DIR)/apq8026-huawei-sturgeon.dtb \
 	$(LOCAL_DIR)/apq8026-lg-lenok.dtb \
+	$(LOCAL_DIR)/apq8026-samsung-r03.dtb \
 	$(LOCAL_DIR)/msm8226-samsung-ms013g.dtb \
 	$(LOCAL_DIR)/msm8926-huawei-g6-l11-vb.dtb
