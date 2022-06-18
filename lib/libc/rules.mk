cbcdc9223000af20cd6d153d83fdf3ac05a688a4
Stephan Gerhold <stephan@gerhold.net>
2022-06-18 19:21:24 +0200

    lib: libc: Build with -fno-builtin
    Without -fno-builtin the compiler might attempt to optimize the libc
    code by replacing loops with calls to libc code, potentially causing
    infinite loops. This seems to cause problems on some platforms with
    GCC 12.
    
    (cherry picked from commit 1b36977eb809e36c3d6439f22bf9c95cbf824ced)

--- lib/libc/rules.mk
+++ lib/libc/rules.mk
@@ -13,6 +13,9 @@ OBJS += \
 	$(LOCAL_DIR)/stdio.o \
 	$(LOCAL_DIR)/eabi.o
 
+# Without -fno-builtin the compiler might attempt to optimize the libc code by
+# replacing loops with calls to libc code, potentially causing infinite loops.
+$(BUILDDIR)/$(LOCAL_DIR)/%.o: CFLAGS := $(CFLAGS) -fno-builtin
 
 include $(LOCAL_DIR)/string/rules.mk
 
