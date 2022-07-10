c5da04b312bd45412cf48eedfb61674c433d72b8
Stephan Gerhold <stephan@gerhold.net>
2022-07-10 13:57:13 +0200

    lk2nd: project: lk1st: Enable device module when bundling DTB

--- lk2nd/project/lk1st.mk
+++ lk2nd/project/lk1st.mk
@@ -1,3 +1,10 @@
 # SPDX-License-Identifier: BSD-3-Clause
 LK2ND_PROJECT := lk1st
 include lk2nd/project/base.mk
+
+# The device module is useless on lk1st if no DTB is bundled
+ifneq ($(LK2ND_BUNDLE_DTB),)
+MODULES += lk2nd/device
+else
+$(info NOTE: Device module is disabled without bundling a device DTB (LK2ND_BUNDLE_DTB))
+endif
