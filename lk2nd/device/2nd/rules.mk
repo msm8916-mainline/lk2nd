7ea2674a10c848e7452a7607f6e70b969a0fe45a
Stephan Gerhold <stephan@gerhold.net>
2022-07-10 13:56:19 +0200

    lk2nd: device: 2nd: Add rules to build Android boot image

--- lk2nd/device/2nd/rules.mk
+++ lk2nd/device/2nd/rules.mk
@@ -7,3 +7,46 @@ OBJS += \
 	$(LOCAL_DIR)/match.o \
 	$(LOCAL_DIR)/parse-cmdline.o \
 	$(LOCAL_DIR)/parse-tags.o \
+
+ifneq ($(OUTBOOTIMG),)
+# Appended DTBs
+OUTBINDTB := $(OUTBIN)
+ifneq ($(ADTBS),)
+OUTBINDTB := $(OUTBIN)-dtb
+$(OUTBINDTB): $(OUTBIN) $(ADTBS)
+	@echo generating image with $(words $(ADTBS)) appended DTBs: $@
+	$(NOECHO)cat $^ > $@
+endif
+
+# QCDT image
+ifneq ($(LK2ND_QCDT),)
+OUTQCDT := $(LK2ND_QCDT)
+$(warning WARNING: Using pre-built QCDT image from $(LK2ND_QCDT))
+else ifneq ($(QCDTBS),)
+OUTQCDT := $(BUILDDIR)/qcdt.img
+$(OUTQCDT): $(QCDTBS)
+	@echo generating QCDT image with $(words $(QCDTBS)) DTBs: $@
+	$(NOECHO)lk2nd/scripts/dtbTool -o $@ $(QCDTBS)
+endif
+
+# Android boot image
+MKBOOTIMG_BASE ?= $(BASE_ADDR)
+
+ifeq ($(ENABLE_UFS_SUPPORT), 1)
+MKBOOTIMG_PAGESIZE ?= 4096
+endif
+
+$(OUTBOOTIMG): $(OUTBINDTB) $(OUTQCDT)
+	@echo generating Android boot image: $@
+	$(NOECHO)lk2nd/scripts/mkbootimg \
+		--kernel=$< \
+		--output=$@ \
+		--cmdline="$(MKBOOTIMG_CMDLINE)" \
+		$(if $(OUTQCDT),--qcdt=$(OUTQCDT)) \
+		$(if $(MKBOOTIMG_BASE),--base=$(MKBOOTIMG_BASE)) \
+		$(if $(MKBOOTIMG_PAGESIZE),--pagesize=$(MKBOOTIMG_PAGESIZE)) \
+		$(MKBOOTIMG_ARGS)
+	$(NOECHO)echo -n SEANDROIDENFORCE >> $@
+
+APPSBOOTHEADER: $(OUTBOOTIMG)
+endif
