e3940ca4219962b291e07d1fc6e98a5f39db2f5b
Stephan Gerhold <stephan@gerhold.net>
2022-07-15 11:32:55 +0200

    Add some example DTs

---
+++ lk2nd/device/dts/msm8916/rules.mk
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: BSD-3-Clause
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+QCDTBS += \
+	$(LOCAL_DIR)/apq8016-sbc.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-harpia-p1b-4d.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-harpia-p1b-4e.dtb \
+	$(LOCAL_DIR)/msm8916-qrd-9.dtb \
+	$(LOCAL_DIR)/samsung.dtb \
