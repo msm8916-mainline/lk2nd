40cae3669414fb2871949e2d7cd4e70e8c71fb70
Stephan Gerhold <stephan@gerhold.net>
2022-07-16 20:47:26 +0200

    dev_tree: Allow registering additional DT update handlers
    Make it possible to register additional handlers to update the device
    tree before booting the kernel. The handlers are registered using the
    DEV_TREE_UPDATE() macro, placed in a special linker section and then
    called at runtime. They also get a guessed boot type which allows
    making certain modifications to the device tree only for particular
    operating systems.

---
+++ app/aboot/boot.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+#ifndef ABOOT_BOOT_H
+#define ABOOT_BOOT_H
+
+#include <bits.h>
+
+enum boot_type {
+	BOOT_ARM64	= BIT(0),
+	BOOT_ANDROID	= BIT(1),
+	BOOT_DOWNSTREAM	= BIT(2),
+};
+
+#if DEVICE_TREE
+struct dt_update_handler {
+	const char *name;
+	int (*update_dt)(void *fdt, const char *cmdline, enum boot_type type);
+};
+#define DEV_TREE_UPDATE(func) \
+	static const struct dt_update_handler _dt_update_##func \
+		__SECTION(".dt_update") __USED = { #func, (func) }
+#else
+#define DEV_TREE_UPDATE(func)
+#endif
+
+#endif /* ABOOT_BOOT_H */
