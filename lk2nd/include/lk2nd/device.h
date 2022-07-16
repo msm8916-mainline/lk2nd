7da0faea84cdc4664458ad2f7533784e92fabc60
Stephan Gerhold <stephan@gerhold.net>
2022-07-16 20:57:47 +0200

    lk2nd: device: Hook into aboot command line update
    Instead of generating a custom kernel command line, reuse the command
    line from the previous bootloader when booting from lk2nd. This is
    necessary for panel detection to work in downstream panels, plus is
    needed when booting another lk2nd from lk2nd (so it can detect the
    correct device again).
    
    For lk1st, pass a "lk2nd.compatible=" command line parameter so the
    next lk2nd has a chance to detect the device properly again.
    
    Also add a GENERATE_CMDLINE_ONLY_FOR_ANDROID option that allows
    skipping generation of a command line with lots of "androidboot."
    variables when booting operating systems other than Android.

---
+++ lk2nd/include/lk2nd/device.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+#ifndef LK2ND_DEVICE_H
+#define LK2ND_DEVICE_H
+
+#include <boot.h>
+
+unsigned char *lk2nd_device_update_cmdline(const char *cmdline, enum boot_type boot_type);
+
+#endif /* LK2ND_DEVICE_H */
