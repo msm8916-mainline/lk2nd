ba9cb466622667904effcafc6ab59a59b1af7c78
Nikita Travkin <nikita@trvn.ru>
2022-07-20 12:17:14 +0500

    lk2nd: device: Add a keymap driver

--- lk2nd/device/rules.mk
+++ lk2nd/device/rules.mk
@@ -1,11 +1,23 @@
 # SPDX-License-Identifier: BSD-3-Clause
 LOCAL_DIR := $(GET_LOCAL_DIR)
-MODULES += lib/libfdt
+
+MODULES += \
+	   lib/libfdt \
+	   lk2nd/hw/gpio \
 
 LK2ND_DEVICE_OBJ := $(LOCAL_DIR)/device.o
+
+# NOTE: The init functions are performed in the order of linking
+# thus the order might be important here. Notably:
+#
+# - keys.o must be after device.o as device.o will (if needed) probe
+#   the keys driver and the keys.o will then rely on the created data
+#   to report the keys.
+
 OBJS += \
 	$(LK2ND_DEVICE_OBJ) \
 	$(LOCAL_DIR)/panel.o \
+	$(LOCAL_DIR)/keys.o \
 
 ifneq ($(LK2ND_COMPATIBLE),)
 DEFINES += LK2ND_COMPATIBLE="$(LK2ND_COMPATIBLE)"
