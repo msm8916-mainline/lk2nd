0bc8381a6118a56d256c4e1f8baa9eba0441538f
Nikita Travkin <nikita@trvn.ru>
2022-08-02 11:44:39 +0500

    lk2nd: hw: gpio: Add pm8921 driver

--- lk2nd/hw/gpio/rules.mk
+++ lk2nd/hw/gpio/rules.mk
@@ -6,3 +6,4 @@ OBJS += \
 	$(LOCAL_DIR)/gpio.o \
 	$(LOCAL_DIR)/tlmm.o \
 	$(if $(filter dev/pmic/pm8x41, $(ALLMODULES)), $(LOCAL_DIR)/pm8x41.o) \
+	$(if $(filter dev/pmic/pm8921, $(ALLMODULES)), $(LOCAL_DIR)/pm8921.o) \
