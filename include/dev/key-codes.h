fe741e9e3acb2531de9f3be3ea21ec2297408bd6
Nikita Travkin <nikita@trvn.ru>
2022-08-04 11:30:31 +0500

    dev: keys: Separate the keycodes into a separate header

--- include/dev/key-codes.h
+++ include/dev/key-codes.h
@@ -29,14 +29,58 @@
  * SUCH DAMAGE.
  */
 
-#ifndef __DEV_KEYS_H
-#define __DEV_KEYS_H
+#ifndef __DEV_KEY_CODES_H
+#define __DEV_KEY_CODES_H
 
-#include <sys/types.h>
-#include "key-codes.h"
+/* these are just the ascii values for the chars */
+#define KEY_0		0x30
+#define KEY_1		0x31
+#define KEY_2		0x32
+#define KEY_3		0x33
+#define KEY_4		0x34
+#define KEY_5		0x35
+#define KEY_6		0x36
+#define KEY_7		0x37
+#define KEY_8		0x38
+#define KEY_9		0x39
 
-void keys_init(void);
-void keys_post_event(uint16_t code, int16_t value);
-int keys_get_state(uint16_t code);
+#define KEY_A		0x61
 
-#endif /* __DEV_KEYS_H */
+#define KEY_ESC		0x100
+#define KEY_F1		0x101
+#define KEY_F2		0x102
+#define KEY_F3		0x103
+#define KEY_F4		0x104
+#define KEY_F5		0x105
+#define KEY_F6		0x106
+#define KEY_F7		0x107
+#define KEY_F8		0x108
+#define KEY_F9		0x109
+
+#define KEY_LEFT	0x110
+#define KEY_RIGHT	0x111
+#define KEY_UP		0x112
+#define KEY_DOWN	0x113
+#define KEY_CENTER	0x114
+
+#define KEY_VOLUMEUP	0x115
+#define KEY_VOLUMEDOWN	0x116
+#define KEY_MUTE	0x117
+#define KEY_SOUND	0x118
+#define KEY_POWER	0x119
+
+#define KEY_SOFT1	0x11a
+#define KEY_SOFT2	0x11b
+#define KEY_STAR	0x11c
+#define KEY_SHARP	0x11d
+#define KEY_MAIL	0x11e
+
+#define KEY_SEND	0x120
+#define KEY_CLEAR	0x121
+#define KEY_HOME	0x122
+#define KEY_BACK	0x123
+#define KEY_MENU	0x124
+
+#define MAX_KEYS	0x1ff
+
+#endif /* __DEV_KEY_CODES_H */
