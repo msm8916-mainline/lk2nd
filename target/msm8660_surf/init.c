863600a88e967a9dfcaf2fcc4650f0fca1e8e7c0
Stephan Gerhold <stephan@gerhold.net>
2022-08-05 20:49:11 +0200

    target: msm8660: Add missing target_mmc_caps()

--- target/msm8660_surf/init.c
+++ target/msm8660_surf/init.c
@@ -550,3 +550,14 @@ int emmc_recovery_init(void)
 	rc = _emmc_recovery_init();
 	return rc;
 }
+
+/*
+ * Function to set the capabilities for the host
+ */
+void target_mmc_caps(struct mmc_host *host)
+{
+	host->caps.ddr_mode = 0;
+	host->caps.hs200_mode = 0;
+	host->caps.bus_width = MMC_BOOT_BUS_WIDTH_8_BIT;
+	host->caps.hs_clk_rate = MMC_CLK_50MHZ;
+}
