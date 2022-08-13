littlekernel/lk

[libc] tweak stdio routines to be a bit more standard
change getc/putc/getchar/putchar style routines to take a fake
FILE pointer.
Move log2 to log2_uint to not collide with stdc log2()

lib/libc/rules.mk
@@ -8,6 +8,7 @@ MODULE_SRCS += \
 	$(LOCAL_DIR)/printf.c \
 	$(LOCAL_DIR)/malloc.c \
 	$(LOCAL_DIR)/rand.c \
+	$(LOCAL_DIR)/stdio.c \
 	$(LOCAL_DIR)/eabi.c
 
 

lib/libc/stdio.c
@@ -1,9 +1,5 @@
 /*
-** Copyright 2001, Travis Geiselbrecht. All rights reserved.
-** Distributed under the terms of the NewOS License.
-*/
-/*
- * Copyright (c) 2008 Travis Geiselbrecht
+ * Copyright (c) 2013 Travis Geiselbrecht
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files
@@ -24,11 +20,50 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#include <string.h>
+#include <debug.h>
+#include <stdio.h>
+#include <stdarg.h>
 #include <sys/types.h>
+#include <platform/debug.h>
 
-void bcopy(void const *src, void *dest, size_t count)
+int fputc(int c, FILE *fp)
 {
-	memcpy(dest, src, count);
+	_dputc(c);
+	return 0;
+}
+
+int putchar(int c)
+{
+	return fputc(c, stdout);
+}
+
+int puts(const char *str)
+{
+	int err = _dputs(str);
+	if (err >= 0)
+		_dputc('\n');
+
+	return err;
+}
+
+int fputs(const char *s, FILE *fp)
+{
+	return _dputs(s);
+}
+
+int getc(FILE *fp)
+{
+	char c;
+
+	int err = platform_dgetc(&c, true);
+	if (err < 0)
+		return err;
+
+	return (unsigned char)c;
+}
+
+int getchar(void)
+{
+	return getc(stdin);
 }
 

msm8916-mainline/lk2nd

(cherry picked from commit df8b371ed9f047ed0025a8d71527ea0a2b439f74)
[adjust for changes missing in Qualcomm's LK fork]

lib/libc/rules.mk
@@ -10,6 +10,7 @@ OBJS += \
 	$(LOCAL_DIR)/printf.o \
 	$(LOCAL_DIR)/malloc.o \
 	$(LOCAL_DIR)/rand.o \
+	$(LOCAL_DIR)/stdio.o \
 	$(LOCAL_DIR)/eabi.o
 
 

lib/libc/stdio.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2013 Travis Geiselbrecht
+ * Copyright (c) 2013 Travis Geiselbrecht
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files
@@ -20,32 +20,49 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#ifndef __STDIO_H
-#define __STDIO_H
-
 #include <debug.h>
-#include <printf.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <sys/types.h>
 
-/* fake FILE struct */
-typedef struct FILE {
-} FILE;
+int fputc(int c, FILE *fp)
+{
+	_dputc(c);
+	return 0;
+}
 
-#define stdin ((FILE *)1)
-#define stdout ((FILE *)2)
-#define stderr ((FILE *)3)
+int putchar(int c)
+{
+	return fputc(c, stdout);
+}
 
-int fputc(int c, FILE *fp);
-#define putc(c, fp) fputc(c, fp)
-int putchar(int c);
+int puts(const char *str)
+{
+	int err = _dputs(str);
+	if (err >= 0)
+		_dputc('\n');
 
-int fputs(const char *s, FILE *fp);
-int puts(const char *str);
+	return err;
+}
 
-int getc(FILE *fp);
-int getchar(void);
+int fputs(const char *s, FILE *fp)
+{
+	return _dputs(s);
+}
 
-size_t fwrite(const void *buf, size_t size, size_t count, FILE *stream);
-int sscanf(const char *str, const char *format, ...);
+int getc(FILE *fp)
+{
+	char c;
 
-#endif
+	int err = dgetc(&c, true);
+	if (err < 0)
+		return err;
+
+	return (unsigned char)c;
+}
+
+int getchar(void)
+{
+	return getc(stdin);
+}
 