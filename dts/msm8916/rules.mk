a5986a02a15cfdf040b5001979bcefa6ae2705e4
reocat <111279389+reocat@users.noreply.github.com>
2022-08-15 13:15:12 +0300

    Update rules.mk

--- dts/msm8916/rules.mk
+++ dts/msm8916/rules.mk
@@ -37,6 +37,7 @@ DTBS += \
 	$(LOCAL_DIR)/msm8929-samsung-r04.dtb \
 	$(LOCAL_DIR)/msm8939-asus-z00t.dtb \
 	$(LOCAL_DIR)/msm8939-huawei-kiwi.dtb \
+	$(LOCAL_DIR)/msm8939-xiaomi-ido.dtb \
 	$(LOCAL_DIR)/msm8939-mtp.dtb \
 	$(LOCAL_DIR)/msm8939-qrd-skuk.dtb \
 	$(LOCAL_DIR)/msm8939-samsung-r01.dtb
