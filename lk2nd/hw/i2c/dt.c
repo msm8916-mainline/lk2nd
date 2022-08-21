ca0bb1d0ee0d3b40cdc2c1d46f93d78593b49c1f
Stephan Gerhold <stephan@gerhold.net>
2022-08-21 20:16:18 +0200

    lk2nd: hw: i2c: Add DT helper for gpio_i2c
    Parse the I2C GPIOs and optionally the I2C address from a DT snippet of
    the following form:
    
    i2c-reg = <0x42>;
    i2c-sda-gpios = <&tlmm 42 (GPIO_ACTIVE_HIGH | GPIO_OPEN_DRAIN);
    i2c-scl-gpios = <&tlmm 43 (GPIO_ACTIVE_HIGH | GPIO_OPEN_DRAIN);

---
+++ lk2nd/hw/i2c/dt.c
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <debug.h>
+#include <err.h>
+
+#include <libfdt.h>
+#include <lk2nd/hw/gpio_i2c.h>
+
+status_t gpio_i2c_get(const void *dtb, int node, gpio_i2c_info_t *i, uint8_t *addr)
+{
+	status_t ret;
+
+	ret = gpiol_get(dtb, node, "i2c-sda", &i->sda, GPIOL_FLAGS_IN);
+	if (ret)
+		return ret;
+	ret = gpiol_get(dtb, node, "i2c-scl", &i->scl, GPIOL_FLAGS_IN);
+	if (ret)
+		return ret;
+
+	/* Set to ~50 KHz, maybe this should be configurable? */
+	i->hcd = 10;
+	i->qcd = 5;
+
+	if (addr) {
+		const fdt32_t *prop;
+		int len;
+
+		prop = fdt_getprop(dtb, node, "i2c-reg", &len);
+		if (len != sizeof(*prop)) {
+			dprintf(CRITICAL, "Invalid i2c-reg property: %d\n", len);
+			return ERROR;
+		}
+		*addr = fdt32_to_cpu(*prop);
+	}
+	return NO_ERROR;
+}
--- lk2nd/hw/i2c/rules.mk
+++ lk2nd/hw/i2c/rules.mk
@@ -1,6 +1,10 @@
 # SPDX-License-Identifier: BSD-3-Clause
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
-MODULES += lk2nd/hw/gpio
+MODULES += \
+	lib/libfdt \
+	lk2nd/hw/gpio \
+
 OBJS += \
+	$(LOCAL_DIR)/dt.o \
 	$(LOCAL_DIR)/gpio_i2c.o \
