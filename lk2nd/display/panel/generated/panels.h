95aef286872d4d0aca9aa1fd7fdf278733920373
Raymond Hackley <raymondhackley@protonmail.com>
2022-11-10 17:05:21 +0000

    lk2nd: display: panel: generated: Add hx8394d_720p_video for acer-a1-724

--- lk2nd/display/panel/generated/panels.h
+++ lk2nd/display/panel/generated/panels.h
@@ -3,6 +3,8 @@
 #ifndef _LK2ND_GENERATED_PANELS_H_
 #define _LK2ND_GENERATED_PANELS_H_
 
+#include "lk_panel_hx8394d_720p_video.h"
+
 int oem_panel_select(const char *panel_name, struct panel_struct *panel,
 		     struct msm_panel_info *pinfo, struct mdss_dsi_phy_ctrl *phy_db);
 
