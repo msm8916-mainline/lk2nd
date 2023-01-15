71d0fdb193cfa85f61c334f486c76c101c5860b1
Newbyte <newbie13xd@gmail.com>
2023-01-15 19:10:12 +0100

    dts: msm8226: Add Samsung Galaxy Tab 4 10.1 LTE (2014) (SM-T535)

--- dts/msm8226/rules.mk
+++ dts/msm8226/rules.mk
@@ -6,4 +6,5 @@ DTBS += \
 	$(LOCAL_DIR)/apq8026-lg-lenok.dtb \
 	$(LOCAL_DIR)/apq8026-samsung-r03.dtb \
 	$(LOCAL_DIR)/msm8226-samsung-ms013g.dtb \
-	$(LOCAL_DIR)/msm8926-huawei-g6-l11-vb.dtb
+	$(LOCAL_DIR)/msm8926-huawei-g6-l11-vb.dtb \
+	$(LOCAL_DIR)/msm8926-samsung-r02.dtb
