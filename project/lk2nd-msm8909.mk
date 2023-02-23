98318a918bf05a59a2e92eb3144cb136e35dd03b
Raymond Hackley <raymondhackley@protonmail.com>
2023-02-23 11:34:23 +0000

    project: msm8909: add rmtfs

--- project/lk2nd-msm8909.mk
+++ project/lk2nd-msm8909.mk
@@ -4,5 +4,8 @@ include $(LOCAL_DIR)/lk2nd-defines.mk
 include $(LOCAL_DIR)/msm8909.mk
 include $(LOCAL_DIR)/lk2nd-base.mk
 
+# Memory usually reserved for RMTFS, should be fine for early SMP bring-up
+SMP_SPIN_TABLE_BASE := 0x87c00000
+
 APPSBOOTHEADER: $(OUTBOOTIMG) $(OUTBOOTIMGADTB)
 ANDROID_BOOT_BASE := 0x00000000
