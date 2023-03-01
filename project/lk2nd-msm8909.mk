2038cd6e498f89829d1c2085be268fb1d86df30a
Raymond Hackley <raymondhackley@protonmail.com>
2023-03-01 16:27:35 +0000

    project: Add experimental lk1st-msm8909

--- project/lk2nd-msm8909.mk
+++ project/lk2nd-msm8909.mk
@@ -1,11 +1,7 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
 include $(LOCAL_DIR)/lk2nd-defines.mk
-include $(LOCAL_DIR)/msm8909.mk
-include $(LOCAL_DIR)/lk2nd-base.mk
-
-# Memory usually reserved for RMTFS, should be fine for early SMP bring-up
-SMP_SPIN_TABLE_BASE := 0x87c00000
+include $(LOCAL_DIR)/lk1st-msm8909.mk
 
 APPSBOOTHEADER: $(OUTBOOTIMG) $(OUTBOOTIMGADTB)
 ANDROID_BOOT_BASE := 0x00000000
