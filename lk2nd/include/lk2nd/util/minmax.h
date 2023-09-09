3fda56b3ffbc90e269787ff086c9041e17442db2
Stephan Gerhold <stephan@gerhold.net>
2023-09-09 13:21:28 +0200

    lk2nd: util: minmax: Evaluate arguments only once
    Store the macro arguments in a temporary variable to avoid calling
    functions multiple times if they are directly passed as arguments.

--- lk2nd/include/lk2nd/util/minmax.h
+++ lk2nd/include/lk2nd/util/minmax.h
@@ -2,9 +2,28 @@
 #ifndef LK2ND_UTIL_MINMAX_H
 #define LK2ND_UTIL_MINMAX_H
 
-#define max(a, b)	((a) > (b) ? (a) : (b))
-#define min(a, b)	((a) < (b) ? (a) : (b))
+#define _max(a, b)	((a) > (b) ? (a) : (b))
+#define _min(a, b)	((a) < (b) ? (a) : (b))
 
-#define clamp(val, lo, hi) min(max(val, lo), hi)
+#define _clamp(val, lo, hi) _min(_max(val, lo), hi)
+
+#define max(a, b) ({ \
+	__typeof__(a) _a = (a); \
+	__typeof__(b) _b = (b); \
+	_max(_a, _b); \
+})
+
+#define min(a, b) ({ \
+	__typeof__(a) _a = (a); \
+	__typeof__(b) _b = (b); \
+	_min(_a, _b); \
+})
+
+#define clamp(val, lo, hi) ({ \
+	__typeof__(val) _val = (val); \
+	__typeof__(lo) _lo = (lo); \
+	__typeof__(hi) _hi = (hi); \
+	_clamp(_val, _lo, _hi); \
+})
 
 #endif /* LK2ND_UTIL_MINMAX_H */
