f43b092329f525223e46b4fc8f1ae7c1adddaebc
Nikita Travkin <nikita@trvn.ru>
2023-09-13 18:24:47 +0500

    lk2nd: hw: Add an mmc_wrapper based bdev implementation.

---
+++ lk2nd/hw/bdev/bdev.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright (c) 2023 Nikita Travkin <nikita@trvn.ru> */
+
+#include <lk2nd/hw/bdev.h>
+
+#include "bdev.h"
+
+/**
+ * lk2nd_bdev_init() - Prepare block devices for lk2nd
+ */
+void lk2nd_bdev_init(void)
+{
+	lk2nd_wrapper_bio_register();
+
+	lk2nd_bdev_dump_devices();
+}
---
+++ lk2nd/hw/bdev/wrapper.c
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright (c) 2023 Nikita Travkin <nikita@trvn.ru> */
+
+#include <debug.h>
+#include <lib/bio.h>
+#include <lib/partition.h>
+#include <partition_parser.h>
+#include <stdlib.h>
+
+#include <lk2nd/init.h>
+
+#include "bdev.h"
+
+static ssize_t lk2nd_wrapper_bdev_read_block(struct bdev *bdev, void *buf, bnum_t block, uint count)
+{
+	return mmc_read((uint64_t)block * bdev->block_size, buf, count * bdev->block_size);
+}
+
+static void lk2nd_wrapper_publish_subdevices(bdev_t *bdev)
+{
+	struct partition_entry* entries = partition_get_partition_entries();
+	unsigned int i, count = partition_get_partition_count();
+	bdev_t *subdev;
+	char name[32];
+
+	for (i = 0; i < count; ++i) {
+		snprintf(name, sizeof(name), "%sp%d", bdev->name, i);
+		bio_publish_subdevice(bdev->name, name, entries[i].first_lba, entries[i].size);
+
+		subdev = bio_open(name);
+		subdev->label = (char *)entries[i].name;
+		bio_close(subdev);
+
+		/* There may be subpartitions... */
+		partition_publish(name, 0);
+	}
+}
+
+void lk2nd_wrapper_bio_register(void)
+{
+	uint32_t block_size = mmc_get_device_blocksize();
+	uint64_t card_capacity = mmc_get_device_capacity();
+	bdev_t *bdev = malloc(sizeof(*bdev));
+	char name[32] = "wrp0";
+
+	dprintf(INFO, "Registering wrapper bio devices...\n");
+	bio_initialize_bdev(bdev, name, block_size, card_capacity / block_size);
+
+	bdev->read_block = lk2nd_wrapper_bdev_read_block;
+
+	bio_register_device(bdev);
+
+	/*
+	 * FIXME: the lk's partition module is not GPT aware yet.
+	 */
+	lk2nd_wrapper_publish_subdevices(bdev);
+}
