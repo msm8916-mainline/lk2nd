42ef3f1d216d288df6d62a744019395df058417c
Luca Weiss <luca@z3ntu.xyz>
2023-09-15 21:14:53 +0200

    dts: msm8226: Add HTC One Mini 2 (memul)
    In the current code the display is not drawn correctly, this is fixed
    through PR #299.

--- dts/msm8226/rules.mk
+++ dts/msm8226/rules.mk
@@ -8,6 +8,7 @@ DTBS += \
 	$(LOCAL_DIR)/apq8026-samsung-r03.dtb \
 	$(LOCAL_DIR)/msm8226-motorola-falcon.dtb \
 	$(LOCAL_DIR)/msm8226-samsung-ms013g.dtb \
+	$(LOCAL_DIR)/msm8926-htc-memul.dtb \
 	$(LOCAL_DIR)/msm8926-huawei-g6-l11-vb.dtb \
 	$(LOCAL_DIR)/msm8926-samsung-r02.dtb \
 	$(LOCAL_DIR)/msm8926-v2-720p-mtp.dtb
