6ea2db8b3da9ff6ae7e1e891205fda6e53c01e6a
Nikita Travkin <nikita@trvn.ru>
2023-09-27 11:32:27 +0500

    lk2nd: hw: bdev: Add mmc_sdhci bdev driver

--- lk2nd/hw/bdev/bdev.c
+++ lk2nd/hw/bdev/bdev.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /* Copyright (c) 2023 Nikita Travkin <nikita@trvn.ru> */
 
+#include <compiler.h>
 #include <lk2nd/hw/bdev.h>
 
 #include "bdev.h"
@@ -11,6 +12,8 @@
 void lk2nd_bdev_init(void)
 {
 	lk2nd_wrapper_bio_register();
+	if (IS_ENABLED(MMC_SDHCI_SUPPORT))
+		lk2nd_mmc_sdhci_bio_register();
 
 	lk2nd_bdev_dump_devices();
 }
--- lk2nd/hw/bdev/bdev.h
+++ lk2nd/hw/bdev/bdev.h
@@ -5,5 +5,6 @@
 void lk2nd_bdev_dump_devices(void);
 
 void lk2nd_wrapper_bio_register(void);
+void lk2nd_mmc_sdhci_bio_register(void);
 
 #endif
