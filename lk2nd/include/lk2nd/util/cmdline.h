feb042f033e85e380ac31495f5c3d8210923695b
Nikita Travkin <nikita@trvn.ru>
2023-10-25 11:40:12 +0500

    lk2nd: util: cmdline: Add helper for getting cmdline argument value

--- lk2nd/include/lk2nd/util/cmdline.h
+++ lk2nd/include/lk2nd/util/cmdline.h
@@ -4,6 +4,21 @@
 
 #include <compiler.h>
 #include <stdbool.h>
+#include <stddef.h>
+
+/**
+ * lk2nd_cmdline_scan_arg() - Get cmdline argument value.
+ * @cmdline: The command line (space-separated parameters)
+ * @arg:     The parameter to search for
+ * @value:   Char array to write value of the argument.
+ * @vlen:    Max size of @value.
+ *
+ * Return: true if @arg was found in @cmdline, false otherwise.
+ * If the value was found but has no parameter ('=' sign and
+ * trailing value), then @value is unchanged. Otherwise @value
+ * is populated with the argument after '='.
+ */
+bool lk2nd_cmdline_scan_arg(const char *cmdline, const char *arg, char *value, size_t vlen);
 
 /**
  * lk2nd_cmdline_scan() - Scan for a space-separated parameter in command line.
