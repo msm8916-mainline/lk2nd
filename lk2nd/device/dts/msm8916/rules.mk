a8e076c18ad42ba7b84848e20f6f1a9e2b7abd0e
Nikita Travkin <nikita@trvn.ru>
2023-11-15 14:58:48 +0500

    dts: Import msm8216-samsung
    Co-authored-by: Raymond Hackley <raymondhackley@protonmail.com>
    Co-authored-by: Aria Moradi <aria.moradi007@gmail.com>
    Co-authored-by: Minecrell <minecrell@minecrell.net>
    Co-authored-by: Siddharth Manthan <siddharth.manthan@gmail.com>

--- lk2nd/device/dts/msm8916/rules.mk
+++ lk2nd/device/dts/msm8916/rules.mk
@@ -4,6 +4,7 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 QCDTBS += \
 	$(LOCAL_DIR)/apq8016-samsung.dtb \
 	$(LOCAL_DIR)/apq8016-sbc.dtb \
+	$(LOCAL_DIR)/msm8216-samsung.dtb \
 	$(LOCAL_DIR)/msm8916-motorola-harpia-p1b-4d.dtb \
 	$(LOCAL_DIR)/msm8916-motorola-harpia-p1b-4e.dtb \
 	$(LOCAL_DIR)/msm8916-qrd-7+12.dtb \
