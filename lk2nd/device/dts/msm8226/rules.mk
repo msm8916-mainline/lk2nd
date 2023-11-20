110a2b01606b49c02ebb70db415a7a619d115728
Nikita Travkin <nikita@trvn.ru>
2023-11-20 19:33:21 +0500

    dts: Import apq8026-{asus,huawei,lg}
    Co-authored-by: Luca Weiss <luca@z3ntu.xyz>
    Co-authored-by: Timon Baetz <timon.baetz@protonmail.com>

--- lk2nd/device/dts/msm8226/rules.mk
+++ lk2nd/device/dts/msm8226/rules.mk
@@ -1,5 +1,14 @@
 # SPDX-License-Identifier: BSD-3-Clause
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
+# NOTE: lenok wants to be first to be able to boot.
+ADTBS += \
+	$(LOCAL_DIR)/apq8026-lg-lenok.dtb \
+
+QCDTBS += \
+	$(LOCAL_DIR)/apq8026-asus-sparrow.dtb \
+	$(LOCAL_DIR)/apq8026-huawei-sturgeon.dtb \
+
+
 DTBS += \
 	$(LOCAL_DIR)/lumia.dtb \
