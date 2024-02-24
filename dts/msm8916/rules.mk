38877e6fb1bd6d2a5c19d20583693a73ec03bc3e
Adam Słaboń <asaillen@protonmail.com>
2024-02-24 13:26:24 +0100

    dts: msm8939: Add support for Lenovo Vibe K5 variant with MSM8939 and HD panels (A6020a40 HW39)

--- dts/msm8916/rules.mk
+++ dts/msm8916/rules.mk
@@ -48,4 +48,5 @@ DTBS += \
 	$(LOCAL_DIR)/msm8939-mtp.dtb \
 	$(LOCAL_DIR)/msm8939-qrd-skuk.dtb \
 	$(LOCAL_DIR)/msm8939-qrd-wt82918.dtb \
+	$(LOCAL_DIR)/msm8939-qrd-wt82918hd.dtb \
 	$(LOCAL_DIR)/msm8939-samsung-r01.dtb
