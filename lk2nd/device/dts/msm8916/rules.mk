6a69b161b6a98fafb34b1af2bb918fe8f3ce72e1
Nikita Travkin <nikita@trvn.ru>
2024-03-06 18:28:40 +0500

    dts: msm8916: don't build m8qlul by default.

--- lk2nd/device/dts/msm8916/rules.mk
+++ lk2nd/device/dts/msm8916/rules.mk
@@ -33,7 +33,6 @@ QCDTBS += \
 	$(LOCAL_DIR)/msm8929-samsung.dtb \
 	$(LOCAL_DIR)/msm8929-qrd-wt82918.dtb \
 	$(LOCAL_DIR)/msm8939-asus-z00t.dtb \
-	$(LOCAL_DIR)/msm8939-htc-m8qlul.dtb \
 	$(LOCAL_DIR)/msm8939-huawei-kiwi.dtb \
 	$(LOCAL_DIR)/msm8939-huawei-rio.dtb \
 	$(LOCAL_DIR)/msm8939-mtp.dtb \
