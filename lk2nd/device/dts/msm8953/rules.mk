d3ceb6fda727ea6f0bf02bf4332ab3ad7ae8f129
Adam Skladowski <a39.skl@gmail.com>
2024-03-11 19:30:18 +0100

    dts: msm8953: Import MSM8953 trees
    Import almost every currently supported tree from MSM8953 fork
    
    Co-authored-by: Ultracoolguy <dummyd241@gmail.com>
    Co-authored-by: Corentin <corentin-pro@mail.com>
    Co-authored-by: Luca Weiss <luca@z3ntu.xyz>
    Co-authored-by: lucasredelive <lucasredelive@gmail.com>
    Co-authored-by: Quentin B <quentin@quent1.fr>
    Co-authored-by: Barnabás Czémán <trabarni@gmail.com>
    Co-authored-by: vipaol <vipaolmail@gmail.com>
    Co-authored-by: Raihan Ahamed <raihan1999ahamed@gmail.com>
    Co-authored-by: Niklas Henrik Tonnätt <apolemus@apolemus.de>
    Co-authored-by: Nils Tonnaett <ntonnatt@ccrma.stanford.edu>
    Co-authored-by: Roman Rihter <teledurak@gmail.com>
    Co-authored-by: bzy-080408 <Bzy080408@outlook.com>
    Co-authored-by: alikates <59802505+alikates@users.noreply.github.com>
    Co-authored-by: M0Rf30 <morf3089@gmail.com>
    Co-authored-by: Sireesh Kodali <sireeshkodali1@gmail.com>
    Co-authored-by: Vladimir Lypak <vladimir.lypak@gmail.com>

---
+++ lk2nd/device/dts/msm8953/rules.mk
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: BSD-3-Clause
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+ADTBS += \
+	$(LOCAL_DIR)/msm8953-huawei-milan.dtb  \
+	$(LOCAL_DIR)/msm8953-lenovo-kuntao.dtb  \
+	$(LOCAL_DIR)/msm8953-motorola-deen.dtb  \
+	$(LOCAL_DIR)/msm8953-motorola-potter.dtb  \
+	$(LOCAL_DIR)/msm8953-mtp.dtb  \
+	$(LOCAL_DIR)/msm8953-qrd.dtb  \
+	$(LOCAL_DIR)/msm8953-xiaomi-common.dtb  \
+	$(LOCAL_DIR)/msm8953-xiaomi-daisy.dtb  \
+	$(LOCAL_DIR)/msm8953-xiaomi-markw.dtb  \
+	$(LOCAL_DIR)/msm8953-xiaomi-vince.dtb  \
+	$(LOCAL_DIR)/sdm450-samsung-r04.dtb  \
+	$(LOCAL_DIR)/sdm450-samsung-r05.dtb  \
+	$(LOCAL_DIR)/sdm632-fairphone-fp3.dtb  \
+	$(LOCAL_DIR)/sdm632-motorola-ocean.dtb  \
+	$(LOCAL_DIR)/sdm632-mtp.dtb  \
+
