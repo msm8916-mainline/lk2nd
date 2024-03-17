f47144913fe17851e0e628729ddf6a061cff46e0
André Apitzsch <git@apitzsch.eu>
2024-03-17 22:00:09 +0100

    dts: msm8226: Fix detection of falcon and peregrine

--- lk2nd/device/dts/msm8226/rules.mk
+++ lk2nd/device/dts/msm8226/rules.mk
@@ -4,16 +4,16 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 # NOTE: lenok wants to be first to be able to boot.
 ADTBS += \
 	$(LOCAL_DIR)/apq8026-lg-lenok.dtb \
+	$(LOCAL_DIR)/msm8226-motorola-falcon.dtb \
+	$(LOCAL_DIR)/msm8926-motorola-peregrine.dtb \
 
 QCDTBS += \
 	$(LOCAL_DIR)/apq8026-asus-sparrow.dtb \
 	$(LOCAL_DIR)/apq8026-huawei-sturgeon.dtb \
 	$(LOCAL_DIR)/apq8026-samsung.dtb \
-	$(LOCAL_DIR)/msm8226-motorola-falcon.dtb \
 	$(LOCAL_DIR)/msm8226-samsung.dtb \
 	$(LOCAL_DIR)/msm8926-htc-memul.dtb \
 	$(LOCAL_DIR)/msm8926-huawei-g6-l11-vb.dtb \
-	$(LOCAL_DIR)/msm8926-motorola-peregrine.dtb \
 	$(LOCAL_DIR)/msm8926-samsung.dtb \
 	$(LOCAL_DIR)/msm8926-v2-720p-mtp.dtb \
 
