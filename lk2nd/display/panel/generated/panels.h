f8e4cc15c66cc2808ddb109f818d6e42fac68014
Raymond Hackley <raymondhackley@protonmail.com>
2024-06-18 11:07:30 +0000

    lk2nd: display: panel: generated: Add ili9806e_boyi_byt45mp41t1m_4p5cpt for acer-t01

--- lk2nd/display/panel/generated/panels.h
+++ lk2nd/display/panel/generated/panels.h
@@ -4,6 +4,7 @@
 #define _LK2ND_GENERATED_PANELS_H_
 
 #include "lk_panel_hx8394d_720p_video.h"
+#include "lk_panel_ili9806e_boyi_byt45mp41t1m_4p5cpt.h"
 
 int oem_panel_select(const char *panel_name, struct panel_struct *panel,
 		     struct msm_panel_info *pinfo, struct mdss_dsi_phy_ctrl *phy_db);
