9e63c2a64cb8258774bfba28a61631f92411a829
Lin, Meng-Bo <linmengbo06890@proton.me>
2024-06-18 13:49:47 +0000

    lk2nd: panel: generated: Add ili9881c_hd_video for fareastone-smart506

--- lk2nd/display/panel/generated/panels.h
+++ lk2nd/display/panel/generated/panels.h
@@ -5,6 +5,7 @@
 
 #include "lk_panel_hx8394d_720p_video.h"
 #include "lk_panel_ili9806e_boyi_byt45mp41t1m_4p5cpt.h"
+#include "lk_panel_ili9881c_hd_video.h"
 
 int oem_panel_select(const char *panel_name, struct panel_struct *panel,
 		     struct msm_panel_info *pinfo, struct mdss_dsi_phy_ctrl *phy_db);
