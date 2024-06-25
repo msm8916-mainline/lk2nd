b8d8b2c18ee54cd76d1d2e3a73f0871a0b222c9d
gompa <gompa@h-bomb.nl>
2024-06-25 19:16:27 +0200

    dts: msm8909: Build all dtb as qcdt

--- lk2nd/device/dts/msm8909/rules.mk
+++ lk2nd/device/dts/msm8909/rules.mk
@@ -1,9 +1,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
-ADTBS += \
-	$(LOCAL_DIR)/msm8905-qrd-skub.dtb \
-	$(LOCAL_DIR)/msm8909-mtp.dtb \
 
 QCDTBS += \
+	$(LOCAL_DIR)/msm8905-qrd-skub.dtb \
 	$(LOCAL_DIR)/msm8909-1gb-qrd-skuc.dtb \
+	$(LOCAL_DIR)/msm8909-mtp.dtb \
