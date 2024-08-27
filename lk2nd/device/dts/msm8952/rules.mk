7b80e662a61b4512134a561facb7343dbb62ac4e
André Apitzsch <git@apitzsch.eu>
2024-08-27 23:49:15 +0200

    dts: msm8952: Fix montana detection with all dtbs enabled

--- lk2nd/device/dts/msm8952/rules.mk
+++ lk2nd/device/dts/msm8952/rules.mk
@@ -11,6 +11,3 @@ ADTBS += \
 	$(LOCAL_DIR)/msm8940-xiaomi-santoni.dtb \
 	$(LOCAL_DIR)/msm8956-mtp.dtb \
 	$(LOCAL_DIR)/msm8976-qrd.dtb \
-
-QCDTBS += \
-	$(LOCAL_DIR)/msm8937-motorola-montana.dtb \
