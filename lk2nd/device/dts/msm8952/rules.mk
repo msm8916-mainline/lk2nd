33fff32cb408c9ad1db1c168e4781eaecfc8aac0
Barnabás Czémán <trabarni@gmail.com>
2024-09-02 17:12:12 +0200

    dts: msm8952: Add support for Xiaomi Redmi Note 5A Prime (ugg)
    Add Xiaomi Redmi Note 5 Prime (ugg), ugg is an mtp device and
    using same panels like ugglite.

--- lk2nd/device/dts/msm8952/rules.mk
+++ lk2nd/device/dts/msm8952/rules.mk
@@ -9,6 +9,7 @@ ADTBS += \
 	$(LOCAL_DIR)/msm8937-mtp.dtb \
 	$(LOCAL_DIR)/msm8937-nokia-ple.dtb \
 	$(LOCAL_DIR)/msm8937-xiaomi-land.dtb \
+	$(LOCAL_DIR)/msm8940-mtp.dtb \
 	$(LOCAL_DIR)/msm8940-oppo-a57.dtb \
 	$(LOCAL_DIR)/msm8940-xiaomi-santoni.dtb \
 	$(LOCAL_DIR)/msm8956-mtp.dtb \
