7666686169462cbefba08c29916ed69299fcb1ea
Barnabás Czémán <trabarni@gmail.com>
2024-10-16 22:53:32 +0200

    doc/dts: msm8974: Remove hammerhead from qcdtbs and document the quirks
    LG Nexus 5 (hammerhead) should not be added for QCDTBS list it breaks
    other devices. It works with ADTBS and QCDTBS list should be empty.

--- lk2nd/device/dts/msm8974/rules.mk
+++ lk2nd/device/dts/msm8974/rules.mk
@@ -7,7 +7,6 @@ QCDTBS += \
 	$(LOCAL_DIR)/msm8974pro-ac-pm8941-mtp.dtb \
 	$(LOCAL_DIR)/msm8974-htc-m8.dtb \
 	$(LOCAL_DIR)/msm8974-lge-d855.dtb \
-	$(LOCAL_DIR)/msm8974-lge-hammerhead.dtb \
 
 ADTBS += \
 	$(LOCAL_DIR)/msm8974-lge-hammerhead.dtb \
