109de35c16478cc0b8daf0477013dc8b42a3872d
exkc <exkc@exkc.moe>
2024-10-17 18:42:16 +0800

    dts: add MSM8909 QRD SKUB

--- lk2nd/device/dts/msm8909/rules.mk
+++ lk2nd/device/dts/msm8909/rules.mk
@@ -6,6 +6,7 @@ QCDTBS += \
 	$(LOCAL_DIR)/msm8905-qrd-skub.dtb \
 	$(LOCAL_DIR)/msm8909-1gb-qrd-skuc.dtb \
 	$(LOCAL_DIR)/msm8909-mtp.dtb \
+	$(LOCAL_DIR)/msm8909-qrd-skub.dtb \
 
 ADTBS += \
         $(LOCAL_DIR)/apq8009w-wtp.dtb \
