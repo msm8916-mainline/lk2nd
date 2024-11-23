8b9e3cc40424849f86a612177614a8998303f1af
Andrew Gigena <mail@andrewgigena.dev>
2024-11-23 15:20:39 -0300

    dts: msm8952: Add support for HMD Global Nokia 5

--- lk2nd/device/dts/msm8952/rules.mk
+++ lk2nd/device/dts/msm8952/rules.mk
@@ -8,6 +8,7 @@ ADTBS += \
 	$(LOCAL_DIR)/msm8937-huawei-aum.dtb \
 	$(LOCAL_DIR)/msm8937-motorola-jeter.dtb \
 	$(LOCAL_DIR)/msm8937-mtp.dtb \
+	$(LOCAL_DIR)/msm8937-nokia-nd1.dtb \
 	$(LOCAL_DIR)/msm8937-nokia-ple.dtb \
 	$(LOCAL_DIR)/msm8937-xiaomi-land.dtb \
 	$(LOCAL_DIR)/msm8940-mtp.dtb \
