1be2df128ad01183e75fe2c2b5ac4afbcd52509f
DeltaSporos <60284244+DeltaSporos@users.noreply.github.com>
2025-02-08 15:57:44 -0500

    Add support for LG G5 (H850)

--- lk2nd/device/dts/msm8996/rules.mk
+++ lk2nd/device/dts/msm8996/rules.mk
@@ -3,5 +3,6 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 ADTBS += \
 	$(LOCAL_DIR)/msm8996pro-oneplus3t.dtb \
+	$(LOCAL_DIR)/msm8996-lg-h850.dtb \
 	$(LOCAL_DIR)/msm8996-lg-h990.dtb \
 	$(LOCAL_DIR)/msm8996-xiaomi-mi5.dtb \
