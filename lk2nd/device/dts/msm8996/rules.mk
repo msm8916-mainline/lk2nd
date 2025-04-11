ac3367e4d1570232e2cf70523fd5b25d5577cb91
LuK1337 <priv.luk@gmail.com>
2025-04-11 21:21:15 +0200

    dts: msm8996: Add support for Xiaomi Mi 5s

--- lk2nd/device/dts/msm8996/rules.mk
+++ lk2nd/device/dts/msm8996/rules.mk
@@ -6,4 +6,5 @@ ADTBS += \
 	$(LOCAL_DIR)/msm8996-lg-h850.dtb \
 	$(LOCAL_DIR)/msm8996-lg-h990.dtb \
 	$(LOCAL_DIR)/msm8996-xiaomi-mi5.dtb \
+	$(LOCAL_DIR)/msm8996-xiaomi-mi5s.dtb \
 	$(LOCAL_DIR)/msm8996-zte-axon7.dtb \
