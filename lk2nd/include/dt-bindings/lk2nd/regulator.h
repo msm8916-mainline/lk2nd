c76eef60010ab7d12321d11f09ac88d4d53854e2
Barnabás Czémán <barnabas.czeman@mainlining.org>
2025-05-25 20:29:26 +0200

    lk2nd: device: Add regulator-ldo driver
    Add simple driver for enable LDO regulators.
    
    Signed-off-by: Barnabás Czémán <barnabas.czeman@mainlining.org>

---
+++ lk2nd/include/dt-bindings/lk2nd/regulator.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+#ifndef DT_BINDINGS_REGULATOR_H
+#define DT_BINDINGS_REGULATOR_H
+
+#define REG_LDO1	BIT(0)
+#define REG_LDO2	BIT(1)
+#define REG_LDO3	BIT(2)
+#define REG_LDO4	BIT(4)
+#define REG_LDO5	BIT(4)
+#define REG_LDO6	BIT(5)
+#define REG_LDO11	BIT(10)
+#define REG_LDO12	BIT(11)
+#define REG_LDO13	BIT(12)
+#define REG_LDO14	BIT(13)
+#define REG_LDO15	BIT(14)
+#define REG_LDO17	BIT(16)
+#define REG_LDO18	BIT(17)
+#define REG_LDO28	BIT(27)
+#define REG_SMPS3	BIT(7)
+
+#endif
