fd3e12dd8e50dd035eee40edaa3d2fee09c7a3b7
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-07 21:44:33 +0800

    lk2nd: dev_tree: Mark as best dtb node if compatible matches with lk2nd dtb
    Makes sure we load the correct dtb node based on lk2nd's device
    selection logic when boot.img contains multiple dtbs with same ids.
    
    For example, on msm8953-xiaomi-common.dts, we see 3 devices sharing
    the same msm-id and board-id. If a given boot.img contains dtbs for
    all these devices, without this logic lk2nd will always load the
    first of these dtbs, regardless of the device it's being run on.
    
    Try using compatible string from `dtb_compatible` property first,
    because there's some devices supposed to use some other devices' dtb
    in mainline kernel (e.g. xiaomi-sakura uses xiaomi-daisy dtb).
    
    This shouldn't affect booting downstream kernels, since they normally
    don't define `<vendor>,<device>` on their dts compatible.
    
    Change-Id: Ib7ed0908038f178d34eaa6b4952691c17055c22b

--- lk2nd/device/device.c
+++ lk2nd/device/device.c
@@ -24,6 +24,14 @@ const char *const *lk2nd_device_get_dtb_hints(void)
 	return lk2nd_dev.dtbfiles;
 }
 
+/**
+ * lk2nd_device_get_dtb_compatible() - Get a compatible string for matching kernel dtb.
+ */
+const char *lk2nd_device_get_dtb_compatible(void)
+{
+	return lk2nd_dev.dtb_compatible ? lk2nd_dev.dtb_compatible : lk2nd_dev.compatible;
+}
+
 static int find_device_node(const void *dtb)
 {
 	int lk2nd_node, node, ret;
@@ -111,6 +119,12 @@ static void parse_dtb(const void *dtb)
 		lk2nd_dev.compatible = strndup(val, len);
 	else
 		dprintf(CRITICAL, "Failed to read 'compatible': %d\n", len);
+
+	val = fdt_getprop(dtb, node, "dtb_compatible", &len);
+	if (val && len > 0)
+		lk2nd_dev.dtb_compatible = strndup(val, len);
+	else
+		dprintf(CRITICAL, "Failed to read 'dtb_compatible': %d\n", len);
 
 	val = fdt_getprop(dtb, node, "model", &len);
 	if (val && len > 0)
