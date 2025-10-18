e8fae12bf672bb69b04d0f63f8871699a2686739
Denis Burkov <hitechshell@mail.ru>
2025-10-19 02:46:13 +0500

    implement multiboot menu based on extlinux

--- lk2nd/boot/boot.h
+++ lk2nd/boot/boot.h
@@ -7,10 +7,25 @@
 
 #include <lk2nd/boot.h>
 
+struct label {
+	const char *name;
+	const char *kernel;
+	const char *initramfs;
+	const char *dtb;
+	const char *dtbdir;
+	const char **dtboverlays;
+	const char *cmdline;
+};
+
 /* util.c */
 void lk2nd_print_file_tree(char *root, char *prefix);
 
 /* extlinux.c */
 void lk2nd_try_extlinux(const char *mountpoint);
+int lk2nd_get_extlinux_labels(const char *root, struct label **labels, int *default_label_idx, int* labels_count);
+int lk2nd_expand_conf_and_boot_label(struct label *label, const char *root);
+
+/* boot.c */
+int lk2nd_scan_devices(char **root_out, struct label **labels, int *default_label_idx, int* labels_count);
 
 #endif /* LK2ND_BOOT_BOOT_H */
--- lk2nd/hw/bdev/bdev.c
+++ lk2nd/hw/bdev/bdev.c
@@ -11,9 +11,14 @@
  */
 void lk2nd_bdev_init(void)
 {
+	static int init_done = 0;
+	if (init_done) {
+		return;
+	}
 	lk2nd_wrapper_bio_register();
 	if (IS_ENABLED(MMC_SDHCI_SUPPORT))
 		lk2nd_mmc_sdhci_bio_register();
 
 	lk2nd_bdev_dump_devices();
+	init_done = 1;
 }
