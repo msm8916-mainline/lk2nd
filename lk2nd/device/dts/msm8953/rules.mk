4741ddabe35eeb47d58fce7c1cb3294bc611f42e
Raymond Hackley <raymondhackley@protonmail.com>
2025-10-22 23:58:00 +0000

    dts: msm8953: rename sdm450-samsung-r05 to sda450-samsung-r05

--- lk2nd/device/dts/msm8953/rules.mk
+++ lk2nd/device/dts/msm8953/rules.mk
@@ -19,10 +19,10 @@ ADTBS += \
 	$(LOCAL_DIR)/msm8953-xiaomi-markw.dtb \
 	$(LOCAL_DIR)/msm8953-xiaomi-oxygen.dtb \
 	$(LOCAL_DIR)/msm8953-xiaomi-vince.dtb \
+	$(LOCAL_DIR)/sda450-samsung-r05.dtb \
 	$(LOCAL_DIR)/sdm450-motorola-ali.dtb \
 	$(LOCAL_DIR)/sdm450-mtp.dtb \
 	$(LOCAL_DIR)/sdm450-samsung-r04.dtb \
-	$(LOCAL_DIR)/sdm450-samsung-r05.dtb \
 	$(LOCAL_DIR)/sdm450-xiaomi-rosy.dtb \
 	$(LOCAL_DIR)/sdm632-fairphone-fp3.dtb \
 	$(LOCAL_DIR)/sdm632-mtp.dtb \
