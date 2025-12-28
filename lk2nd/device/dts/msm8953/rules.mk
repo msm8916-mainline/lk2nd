cb864ffd6ec5584c635d1874115b8e177f758901
17683954109@163.com <17683954109@163.com>
2025-12-28 17:59:37 +0800

    dts: msm8953: Add support for Meizu M15(m1871)
    update Documentation/devices.md and sort lk2nd/device/dts/rules.mk

--- lk2nd/device/dts/msm8953/rules.mk
+++ lk2nd/device/dts/msm8953/rules.mk
@@ -8,6 +8,7 @@ ADTBS += \
 	$(LOCAL_DIR)/msm8953-huawei-cannes.dtb \
 	$(LOCAL_DIR)/msm8953-huawei-milan.dtb \
 	$(LOCAL_DIR)/msm8953-lenovo-kuntao.dtb \
+	$(LOCAL_DIR)/msm8953-meizu-m1871.dtb \
 	$(LOCAL_DIR)/msm8953-motorola-deen.dtb \
 	$(LOCAL_DIR)/msm8953-motorola-sanders.dtb \
 	$(LOCAL_DIR)/msm8953-mtp.dtb \
