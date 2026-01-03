44776de1a861d96909330bced2bf8997921ddaba
Tikily <118419602+Tikily@users.noreply.github.com>
2026-01-03 13:57:04 +0800

    Remove duplicate include for lk_panel_nt35510s_fwvga_cmd

--- lk2nd/display/panel/generated/panels.h
+++ lk2nd/display/panel/generated/panels.h
@@ -12,9 +12,10 @@
 #include "lk_panel_ili9806e_fwvga_hsd_hlt_video.h"
 #include "lk_panel_ili9881c_hd_video.h"
 #include "lk_panel_innolux_qhd_video.h"
+#include "lk_panel_nt35510s_fwvga_cmd.h"
 #include "lk_panel_r61308_720p_video.h"
 #include "lk_panel_r69431_720p_video.h"
-#include "lk_panel_nt35510s_fwvga_cmd.h"
+
 
 int oem_panel_select(const char *panel_name, struct panel_struct *panel,
 		     struct msm_panel_info *pinfo, struct mdss_dsi_phy_ctrl *phy_db);
