a863ca10edd0fdfdaa6484c8ac0233772699decc
Tikily <1606320261@qq.com>
2026-01-08 18:40:33 +0800

    add support for nt35510s

--- lk2nd/display/panel/generated/panels.h
+++ lk2nd/display/panel/generated/panels.h
@@ -12,6 +12,7 @@
 #include "lk_panel_ili9806e_fwvga_hsd_hlt_video.h"
 #include "lk_panel_ili9881c_hd_video.h"
 #include "lk_panel_innolux_qhd_video.h"
+#include "lk_panel_nt35510s_fwvga_cmd.h"
 #include "lk_panel_r61308_720p_video.h"
 #include "lk_panel_r69431_720p_video.h"
 
