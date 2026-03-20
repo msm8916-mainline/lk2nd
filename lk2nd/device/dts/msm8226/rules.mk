612ebd477ba6b6191600e715c47a9d9f25a978c1
David Wales <daviewales@disroot.org>
2026-03-20 20:47:20 +1100

    dts: msm8226: motorola-titan: Fix detection of titan

--- lk2nd/device/dts/msm8226/rules.mk
+++ lk2nd/device/dts/msm8226/rules.mk
@@ -5,6 +5,7 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 ADTBS += \
 	$(LOCAL_DIR)/apq8026-lg-lenok.dtb \
 	$(LOCAL_DIR)/msm8226-motorola-falcon.dtb \
+	$(LOCAL_DIR)/msm8226-motorola-titan.dtb \
 	$(LOCAL_DIR)/msm8926-motorola-peregrine.dtb \
 	$(LOCAL_DIR)/msm8926-motorola-thea.dtb \
 
@@ -12,7 +13,6 @@ QCDTBS += \
 	$(LOCAL_DIR)/apq8026-asus-sparrow.dtb \
 	$(LOCAL_DIR)/apq8026-huawei-sturgeon.dtb \
 	$(LOCAL_DIR)/apq8026-samsung.dtb \
-	$(LOCAL_DIR)/msm8226-motorola-titan.dtb \
 	$(LOCAL_DIR)/msm8226-samsung.dtb \
 	$(LOCAL_DIR)/msm8926-htc-memul.dtb \
 	$(LOCAL_DIR)/msm8926-huawei-g6-l11-vb.dtb \
