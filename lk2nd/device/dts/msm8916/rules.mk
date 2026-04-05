999ce8245d0ab822aa69c36026bcc9959aebb1cd
QuiXan <126559216+QuiXan@users.noreply.github.com>
2026-04-05 22:41:28 +0800

    dts: msm8916: Add support for Vivo Y31a

--- lk2nd/device/dts/msm8916/rules.mk
+++ lk2nd/device/dts/msm8916/rules.mk
@@ -34,6 +34,7 @@ QCDTBS += \
 	$(LOCAL_DIR)/msm8916-samsung.dtb \
 	$(LOCAL_DIR)/msm8916-vivo-pd1304cl.dtb \
 	$(LOCAL_DIR)/msm8916-vivo-pd1505.dtb \
+	$(LOCAL_DIR)/msm8916-vivo-pd1505f.dtb \
 	$(LOCAL_DIR)/msm8916-vivo-pd1510.dtb \
 	$(LOCAL_DIR)/msm8916-vivo-y21l.dtb \
 	$(LOCAL_DIR)/msm8929-samsung.dtb \
