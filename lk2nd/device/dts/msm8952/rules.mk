e590c07ffc29b28921125be5ff0c828dfc30dd55
QuiXan <126559216+QuiXan@users.noreply.github.com>
2026-05-27 11:16:03 +0800

    Add msm8940-huawei-trt-tl10.dtb to rules.mk

--- lk2nd/device/dts/msm8952/rules.mk
+++ lk2nd/device/dts/msm8952/rules.mk
@@ -13,6 +13,7 @@ ADTBS += \
 	$(LOCAL_DIR)/msm8937-nokia-nd1.dtb \
 	$(LOCAL_DIR)/msm8937-nokia-ple.dtb \
 	$(LOCAL_DIR)/msm8937-xiaomi-land.dtb \
+	$(LOCAL_DIR)/msm8940-huawei-trt-tl10.dtb \
 	$(LOCAL_DIR)/msm8940-mtp.dtb \
 	$(LOCAL_DIR)/msm8940-oppo-a57.dtb \
 	$(LOCAL_DIR)/msm8940-xiaomi-santoni.dtb \
