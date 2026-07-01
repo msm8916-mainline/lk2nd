5b310be3d81d7dcef4e5b43568ca0ec3512054f8
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2026-07-01 22:21:50 +0200

    dts: msm8937: Add support for Asus Zenfone 4 Max Pro (x00i)

--- lk2nd/device/dts/msm8952/rules.mk
+++ lk2nd/device/dts/msm8952/rules.mk
@@ -7,6 +7,7 @@ ADTBS += \
 	$(LOCAL_DIR)/msm8917-xiaomi-riva.dtb \
 	$(LOCAL_DIR)/msm8920-motorola-jeter.dtb \
 	$(LOCAL_DIR)/msm8920-mtp.dtb \
+	$(LOCAL_DIR)/msm8937-asus-x00i.dtb \
 	$(LOCAL_DIR)/msm8937-huawei-aum.dtb \
 	$(LOCAL_DIR)/msm8937-medion-malata.dtb \
 	$(LOCAL_DIR)/msm8937-motorola-jeter.dtb \
